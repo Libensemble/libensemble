66b64fba0f915f9b2cd84abc54a2b629cb3da472
jmlarson1 <jmlarson@anl.gov>
2017-07-26 08:10:01 -0500

    Making dtype declarations not strings

--- code/examples/chwirut_and_aposmm_one_residual_at_a_time/call_libE_on_chwirut.py
+++ code/examples/chwirut_and_aposmm_one_residual_at_a_time/call_libE_on_chwirut.py
@@ -36,29 +36,29 @@ allocation_specs = {'manager_ranks': set([0]),
 
 sim_specs = {'sim_f': [libE_func_wrapper],
              'in': ['x', 'obj_component'],
-             'out': [('f_i','float'),
+             'out': [('f_i',float),
                      ],
              'params': {}, 
              }
 
-out = [('x','float',n),
-      ('x_on_cube','float',n),
-      ('sim_id','int'),
-      ('priority','float'),
-      ('iter_plus_1_in_run_id','int',max_sim_budget//m),
-      ('local_pt','bool'),
-      ('known_to_aposmm','bool'), # Mark known points so fewer updates are needed.
-      ('dist_to_unit_bounds','float'),
-      ('dist_to_better_l','float'),
-      ('dist_to_better_s','float'),
-      ('ind_of_better_l','int'),
-      ('ind_of_better_s','int'),
-      ('started_run','bool'),
-      ('num_active_runs','int'), # Number of active runs point is involved in
-      ('local_min','bool'),
-      ('obj_component','int'),
-      ('f','float'), # To store the point's combined objective function value (after all f_i are computed)
-      ('pt_id','int'), # To be used by APOSMM to identify points evaluated by different simulations
+out = [('x',float,n),
+      ('x_on_cube',float,n),
+      ('sim_id',int),
+      ('priority',float),
+      ('iter_plus_1_in_run_id',int,max_sim_budget//m),
+      ('local_pt',bool),
+      ('known_to_aposmm',bool), # Mark known points so fewer updates are needed.
+      ('dist_to_unit_bounds',float),
+      ('dist_to_better_l',float),
+      ('dist_to_better_s',float),
+      ('ind_of_better_l',int),
+      ('ind_of_better_s',int),
+      ('started_run',bool),
+      ('num_active_runs',int), # Number of active runs point is involved in
+      ('local_min',bool),
+      ('obj_component',int),
+      ('f',float), # To store the point's combined objective function value (after all f_i are computed)
+      ('pt_id',int), # To be used by APOSMM to identify points evaluated by different simulations
       ]
 
 gen_specs = {'gen_f': aposmm_logic,
