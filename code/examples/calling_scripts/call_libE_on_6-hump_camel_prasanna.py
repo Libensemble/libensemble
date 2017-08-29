279b1ec25f77efb17b290e875a3c3ba61336b665
jmlarson1 <jmlarson@anl.gov>
2017-08-29 14:12:30 -0500

    Improving prasanna example: better machinefile. Calling script limits ranks_per_node and num_nodes

--- code/examples/calling_scripts/call_libE_on_6-hump_camel_prasanna.py
+++ code/examples/calling_scripts/call_libE_on_6-hump_camel_prasanna.py
@@ -11,87 +11,29 @@ from __future__ import division
 from __future__ import absolute_import
 
 from mpi4py import MPI # for libE communicator
-import sys             # for adding to path
+import sys, os             # for adding to path
 import numpy as np
 from math import *
 
 import time
 
+# Import libEnsemble main
 sys.path.append('../../src')
 from libE import libE
 
-def six_hump_camel(H, sim_out, obj_params, info):
-    import subprocess, os
-    batch = len(H['x'])
-    O = np.zeros(batch,dtype=sim_out)
+# Import sim_func 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../examples/sim_funcs'))
+from six_hump_camel import six_hump_camel_with_different_ranks_and_nodes
 
-
-    for i,x in enumerate(H['x']):
-
-        if 'blocking' in info:
-            ranks_involved = [MPI.COMM_WORLD.Get_rank()] +  list(info['blocking'])
-        else:
-            ranks_involved = [MPI.COMM_WORLD.Get_rank()] 
-
-        machinefilename = str(ranks_involved)
-
-        with open(machinefilename,'w') as f:
-            for rank in ranks_involved:
-                b = obj_params['nodelist'][rank] + '\n'
-                f.write(b*H['ranks_per_node'][i])
-
-        outfile_name = "outfile_"+ machinefilename+".txt"
-        if os.path.isfile(outfile_name) == True:
-            os.remove(outfile_name)
-
-        call_str = ["mpiexec","-np",str(H[i]['ranks_per_node']*len(ranks_involved)),"-machinefile",machinefilename,"python", "../sim_funcs/helloworld.py"]
-        print(call_str)
-        process = subprocess.call(call_str, stdout = open(outfile_name,'w'), shell=False)
-
-        x1 = H['x'][i][0]
-        x2 = H['x'][i][1]
-        term1 = (4-2.1*x1**2+(x1**4)/3) * x1**2;
-        term2 = x1*x2;
-        term3 = (-4+4*x2**2) * x2**2;
-
-        O['f'][i] = term1 + term2 + term3;
-
-        # v = np.random.uniform(0,10)
-        # print('About to sleep for :' + str(v))
-        # time.sleep(v)
-    
-    return O
-
-def uniform_random_sample(g_in,gen_out,params,info):
-    ub = params['ub']
-    lb = params['lb']
-    n = len(lb)
-
-    if len(g_in) == 0: 
-        b = params['initial_batch_size']
-
-        O = np.zeros(b, dtype=gen_out)
-        for i in range(0,b):
-            x = np.random.uniform(lb,ub,(1,n))
-            O['x'][i] = x
-            O['num_nodes'][i] = 1
-            O['ranks_per_node'][i] = 16
-            O['priority'] = 1
-        
-    else:
-        O = np.zeros(1, dtype=gen_out)
-        O['x'] = len(g_in)*np.ones(n)
-        O['num_nodes'] = np.random.choice([1,2,3,4]) 
-        O['ranks_per_node'] = np.random.randint(1,17)
-        O['priority'] = 10*O['num_nodes']
-
-    return O
-
+# Import gen_func 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../examples/gen_funcs'))
+from uniform_sampling import uniform_random_sample_with_different_nodes_and_ranks
 
 libE_machinefile = open(sys.argv[1]).read().splitlines()
+# libE_machinefile = [MPI.Get_processor_name()]*MPI.COMM_WORLD.Get_size()
 
 #State the objective function, its arguments, output, and necessary parameters (and their sizes)
-sim_specs = {'sim_f': [six_hump_camel], # This is the function whose output is being minimized
+sim_specs = {'sim_f': [six_hump_camel_with_different_ranks_and_nodes], # This is the function whose output is being minimized
              'in': ['x','num_nodes','ranks_per_node'], # These keys will be given to the above function
              'out': [('f',float), # This is the output from the function being minimized
                     ],
@@ -100,7 +42,7 @@ sim_specs = {'sim_f': [six_hump_camel], # This is the function whose output is b
              }
 
 # State the generating function, its arguments, output, and necessary parameters.
-gen_specs = {'gen_f': uniform_random_sample,
+gen_specs = {'gen_f': uniform_random_sample_with_different_nodes_and_ranks,
              'in': ['sim_id'],
              'out': [('x',float,2),
                      ('priority',float),
@@ -110,6 +52,8 @@ gen_specs = {'gen_f': uniform_random_sample,
              'params': {'lb': np.array([-3,-2]),
                         'ub': np.array([ 3, 2]),
                         'initial_batch_size': 5,
+                        'max_ranks_per_node': 8,
+                        'max_num_nodes': MPI.COMM_WORLD.Get_size()-1,
                        },
              'num_inst': 1,
              'batch_mode': False,
--- code/examples/sim_funcs/six_hump_camel.py
+++ code/examples/sim_funcs/six_hump_camel.py
@@ -18,7 +18,7 @@ def six_hump_camel_with_different_ranks_and_nodes(H, sim_out, obj_params, info):
         else:
             ranks_involved = [MPI.COMM_WORLD.Get_rank()] 
 
-        machinefilename = str(ranks_involved)
+        machinefilename = 'machinefile_for_sim_id=' + str(info['sim_id'][i] )+ '_ranks='+'_'.join([str(r) for r in ranks_involved])
 
         with open(machinefilename,'w') as f:
             for rank in ranks_involved:
@@ -29,7 +29,7 @@ def six_hump_camel_with_different_ranks_and_nodes(H, sim_out, obj_params, info):
         if os.path.isfile(outfile_name) == True:
             os.remove(outfile_name)
 
-        call_str = ["mpiexec","-np",str(H[i]['ranks_per_node']*len(ranks_involved)),"-machinefile",machinefilename,"python", "../sim_funcs/helloworld.py"]
+        call_str = ["mpiexec","-np",str(H[i]['ranks_per_node']*len(ranks_involved)),"-machinefile",machinefilename,"python", os.path.join(os.path.dirname(__file__),"helloworld.py")]
         process = subprocess.call(call_str, stdout = open(outfile_name,'w'), shell=False)
 
         O['f'][i] = six_hump_camel_func(H['x'][i])
