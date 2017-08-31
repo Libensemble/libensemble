1d5f5693e92f6f323182c231aec1bd75e3e830cb
jmlarson1 <jmlarson@anl.gov>
2017-08-31 15:56:43 -0500

    Adding new unit tests

---
+++ code/tests/unit_tests/test_libE_main.py
@@ -0,0 +1,16 @@
+import sys, time, os
+import numpy as np
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../src')) 
+
+from libE import * 
+
+def test_nonworker_and_nonmanager_rank():
+
+    # Intentionally making worker 0 not be a manager or worker rank
+    libE({'out':[('f',float)]},{'out':[('x',float)]},{'sim_max':1},allocation_specs={'manager_ranks':set([1]), 'worker_ranks':set([1])})
+
+
+if __name__ == "__main__":
+    # import ipdb; ipdb.set_trace()
+    test_nonworker_and_nonmanager_rank()
