0b126d5ddeee7915e29df441c2db710ef06a85f4
shudson <shudson@anl.gov>
2017-09-26 22:03:34 -0500

    Add napoleon extension to enabled numpy style docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ print(sys.path)
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 #extensions = ['sphinx.ext.autodoc', 'breathe']
 #breathe_projects = { "libensemble": "../code/src/xml/" }
 #breathe_default_project = "libensemble"
