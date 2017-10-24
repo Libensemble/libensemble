cb21324fa8e8e8267f6e15d11b7149b07dd6f72f
Jeffrey Larson <jmlarson@anl.gov>
2017-10-24 14:29:36 -0500

    Moving allocation_functions (alloc_funcs) to their own directory in examples

---
+++ code/examples/alloc_funcs/give_sim_work_first.py
@@ -0,0 +1,78 @@
+def give_sim_work_first(active_w, idle_w, H, H_ind, sim_specs, gen_specs, term_test):
+    """ Decide what should be given to workers. Note that everything put into
+    the Work dictionary will be given, so we are careful not to put more gen or
+    sim items into Work than necessary.
+    """
+
+    Work = {}
+    gen_work = 0
+
+    for i in idle_w:
+        if term_test(H, H_ind):
+            break
+
+        # Only consider giving to worker i if it's resources are not blocked by some other calculation
+        blocked_set = active_w['blocked'].union(*[j['calc_info']['blocking'] for j in Work.values() if 'blocking' in j['calc_info']])
+        if i in blocked_set:
+            continue
+
+        # Find indices of H_s where that are not given nor paused
+        q_inds_logical = np.logical_and(~H['given'][:H_ind],~H['paused'][:H_ind])
+
+        if np.any(q_inds_logical):
+            # Give sim work if possible
+
+            if 'priority' in H.dtype.fields:
+                if 'give_all_with_same_priority' in gen_specs and gen_specs['give_all_with_same_priority']:
+                    # Give all points with highest priority
+                    q_inds = H['priority'][:H_ind][q_inds_logical] == np.max(H['priority'][:H_ind][q_inds_logical])
+                    sim_ids_to_send = np.nonzero(q_inds_logical)[0][q_inds]
+                else:
+                    # Give first point with highest priority
+                    sim_ids_to_send = np.nonzero(q_inds_logical)[0][np.argmax(H['priority'][:H_ind][q_inds_logical])]
+            else:
+                # Give oldest point
+                sim_ids_to_send = np.nonzero(q_inds_logical)[0][0]
+
+            sim_ids_to_send = np.atleast_1d(sim_ids_to_send)
+
+            # Only give work if enough idle workers
+            if 'num_nodes' in H.dtype.names and np.any(H[sim_ids_to_send]['num_nodes'] > 1):
+                if np.any(H[sim_ids_to_send]['num_nodes'] > len(idle_w) - len(Work) - len(blocked_set)):
+                    # Worker i doesn't get any work. Just waiting for other resources to open up
+                    continue
+                block_others = True
+            else:
+                block_others = False
+
+            Work[i] = {'calc_rows': sim_ids_to_send,
+                       'calc_info': {'type':'sim', 'sim_id': sim_ids_to_send},
+                      }
+
+            if block_others:
+                unassigned_workers = idle_w - set(Work.keys()) - blocked_set
+                workers_to_block = list(unassigned_workers)[:np.max(H[sim_ids_to_send]['num_nodes'])-1]
+                Work[i]['calc_info']['blocking'] = set(workers_to_block)
+
+            update_history_x_out(H, sim_ids_to_send, i)
+
+        else:
+            # Since there is no sim work to give, give gen work. 
+
+            # Limit number of gen instances if given
+            if 'num_inst' in gen_specs and len(active_w['gen']) + gen_work >= gen_specs['num_inst']:
+                break
+
+            # Don't give out any gen instances if in batch mode and any point has not been returned or paused
+            if 'batch_mode' in gen_specs and gen_specs['batch_mode'] and np.any(np.logical_and(~H['returned'][:H_ind],~H['paused'][:H_ind])):
+                break
+
+            # Give gen work 
+            gen_work += 1 
+
+            Work[i] = {'calc_rows': range(0,H_ind),
+                       'calc_info': {'type':'gen'},
+                       }
+
+
+    return Work
