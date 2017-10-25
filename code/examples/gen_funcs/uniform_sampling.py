f87a04f51fc0ae31add94963363cd61dc77b3fd4
Jeffrey Larson <jmlarson@anl.gov>
2017-10-25 07:30:34 -0500

    Changing test_6-hump_camel_uniform_sampling to new format

--- code/examples/gen_funcs/uniform_sampling.py
+++ code/examples/gen_funcs/uniform_sampling.py
@@ -3,15 +3,15 @@ from __future__ import absolute_import
 
 import numpy as np
 
-def uniform_random_sample_with_different_nodes_and_ranks(g_in,gen_out,params,info):
-    ub = params['ub']
-    lb = params['lb']
+def uniform_random_sample_with_different_nodes_and_ranks(H_s,H_g,gen_specs,info):
+    ub = gen_specs['ub']
+    lb = gen_specs['lb']
     n = len(lb)
 
-    if len(g_in) == 0: 
-        b = params['initial_batch_size']
+    if len(H_s) == 0: 
+        b = gen_specs['initial_batch_size']
 
-        O = np.zeros(b, dtype=gen_out)
+        O = np.zeros(b, dtype=gen_specs['out'])
         for i in range(0,b):
             x = np.random.uniform(lb,ub,(1,n))
             O['x'][i] = x
@@ -20,24 +20,24 @@ def uniform_random_sample_with_different_nodes_and_ranks(g_in,gen_out,params,inf
             O['priority'] = 1
         
     else:
-        O = np.zeros(1, dtype=gen_out)
-        O['x'] = len(g_in)*np.ones(n)
-        O['num_nodes'] = np.random.randint(1,params['max_num_nodes']+1) 
-        O['ranks_per_node'] = np.random.randint(1,params['max_ranks_per_node']+1)
+        O = np.zeros(1, dtype=gen_specs['out'])
+        O['x'] = len(H_s)*np.ones(n)
+        O['num_nodes'] = np.random.randint(1,gen_specs['max_num_nodes']+1) 
+        O['ranks_per_node'] = np.random.randint(1,gen_specs['max_ranks_per_node']+1)
         O['priority'] = 10*O['num_nodes']
 
     return O
 
 
-def uniform_random_sample_obj_components(g_in,gen_out,params,info):
-    ub = params['ub']
-    lb = params['lb']
+def uniform_random_sample_obj_components(H_s,H_g,gen_specs,info):
+    ub = gen_specs['ub']
+    lb = gen_specs['lb']
 
     n = len(lb)
-    m = params['components']
-    b = params['gen_batch_size']
+    m = gen_specs['components']
+    b = gen_specs['gen_batch_size']
 
-    O = np.zeros(b*m, dtype=gen_out)
+    O = np.zeros(b*m, dtype=gen_specs['out'])
     for i in range(0,b):
         x = np.random.uniform(lb,ub,(1,n))
 
@@ -45,18 +45,18 @@ def uniform_random_sample_obj_components(g_in,gen_out,params,info):
         O['priority'][i*m:(i+1)*m] = np.random.uniform(0,1,m)
         O['obj_component'][i*m:(i+1)*m] = np.arange(0,m)
 
-        O['pt_id'][i*m:(i+1)*m] = len(g_in)//m+i
+        O['pt_id'][i*m:(i+1)*m] = len(H_s)//m+i
 
     return O
 
-def uniform_random_sample(g_in,gen_out,params,info):
-    ub = params['ub']
-    lb = params['lb']
+def uniform_random_sample(H_s,H_g,gen_specs,info):
+    ub = gen_specs['params']['ub']
+    lb = gen_specs['params']['lb']
 
     n = len(lb)
-    b = params['gen_batch_size']
+    b = gen_specs['params']['gen_batch_size']
 
-    O = np.zeros(b, dtype=gen_out)
+    O = np.zeros(b, dtype=gen_specs['out'])
     for i in range(0,b):
         x = np.random.uniform(lb,ub,(1,n))
 
