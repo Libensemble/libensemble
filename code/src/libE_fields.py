a70c31ff4499604ae8d6a5a87e833e9030c30407
Jeffrey Larson <jmlarson@anl.gov>
2017-10-26 15:23:45 -0500

    Adding a libE_fields file

---
+++ code/src/libE_fields.py
@@ -0,0 +1,9 @@
+
+libE_fields = [('sim_id',int),
+               ('given',bool),       
+               ('given_time',float), 
+               ('sim_rank',int),    
+               ('gen_rank',int),    
+               ('returned',bool),    
+               ('paused',bool),    
+               ]
