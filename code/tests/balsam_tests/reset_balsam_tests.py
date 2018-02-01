4197fb42ca8060a46a56f51cc018c2d2dda28517
shudson <shudson@anl.gov>
2018-02-01 16:50:06 -0600

    Change reset from state READY to state CREATED so obeys dependencies

--- code/tests/balsam_tests/reset_balsam_tests.py
+++ code/tests/balsam_tests/reset_balsam_tests.py
@@ -5,5 +5,5 @@ import balsam.launcher.dag as dag
 dag.BalsamJob.objects.filter(name__contains='outfile').delete()
 
 for job in dag.BalsamJob.objects.filter(name__contains='job_test_balsam'):
-    job.update_state('READY')
+    job.update_state('CREATED')
     job.save()
