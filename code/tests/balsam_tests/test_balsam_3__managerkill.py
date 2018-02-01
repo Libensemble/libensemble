b35ab5eb9fce3b8dda52f89c5bfd01fab719f539
shudson <shudson@anl.gov>
2018-02-01 14:27:41 -0600

    For balsam test 3 - make killing finished jobs default

--- code/tests/balsam_tests/test_balsam_3__managerkill.py
+++ code/tests/balsam_tests/test_balsam_3__managerkill.py
@@ -61,10 +61,11 @@ for sim_id in range(steps):
       BalsamJob = dag.BalsamJob
       
       #If job already finished will stage out results
-      pending_sim1_jobs = BalsamJob.objects.filter(name__contains='t3_for_sim_id_1').exclude(state='JOB_FINISHED')
+      
+      #pending_sim1_jobs = BalsamJob.objects.filter(name__contains='t3_for_sim_id_1').exclude(state='JOB_FINISHED')
       
       #If job already finished will NOT stage out results - once classed as USER_KILLED
-      #pending_sim1_jobs = BalsamJob.objects.filter(name__contains='t3_for_sim_id_1')
+      pending_sim1_jobs = BalsamJob.objects.filter(name__contains='t3_for_sim_id_1')
 
       num_pending = pending_sim1_jobs.count() #will only kill if already in database
 
