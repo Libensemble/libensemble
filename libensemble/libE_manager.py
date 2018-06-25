abed41d15781c302ff66110a56c274539db7a49f
shudson <shudson@anl.gov>
2018-06-25 11:41:30 -0500

    Add in termination test fix

--- libensemble/libE_manager.py
+++ libensemble/libE_manager.py
@@ -286,6 +286,10 @@ def termination_test(H, H_ind, exit_criteria, start_time, lenH0):
     Return nonzero if the libEnsemble run should stop 
     """
 
+    if 'elapsed_wallclock_time' in exit_criteria:
+        if time.time() - start_time >= exit_criteria['elapsed_wallclock_time']:
+            return 2
+        
     if 'sim_max' in exit_criteria:
         if np.sum(H['given']) >= exit_criteria['sim_max'] + lenH0:
             return 1
@@ -299,10 +303,6 @@ def termination_test(H, H_ind, exit_criteria, start_time, lenH0):
         val = exit_criteria['stop_val'][1]
         if np.any(H[key][:H_ind][~np.isnan(H[key][:H_ind])] <= val): 
             return 1
-
-    if 'elapsed_wallclock_time' in exit_criteria:
-        if time.time() - start_time >= exit_criteria['elapsed_wallclock_time']:
-            return 2
 
     return False
 
