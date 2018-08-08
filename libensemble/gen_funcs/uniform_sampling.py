05194fb920fdbf89b7c0828fcbb3e639812c129d
Jeffrey Larson <jmlarson@anl.gov>
2018-08-08 15:54:10 -0500

    Updating uniform_sampling docstrings

--- libensemble/gen_funcs/uniform_sampling.py
+++ libensemble/gen_funcs/uniform_sampling.py
@@ -11,8 +11,11 @@ def uniform_random_sample_with_different_nodes_and_ranks(H,persis_info,gen_specs
     Generates points uniformly over the domain defined by ``gen_specs['ub']`` and
     ``gen_specs['lb']``. Also randomly requests a different ``number_of_nodes``
     and ``ranks_per_node`` to be used in the evaluation of the generated point.
+    Uses (and returns) the random stream in persis_info, given by the
+    allocation function.
 
-    :Example usage: 
+    :See: 
+        ``libensemble/tests/regression_tests/test_6-hump_camel_with_different_nodes_uniform_sample.py``
     """
     ub = gen_specs['ub']
     lb = gen_specs['lb']
@@ -44,9 +47,12 @@ def uniform_random_sample_obj_components(H,persis_info,gen_specs,_):
     """
     Generates points uniformly over the domain defined by ``gen_specs['ub']``
     and ``gen_specs['lb']`` but requests each ``obj_component`` be evaluated
-    separately. 
+    separately.  
+    Uses (and returns) the random stream in persis_info, given by the
+    allocation function.
 
-    :Example usage: 
+    :See: 
+        ``libensemble/tests/regression_tests/test_chwirut_uniform_sampling_one_residual_at_a_time.py``
     """
     ub = gen_specs['ub']
     lb = gen_specs['lb']
@@ -71,10 +77,13 @@ def uniform_random_sample_obj_components(H,persis_info,gen_specs,_):
 
 def uniform_random_sample(H,persis_info,gen_specs,_):
     """
-    Generates points uniformly over the domain defined by ``gen_specs['ub']`` and
-    ``gen_specs['lb']``.
+    Generates ``gen_specs['gen_batch_size']`` points uniformly over the domain
+    defined by ``gen_specs['ub']`` and ``gen_specs['lb']``.
+    Uses (and returns) the random stream in persis_info, given by the
+    allocation function.
 
-    :Example usage: 
+    :See:
+        ``libensemble/tests/regression_tests/test_6-hump_camel_uniform_sampling.py``
     """
     ub = gen_specs['ub']
     lb = gen_specs['lb']
