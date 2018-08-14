fd47fc5e45b9a78e4f7c9e960ef79374adda2cf4
David Bindel <bindel@cs.cornell.edu>
2018-08-14 15:16:31 -0500

    Switch inverse_bayes_allocf to use avail_worker_ids.

--- libensemble/alloc_funcs/inverse_bayes_allocf.py
+++ libensemble/alloc_funcs/inverse_bayes_allocf.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 import numpy as np
 
 from libensemble.message_numbers import EVAL_SIM_TAG, EVAL_GEN_TAG
-
+from libensemble.alloc_funcs.support import avail_worker_ids
 
 def only_persistent_gens_for_inverse_bayes(W, H, sim_specs, gen_specs, persis_info):
     """
@@ -24,7 +24,7 @@ def only_persistent_gens_for_inverse_bayes(W, H, sim_specs, gen_specs, persis_in
 
     # If i is idle, but in persistent mode, and generated work has all returned
     # give output back to i. Otherwise, give nothing to i
-    for i in W['worker_id'][np.logical_and(W['active'] == 0, W['persis_state'] != 0)]:
+    for i in avail_worker_ids(W, persistent=True):
         inds_generated_by_i = (H['gen_worker'] == i) # if > 1 persistant generator, assign the correct work to it
         if np.all(H['returned'][inds_generated_by_i]): # Has sim_f completed everything from this persistent worker?
             # Then give back everything in the last batch
@@ -36,21 +36,21 @@ def only_persistent_gens_for_inverse_bayes(W, H, sim_specs, gen_specs, persis_in
                 H['weight'][(n*(k-1)):(n*k)] = H['weight'][(n*k):(n*(k+1))]
             Work[i] = {'persis_info': persis_info[i],
                        'H_fields': ['like'],
-                       'tag':EVAL_GEN_TAG,
+                       'tag': EVAL_GEN_TAG,
                        'libE_info': {'H_rows': np.atleast_1d(inds_to_send_back),
                                      'persistent': True}
                       }
 
-    for i in W['worker_id'][np.logical_and(W['active'] == 0, W['persis_state'] == 0)]:
+    for i in avail_worker_ids(W, persistent=False):
         # perform sim evaluations (if any point hasn't been given).
         q_inds_logical = np.logical_and(~H['given'], ~already_in_Work)
         if np.any(q_inds_logical):
-            sim_ids_to_send = np.nonzero(q_inds_logical)[0][H['subbatch'][q_inds_logical] == np.min(H['subbatch'][q_inds_logical])]
-            Work[i] = {'H_fields': sim_specs['in'], #things to evaluate
-                       'persis_info': {}, # Our sims don't need information about how points were generated
+            sims_subbatches = H['subbatch'][q_inds_logical]
+            sim_ids_to_send = np.nonzero(q_inds_logical)[0][sims_subbatches == np.min(sims_subbatches)]
+            Work[i] = {'H_fields': sim_specs['in'],
+                       'persis_info': {},
                        'tag': EVAL_SIM_TAG,
-                       'libE_info': {'H_rows': np.atleast_1d(sim_ids_to_send), #tells me what x's the returned values go with
-                                    },
+                       'libE_info': {'H_rows': np.atleast_1d(sim_ids_to_send)},
                       }
 
             already_in_Work[sim_ids_to_send] = True
