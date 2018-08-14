aba0a550964b40db46b5ce8a77bdb7f050a377bb
David Bindel <bindel@cs.cornell.edu>
2018-08-14 15:16:07 -0500

    Fix avail_worker_ids function with persistent flag.

--- libensemble/alloc_funcs/support.py
+++ libensemble/alloc_funcs/support.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 def avail_worker_ids(W, persistent=None):
     if persistent is None:
         return W['worker_id'][W['active'] == 0]
     elif persistent:
         return W['worker_id'][np.logical_and(W['active'] == 0,
-                                             W['persis_state' != 0])]
+                                             W['persis_state'] != 0)]
     else:
         return W['worker_id'][np.logical_and(W['active'] == 0,
-                                             W['persis_state' == 0])]
+                                             W['persis_state'] == 0)]
