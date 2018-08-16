6af03ad1a9dab6c73438a447b7bf821739e6125e
David Bindel <bindel@cs.cornell.edu>
2018-08-16 13:36:39 -0500

    Minor cleanup in resources.py during read-through.

--- libensemble/resources.py
+++ libensemble/resources.py
@@ -3,7 +3,6 @@ Module for detecting and returning system resources
 
 """
 
-#from mpi4py import MPI
 import os
 import socket
 import logging
@@ -19,18 +18,18 @@ class ResourcesException(Exception): pass
 class Resources:
 
     """Provide system resources to libEnsemble and job controller with knowledge of workers.
-    
+
     This is intialised when the job_controller is created with auto_resources set to True.
-    
+
     **Class Attributes:**
-    
+
     :cvar string default_nodelist_env_slurm: Default SLRUM nodelist environment variable
     :cvar string default_nodelist_env_cobalt: Default Cobal nodelist environment variable
-    
+
     **Object Attributes:**
-    
+
     These are set on initialisation.
-    
+
     :ivar string top_level_dir: Directory where searches for worker_list file.
     :ivar boolean central_mode: If true, then running in central mode, else distributed.
     :ivar string nodelist_env_slurm: Slurm environment variable giving node-list.
@@ -40,10 +39,10 @@ class Resources:
     :ivar int logical_cores_avail_per_node: Logical cores (including SMT threads) available on a node.
     :ivar int physical_cores_avail_per_node: Physical cores available on a node.
     :ivar int workerID: workerID
-    :ivar list local_nodelist: A list of all nodes assigned to this worker       
+    :ivar list local_nodelist: A list of all nodes assigned to this worker
     :ivar int local_node_count: The number of nodes available to this worker (rounded up to whole number)
     :ivar int workers_per_node: The number of workers per node (if using sub-node workers)
-    
+
     """
 
     # These can be overridden by passing in (e.g. nodelist_env_slurm) on init.
@@ -51,35 +50,35 @@ class Resources:
     default_nodelist_env_cobalt = 'COBALT_PARTNAME'
 
     def __init__(self, top_level_dir=None, workerID=None, central_mode=False,
-                 nodelist_env_slurm = None,
-                 nodelist_env_cobalt = None):
+                 nodelist_env_slurm=None,
+                 nodelist_env_cobalt=None):
         """Initialise new Resources instance
-        
+
         Parameters
         ----------
-        
+
         top_level_dir: string, optional:
             Directory libEnsemble runs in (default is current working directory)
-            
+
         workerID: int, optional:
             workerID of current process
-            
+
         central_mode, optional: boolean:
             If true, then running in central mode, else distributed.
             Central mode means libE processes (manager and workers) are grouped together and
             do not share nodes with applications. Distributed mode means Workers share nodes
             with applications.
-            
+
         nodelist_env_slurm: String, optional
             The environment variable giving a node list in Slurm format (Default: Uses SLURM_NODELIST)
             Note: This is only queried if a worker_list file is not provided and auto_resources=True.
-            
+
         nodelist_env_cobalt: String, optional
             The environment variable giving a node list in Cobalt format (Default: Uses COBALT_PARTNAME)
-            Note: This is only queried if a worker_list file is not provided and auto_resources=True.        
-        
+            Note: This is only queried if a worker_list file is not provided and auto_resources=True.
+
         """
-        
+
         self.top_level_dir = top_level_dir or os.getcwd()
         self.central_mode = central_mode
 
@@ -89,8 +88,8 @@ class Resources:
 
         #This is global nodelist avail to workers - may change to global_worker_nodelist
         self.global_nodelist = Resources.get_global_nodelist(rundir=self.top_level_dir, central_mode=self.central_mode,
-                                                             nodelist_env_slurm = self.nodelist_env_slurm,
-                                                             nodelist_env_cobalt = self.nodelist_env_cobalt)
+                                                             nodelist_env_slurm=self.nodelist_env_slurm,
+                                                             nodelist_env_cobalt=self.nodelist_env_cobalt)
         self.num_workers = Resources.get_num_workers()
         self.logical_cores_avail_per_node = Resources.get_cpu_cores(hyperthreads=True)
         self.physical_cores_avail_per_node = Resources.get_cpu_cores(hyperthreads=False)
@@ -121,7 +120,6 @@ class Resources:
     @staticmethod
     def get_num_workers():
         """Returns total number of workers"""
-        """Return the total number of workers"""
         #Will use MPI_MODE from settyings.py global - for now assume using mpi.
         #Or the function may be in some worker_concurrency module
         from mpi4py import MPI
@@ -143,12 +141,12 @@ class Resources:
     @staticmethod
     def get_MPI_variant():
         """Returns MPI base implementation
-        
+
         Returns
         -------
         mpi_variant: string:
             MPI variant 'mpich' or 'openmpi'
-        
+
         """
         # Explore mpi4py.MPI.get_vendor() and mpi4py.MPI.Get_library_version() for mpi4py
         try_mpich = subprocess.Popen(['mpirun', '-npernode'], stdout=subprocess.PIPE,
@@ -156,33 +154,29 @@ class Resources:
         stdout, _ = try_mpich.communicate()
         if 'unrecognized argument npernode' in stdout.decode():
             return 'mpich'
-        else:
-            return 'openmpi'
+        return 'openmpi'
 
     #---------------------------------------------------------------------------
-
 
     @staticmethod
     def get_slurm_nodelist(node_list_env):
         """Get global libEnsemble nodelist from the Slurm environment"""
         nidlst = []
         fullstr = os.environ[node_list_env]
-        if not len(fullstr):
+        if not fullstr:
             return []
-        splitstr = fullstr.split('-',1)
+        splitstr = fullstr.split('-', 1)
         prefix = splitstr[0]
         nidstr = splitstr[1].strip("[]")
         nidgroups = nidstr.split(',')
         for nidgroup in nidgroups:
-            if (nidgroup.find("-") != -1):
+            if nidgroup.find("-") != -1:
                 a, b = nidgroup.split("-", 1)
                 nnum_len = len(a)
                 a = int(a)
                 b = int(b)
-                if (a > b):
-                    tmp = b
-                    b = a
-                    a = tmp
+                if a > b:
+                    a, b = b, a
                 b = b + 1 #need one more for inclusive
             else:
                 a = nidgroup
@@ -197,26 +191,24 @@ class Resources:
     @staticmethod
     def get_cobalt_nodelist(node_list_env):
         """Get global libEnsemble nodelist from the Cobalt environment"""
-        prefix='nid'
+        prefix = 'nid'
         hostname = socket.gethostname()
         nnum_len = 5 # default
         if hostname.startswith(prefix):
             nnum_len = len(hostname[len(prefix):])
         nidlst = []
         nidstr = os.environ[node_list_env]
-        if not len(nidstr):
+        if not nidstr:
             return []
         nidgroups = nidstr.split(',')
         for nidgroup in nidgroups:
-            if (nidgroup.find("-") != -1):
+            if nidgroup.find("-") != -1:
                 a, b = nidgroup.split("-", 1)
                 #nnum_len = len(a)
                 a = int(a)
                 b = int(b)
-                if (a > b):
-                    tmp = b
-                    b = a
-                    a = tmp
+                if a > b:
+                    a, b = b, a
                 b = b + 1 #need one more for inclusive
             else:
                 a = int(nidgroup)
@@ -248,8 +240,8 @@ class Resources:
     #Also nodelist_env_slurm etc could just use self values.
     @staticmethod
     def get_global_nodelist(rundir=None, central_mode=False,
-                            nodelist_env_slurm = None,
-                            nodelist_env_cobalt = None):
+                            nodelist_env_slurm=None,
+                            nodelist_env_cobalt=None):
         """
         Return the list of nodes available to all libEnsemble workers
 
@@ -263,13 +255,12 @@ class Resources:
         nodelist_env_slurm  = nodelist_env_slurm  or Resources.default_nodelist_env_slurm
         nodelist_env_cobalt = nodelist_env_cobalt or Resources.default_nodelist_env_cobalt
 
-        worker_list_file = os.path.join(top_level_dir,'worker_list')
+        worker_list_file = os.path.join(top_level_dir, 'worker_list')
 
         global_nodelist = []
         if os.path.isfile(worker_list_file):
             logger.debug("worker_list found - getting nodelist from worker_list")
-            num_nodes = 0
-            with open(worker_list_file,'r') as f:
+            with open(worker_list_file, 'r') as f:
                 for line in f:
                     global_nodelist.append(line.rstrip())
         else:
@@ -293,8 +284,7 @@ class Resources:
 
         if global_nodelist:
             return global_nodelist
-        else:
-            raise ResourcesException("Error. global_nodelist is empty")
+        raise ResourcesException("Error. global_nodelist is empty")
 
 
     def get_workers_on_a_node(self):
@@ -340,11 +330,11 @@ class Resources:
         if distrib_mode and not sub_node_workers:
             #Could just read in the libe machinefile and use that - but this should match
             #Alt. create machinefile/host-list with same algorithm as best_split - future soln.
-            nodes_per_worker, remainder = divmod(num_nodes,num_workers)
+            nodes_per_worker, remainder = divmod(num_nodes, num_workers)
             if remainder != 0:
                 # Worker node may not be at head of list after truncation - should perhaps be warning or enforced
                 logger.warning("Nodes to workers not evenly distributed. Wasted nodes. {} workers and {} nodes"\
-                                .format(num_workers,num_nodes))
+                                .format(num_workers, num_nodes))
                 num_nodes = num_nodes - remainder
                 global_nodelist = global_nodelist[0:num_nodes]
 
@@ -409,10 +399,7 @@ class Resources:
         """
         try:
             import psutil
-            if hyperthreads:
-                ranks_per_node = psutil.cpu_count()
-            else:
-                ranks_per_node = psutil.cpu_count(logical=False)
+            ranks_per_node = psutil.cpu_count(logical=hyperthreads)
         except ImportError:
             #logger
             if hyperthreads:
