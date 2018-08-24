cd68f04a6bd7e2875839d2e70f0a90d15cb324dc
David Bindel <bindel@cs.cornell.edu>
2018-08-24 16:49:16 -0500

    Switched over manager tester to look at manager class.

--- libensemble/tests/unit_tests/test_manager_main.py
+++ libensemble/tests/unit_tests/test_manager_main.py
@@ -17,29 +17,29 @@ def test_term_test_1():
     #sh - Should separate the tests
     # Test 1
     hist, sim_specs, gen_specs, exit_criteria, al  = setup.hist_setup1()
-    term_test,_,_ = man.initialize(hist, sim_specs, gen_specs, al, exit_criteria, libE_specs)
-    assert not term_test(hist)
+    mgr = man.Manager(hist, libE_specs, al, sim_specs, gen_specs, exit_criteria)
+    assert not mgr.term_test()
 
 
 def test_term_test_2():
     # Test 2 - these could also be sep - with a setup or fixture....
     # Shouldn't terminate
     hist, sim_specs, gen_specs, exit_criteria, al  = setup.hist_setup2()
-    term_test,_,_ = man.initialize(hist, sim_specs, gen_specs, al, exit_criteria, libE_specs)
-    assert not term_test(hist)
+    mgr = man.Manager(hist, libE_specs, al, sim_specs, gen_specs, exit_criteria)
+    assert not mgr.term_test()
     #
     # Terminate because we've found a good 'g' value
     #H, hist.index,term_test,_,_,hist.given_count = man.initialize(sim_specs, gen_specs, al, exit_criteria,[],libE_specs)
     hist.H['g'][0] = -1
     hist.index = 1
     hist.given_count = 1
-    assert term_test(hist)
+    assert mgr.term_test()
     #
     # Terminate because everything has been given.
     #H, hist.index,term_test,_,_,hist.given_count = man.initialize(sim_specs, gen_specs, al, exit_criteria,[],libE_specs)
     hist.H['given'] = np.ones
     hist.given_count = len(hist.H)
-    assert term_test(hist)
+    assert mgr.term_test()
 
 
 def test_term_test_3():
@@ -47,13 +47,12 @@ def test_term_test_3():
     # Terminate because enough time has passed
     H0 = np.zeros(3,dtype=[('g',float)] + [('x',float),('priority',float)])
     hist, sim_specs, gen_specs, exit_criteria, al  = setup.hist_setup2(H0_in=H0)
-    term_test,_,_ = man.initialize(hist, sim_specs, gen_specs, al, exit_criteria, libE_specs)
-
+    mgr = man.Manager(hist, libE_specs, al, sim_specs, gen_specs, exit_criteria)
     hist.index = 4
     hist.H['given_time'][0] = time.time()
     time.sleep(0.5)
     hist.given_count = 4
-    assert term_test(hist)
+    assert mgr.term_test()
     #
 
 
