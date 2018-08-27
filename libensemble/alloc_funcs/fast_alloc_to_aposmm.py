7d0435a769f6940792ef1c5c723e8540d2b8ea28
David Bindel <bindel@cs.cornell.edu>
2018-08-27 11:44:15 -0500

    Put in numpy import in fast_alloc_to_apossm.

--- libensemble/alloc_funcs/fast_alloc_to_aposmm.py
+++ libensemble/alloc_funcs/fast_alloc_to_aposmm.py
@@ -1,6 +1,8 @@
 from __future__ import division
 from __future__ import absolute_import
 
+import numpy as np
+
 from libensemble.alloc_funcs.support import \
      avail_worker_ids, sim_work, gen_work, count_gens
 
