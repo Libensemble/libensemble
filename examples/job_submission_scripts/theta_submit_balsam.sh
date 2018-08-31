da5f4bd9375934dca80ecd0b21cbef436ecada29
shudson <shudson@anl.gov>
2018-08-31 18:03:11 -0500

    Update Theta submission script

--- examples/job_submission_scripts/theta_submit_balsam.sh
+++ examples/job_submission_scripts/theta_submit_balsam.sh
@@ -8,7 +8,11 @@
 
 # Script to launch libEnsemble using Balsam within Conda. Conda environment must be set up.
 
-# To be run with central job management - Manager and workers run on one node (or more if nec). Workers submit jobs to the rest of the nodes in the pool.
+# Requires Balsam is installed and a database initialized (this can be the default database).
+
+# To be run with central job management
+# - Manager and workers run on one node (or more if nec).
+# - Workers submit jobs to the rest of the nodes in the pool.
 
 # Constaint: - As set up - only uses one node (up to 63 workers) for libE. To use more, modifiy "balsam job" line to use hyper-threading and/or more than one node.
 
@@ -37,6 +41,8 @@ export LD_LIBRARY_PATH=~/.conda/envs/balsam/lib:$LD_LIBRARY_PATH
 
 export PYTHONNOUSERSITE=1 #Ensure environment isolated
 
+export PMI_NO_FORK=1 # Required for python kills on Theta
+
 # Activate conda environment
 . activate $CONDA_ENV_NAME
 
@@ -48,56 +54,43 @@ module unload xalt
 #Location of sim func script (only required if not in sys.path already)
 # export PYTHONPATH=/home/shudson/opal_work/opal/emittance_minimization/code:$PYTHONPATH
 
-#Where you want your Balam database to be
-#export BALSAM_DB_PATH=~/database_sqlite
-#export BALSAM_DB_PATH=~/database_postgres
-export BALSAM_DB_PATH=/projects/CSC250STMS07/libensemble/database_postgres
+. balsamactivate default
+# . balsamactivate my_dbase
 
-#Create a new database is not setup.
-if [[ ! -d $BALSAM_DB_PATH ]]; then
-  echo -e '\nCreating database at $BALSAM_DB_PATH'
-  #balsam init $BALSAM_DB_PATH --db-type sqlite3 #small/medium jobs
-  balsam init $BALSAM_DB_PATH --db-type postgres #big jobs (many concurrent calcs)
-fi;
 
 # Currently need atleast one DB connection per worker (for postgres).
-if [[ $NUM_WORKERS -gt 100 ]]
+if [[ $NUM_WORKERS -gt 128 ]]
 then
    #Add a margin
    echo -e "max_connections=$(($NUM_WORKERS+10)) #Appended by submission script" >> $BALSAM_DB_PATH/balsamdb/postgresql.conf
 fi
 wait
 
-#Start Balsam database server running
-# balsam dbserver --stop #In case running
-balsam dbserver --reset $BALSAM_DB_PATH
-balsam dbserver --start 
-wait
-sleep 5
-
 # Make sure no existing apps/jobs
 balsam rm apps --all --force
 balsam rm jobs --all --force
 wait
-sleep 5
+sleep 3
 
 # Add calling script to Balsam database as app and job.
 THIS_DIR=$PWD
 SCRIPT_BASENAME=${EXE%.*}
+
 balsam app --name $SCRIPT_BASENAME.app --exec $EXE --desc "Run $SCRIPT_BASENAME"
 
 # Running libE on one node - one manager and upto 63 workers
-balsam job --name job_$SCRIPT_BASENAME --workflow $WORKFLOW_NAME --application $SCRIPT_BASENAME.app --args $SCRIPT_ARGS --wall-min $LIBE_WALLCLOCK  --num-nodes 1 --ranks-per-node $((NUM_WORKERS+1)) --url-out="local:/$THIS_DIR" --stage-out-files="*.out *.dat" --url-in="local:/$THIS_DIR" --yes
+balsam job --name job_$SCRIPT_BASENAME --workflow $WORKFLOW_NAME --application $SCRIPT_BASENAME.app --args $SCRIPT_ARGS --wall-time-minutes $LIBE_WALLCLOCK --num-nodes 1 --ranks-per-node $((NUM_WORKERS+1)) --url-out="local:/$THIS_DIR" --stage-out-files="*.out *.dat" --url-in="local:/$THIS_DIR/*" --yes
+
 
 # Hyper-thread libE (note this will not affect HT status of user calcs - only libE itself)
 # Running 255 workers and one manager on one libE node.
-# balsam job --name job_$SCRIPT_BASENAME --workflow $WORKFLOW_NAME --application $SCRIPT_BASENAME.app --args $SCRIPT_ARGS --wall-min $LIBE_WALLCLOCK  --num-nodes 1 --ranks-per-node 256 --threads-per-core 4 --url-out="local:/$THIS_DIR" --stage-out-files="*.out *.dat" --url-in="local:/$THIS_DIR" --yes
+# balsam job --name job_$SCRIPT_BASENAME --workflow $WORKFLOW_NAME --application $SCRIPT_BASENAME.app --args $SCRIPT_ARGS --wall-time-minutes $LIBE_WALLCLOCK  --num-nodes 1 --ranks-per-node 256 --threads-per-core 4 --url-out="local:/$THIS_DIR/*" --stage-out-files="*.out *.dat" --url-in="local:/$THIS_DIR" --yes
 
 # Multiple nodes for libE
 # Running 127 workers and one manager - launch script on 129 nodes (if one node per worker)
-# balsam job --name job_$SCRIPT_BASENAME --workflow $WORKFLOW_NAME --application $SCRIPT_BASENAME.app --args $SCRIPT_ARGS --wall-min $LIBE_WALLCLOCK  --num-nodes 2 --ranks-per-node 64 --url-out="local:/$THIS_DIR" --stage-out-files="*.out *.dat" --url-in="local:/$THIS_DIR" --yes
+# balsam job --name job_$SCRIPT_BASENAME --workflow $WORKFLOW_NAME --application $SCRIPT_BASENAME.app --args $SCRIPT_ARGS --wall-time-minutes $LIBE_WALLCLOCK  --num-nodes 2 --ranks-per-node 64 --url-out="local:/$THIS_DIR/*" --stage-out-files="*.out *.dat" --url-in="local:/$THIS_DIR" --yes
 
 #Run job
-balsam launcher --consume-all
+balsam launcher --consume-all --job-mode=mpi --num-transition-threads=1
 
-balsam dbserver --stop
+source balsamdeactivate
