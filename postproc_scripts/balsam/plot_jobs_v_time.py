dbaad2a680cb2b50a1ea90b062e3e14686245be4
shudson <shudson@anl.gov>
2019-01-02 15:42:30 -0600

    Reformat balsam plots

--- postproc_scripts/balsam/plot_jobs_v_time.py
+++ postproc_scripts/balsam/plot_jobs_v_time.py
@@ -4,7 +4,18 @@ matplotlib.use('Agg')
 from balsam.core import models
 from matplotlib import pyplot as plt
 
+fig, ax = plt.subplots()
+
+plt.title('Balsam: Jobs completed v Time')
+plt.xlabel('Time of Day (H:M)')
+plt.ylabel('Num. Jobs Completed (Accum)')
+
 times, throughputs = models.throughput_report()
 plt.step(times, throughputs, where='post')
-plt.savefig('balsam_jobs_v_time.png')
 
+import matplotlib.dates as mdates
+myFmt = mdates.DateFormatter('%H:%M')
+ax.xaxis.set_major_formatter(myFmt)
+fig.autofmt_xdate()
+
+plt.savefig('balsam_jobs_v_time.png')
--- postproc_scripts/balsam/plot_util_v_time.py
+++ postproc_scripts/balsam/plot_util_v_time.py
@@ -4,7 +4,18 @@ matplotlib.use('Agg')
 from balsam.core import models
 from matplotlib import pyplot as plt
 
+fig, ax = plt.subplots()
+
+plt.title('Balsam Utilization: Running Jobs v Date/Time')
+plt.xlabel('Time of Day (H:M)')
+plt.ylabel('Num. Jobs Running')
+
 times, utilization = models.utilization_report()
 plt.step(times, utilization, where='post')
-plt.savefig('balsam_util_v_time.png')
 
+import matplotlib.dates as mdates
+myFmt = mdates.DateFormatter('%H:%M')
+ax.xaxis.set_major_formatter(myFmt)
+fig.autofmt_xdate()
+
+plt.savefig('balsam_util_v_time.png')
