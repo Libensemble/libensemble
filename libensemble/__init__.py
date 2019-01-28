129b542b987d13eb70c4c6eece790c0d862389be
shudson <shudson@anl.gov>
2019-01-28 17:00:55 -0600

    Update versions to 0.4.1 in setup.py, __init__.py and docs

--- libensemble/__init__.py
+++ libensemble/__init__.py
@@ -4,7 +4,7 @@ libEnsemble.
 Library for managing ensemble-like collections of computations.
 """
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __author__ = 'Jeffrey Larson, Stephen Hudson and David Bindel'
 __credits__ = 'Argonne National Laboratory'
 
