Fix units tests to work with manager-only error mode
Add test for manager-only error.
Create an object to mock MPI calls to test other error modes.

libensemble/tests/unit_tests/test_libE_main.py
@@ -9,6 +9,15 @@ from libensemble.libE import check_inputs, libE
 import libensemble.tests.unit_tests.setup as setup
 from mpi4py import MPI
 
+class Fake_MPI:
+    def Get_size(self):
+        return 2
+    def Get_rank(self):
+        return 0
+    def Barrier(self):
+        return 0    
+fake_mpi = Fake_MPI()
+
 al = {}
 libE_specs = {'comm':MPI.COMM_WORLD}
 fname_abort = 'libE_history_at_abort_0.npy'
@@ -24,7 +33,8 @@ def test_manager_exception():
         with mock.patch('libensemble.libE.comms_abort') as abortMock:
             abortMock.side_effect = Exception
             with pytest.raises(Exception, message='Expected exception'):
-                libE({'out':[('f',float)]},{'out':[('x',float)]},{'sim_max':1},libE_specs={'comm': MPI.COMM_WORLD})
+                #libE({'out':[('f',float)]},{'out':[('x',float)]},{'sim_max':1},libE_specs={'comm': MPI.COMM_WORLD})
+                libE({'out':[('f',float)]},{'out':[('x',float)]},{'sim_max':1},libE_specs={'comm': fake_mpi})
             # Check npy file dumped
             assert os.path.isfile(fname_abort), "History file not dumped"
             os.remove(fname_abort)
@@ -44,10 +54,22 @@ def test_exception_raising_manager():
 #     assert H==[]
 
 def test_checking_inputs():
-
+    
     # Don't take more points than there is space in history.
     sim_specs, gen_specs, exit_criteria = setup.make_criteria_and_specs_0()
-
+    
+    # Should fail because only got a manager
+    H0 = {}
+    libE_specs = {'comm':MPI.COMM_WORLD}
+    try:
+        check_inputs(libE_specs, al, sim_specs, gen_specs, exit_criteria,H0)
+    except AssertionError:
+        assert 1
+    else:
+        assert 0
+        
+    libE_specs={'comm': fake_mpi}
+    
     H0 = np.zeros(3,dtype=sim_specs['out'] + gen_specs['out'] + [('returned',bool)])
     # Should fail because H0 has points with 'return'==False
     try: