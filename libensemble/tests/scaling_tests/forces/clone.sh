76c2987b5998a61be8299356bbb4d62d56074e86
shudson <shudson@anl.gov>
2019-03-08 16:51:07 -0600

    Add clone.sh and summit submit script to forces test dir

---
+++ libensemble/tests/scaling_tests/forces/clone.sh
@@ -0,0 +1,13 @@
+if [[ -z $1 ]]
+then
+  echo -e "Need output dir name: ./clone NEW_DIR"
+else
+  NEWDIR=$1
+  
+  INPUTS="sim/ forces.c forces_simf.py \
+          run_libe_forces.py forces.x \
+          *.sh"
+  # Take you there if source script.
+  mkdir ../$NEWDIR && cp -rp $INPUTS ../$NEWDIR && cd ../$NEWDIR 
+fi
+
