7c8644f0185bdf4c38e65e2e2bf26995e4031f60
Jeffrey Larson <jmlarson@anl.gov>
2019-03-11 14:39:57 -0500

    Removing 2.7 in a few places

--- setup.py
+++ setup.py
@@ -64,8 +64,6 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Operating System :: Unix',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
