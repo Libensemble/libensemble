08f48853def7070e64684a849e24892b3b82ba8e
Jeffrey Larson <jmlarson@anl.gov>
2019-03-12 07:46:01 -0500

    Not running this regression test with tcp.

--- libensemble/tests/regression_tests/test_chwirut_uniform_sampling_one_residual_at_a_time.py
+++ libensemble/tests/regression_tests/test_chwirut_uniform_sampling_one_residual_at_a_time.py
@@ -17,7 +17,7 @@ from libensemble.tests.regression_tests.common import parse_args
 
 # Parse args for test code
 nworkers, is_master, libE_specs, _ = parse_args()
-if libE_specs['comms'] == 'local':
+if libE_specs['comms'] != 'mpi':
     quit()
 
 # Import libEnsemble main, sim_specs, gen_specs, alloc_specs, and persis_info
