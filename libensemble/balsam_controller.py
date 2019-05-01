d8588376c7d56d17a00ed4d11f69f8dd0a0f060a
Jeffrey Larson <jmlarson@anl.gov>
2019-05-01 10:52:28 -0500

    Flake8 for main libE routines

--- libensemble/balsam_controller.py
+++ libensemble/balsam_controller.py
@@ -10,15 +10,15 @@ from mpi4py import MPI
 
 from libensemble.mpi_resources import MPIResources
 from libensemble.controller import \
-     Job, JobControllerException, jassert, STATES
+    Job, JobControllerException, jassert, STATES
 from libensemble.mpi_controller import MPIJobController
 
 import balsam.launcher.dag as dag
 from balsam.core import models
 
 logger = logging.getLogger(__name__)
-#To change logging level for just this module
-#logger.setLevel(logging.DEBUG)
+# To change logging level for just this module
+# logger.setLevel(logging.DEBUG)
 
 
 class BalsamJob(Job):
@@ -51,7 +51,7 @@ class BalsamJob(Job):
     def calc_job_timing(self):
         """Calculate timing information for this job"""
 
-        #Get runtime from Balsam
+        # Get runtime from Balsam
         self.runtime = self.process.runtime_seconds
 
         if self.launch_time is None:
@@ -80,9 +80,9 @@ class BalsamJob(Job):
 
             if balsam_state == 'JOB_FINISHED':
                 self.state = 'FINISHED'
-            elif balsam_state == 'PARENT_KILLED': # Not currently used
+            elif balsam_state == 'PARENT_KILLED':  # Not currently used
                 self.state = 'USER_KILLED'
-            elif balsam_state in STATES: #In my states
+            elif balsam_state in STATES:  # In my states
                 self.state = balsam_state
             else:
                 logger.warning("Job finished, but in unrecognized "
@@ -105,15 +105,14 @@ class BalsamJob(Job):
                 "Job state returned from Balsam is not in known list of "
                 "Balsam states. Job state is {}".format(balsam_state))
 
-
     def kill(self, wait_time=None):
         """ Kills or cancels the supplied job """
 
         dag.kill(self.process)
 
-        #Could have Wait here and check with Balsam its killed -
-        #but not implemented yet.
-        
+        # Could have Wait here and check with Balsam its killed -
+        # but not implemented yet.
+
         logger.info("Killing job {}".format(self.name))
         self.state = 'USER_KILLED'
         self.finished = True
@@ -136,7 +135,7 @@ class BalsamJobController(MPIJobController):
 
         if not central_mode:
             logger.warning("Balsam does not currently support distributed mode - running in central mode")
-            central_mode=True
+            central_mode = True
 
         super().__init__(auto_resources, central_mode,
                          nodelist_env_slurm, nodelist_env_cobalt)
@@ -150,7 +149,7 @@ class BalsamJobController(MPIJobController):
         """Deletes all Balsam apps whose names contains .simfunc or .genfunc"""
         AppDef = models.ApplicationDefinition
 
-        #Some error handling on deletes.... is it internal
+        # Some error handling on deletes.... is it internal
         for app_type in ['.simfunc', '.genfunc']:
             deletion_objs = AppDef.objects.filter(name__contains=app_type)
             if deletion_objs:
@@ -169,11 +168,11 @@ class BalsamJobController(MPIJobController):
                     logger.debug("Deleting job {}".format(del_job.name))
                 deletion_objs.delete()
 
-        ##May be able to use union function - to combine - see queryset help.
-        ##Eg (not tested)
-        #del_simfuncs = Job.objects.filter(name__contains='.simfunc')
-        #del_genfuncs = Job.objects.filter(name__contains='.genfunc')
-        #deletion_objs = deletion_objs.union()
+        # May be able to use union function - to combine - see queryset help.
+        # Eg (not tested)
+        # del_simfuncs = Job.objects.filter(name__contains='.simfunc')
+        # del_genfuncs = Job.objects.filter(name__contains='.genfunc')
+        # deletion_objs = deletion_objs.union()
 
     @staticmethod
     def add_app(name, exepath, desc):
@@ -183,8 +182,8 @@ class BalsamJobController(MPIJobController):
         app.name = name
         app.executable = exepath
         app.description = desc
-        #app.default_preprocess = '' # optional
-        #app.default_postprocess = '' # optional
+        # app.default_preprocess = '' # optional
+        # app.default_postprocess = '' # optional
         app.save()
         logger.debug("Added App {}".format(app.name))
 
@@ -208,7 +207,7 @@ class BalsamJobController(MPIJobController):
         # OK to use Python 3 syntax (Balsam requires 3.6+)
         super().register_calc(full_path, calc_type, desc)
 
-        #Get from one place - so always matches
+        # Get from one place - so always matches
         calc_name = self.default_apps[calc_type].name
         desc = self.default_apps[calc_type].desc
 
@@ -226,75 +225,74 @@ class BalsamJobController(MPIJobController):
         """
         app = self.default_app(calc_type)
 
-        #Need test somewhere for if no breakdown supplied....
-        #or only machinefile
+        # Need test somewhere for if no breakdown supplied....
+        # or only machinefile
 
-        #Specific to this class
+        # Specific to this class
         if machinefile is not None:
             logger.warning("machinefile arg ignored - not supported in Balsam")
             jassert(num_procs or num_nodes or ranks_per_node,
                     "No procs/nodes provided - aborting")
 
+        # Set num_procs, num_nodes and ranks_per_node for this job
 
-        #Set num_procs, num_nodes and ranks_per_node for this job
+        # Without resource detection
+        # num_procs, num_nodes, ranks_per_node = JobController.job_partition(num_procs, num_nodes, ranks_per_node)  # Note: not included machinefile option
 
-        #Without resource detection
-        #num_procs, num_nodes, ranks_per_node = JobController.job_partition(num_procs, num_nodes, ranks_per_node) #Note: not included machinefile option
-
-        #With resource detection (may do only if under-specified?? though that will not tell if larger than possible
-        #for static allocation - but Balsam does allow dynamic allocation if too large!!
-        #For now allow user to specify - but default is True....
+        # With resource detection (may do only if under-specified?? though that will not tell if larger than possible
+        # for static allocation - but Balsam does allow dynamic allocation if too large!!
+        # For now allow user to specify - but default is True....
         if self.auto_resources:
             num_procs, num_nodes, ranks_per_node = \
-              self.resources.get_resources(
-                  num_procs=num_procs,
-                  num_nodes=num_nodes, ranks_per_node=ranks_per_node,
-                  hyperthreads=hyperthreads)
+                self.resources.get_resources(
+                    num_procs=num_procs,
+                    num_nodes=num_nodes, ranks_per_node=ranks_per_node,
+                    hyperthreads=hyperthreads)
         else:
-            #Without resource detection (note: not included machinefile option)
+            # Without resource detection (note: not included machinefile option)
             num_procs, num_nodes, ranks_per_node = \
-              MPIResources.job_partition(num_procs, num_nodes, ranks_per_node)
+                MPIResources.job_partition(num_procs, num_nodes, ranks_per_node)
 
-        #temp - while balsam does not accept a standard out name
+        # temp - while balsam does not accept a standard out name
         if stdout is not None or stderr is not None:
             logger.warning("Balsam does not currently accept a stdout "
                            "or stderr name - ignoring")
             stdout = None
             stderr = None
 
-        #Will be possible to override with arg when implemented
-        #(or can have option to let Balsam assign)
+        # Will be possible to override with arg when implemented
+        # (or can have option to let Balsam assign)
         default_workdir = os.getcwd()
         job = BalsamJob(app, app_args, default_workdir,
                         stdout, stderr, self.workerID)
 
-        #This is not used with Balsam for run-time as this would include wait time
-        #Again considering changing launch to submit - or whatever I chose before.....
-        job.launch_time = time.time() #Not good for timing job - as I dont know when it finishes - only poll/kill est.
+        # This is not used with Balsam for run-time as this would include wait time
+        # Again considering changing launch to submit - or whatever I chose before.....
+        job.launch_time = time.time()  # Not good for timing job - as I dont know when it finishes - only poll/kill est.
 
         add_job_args = {'name': job.name,
-                        'workflow': "libe_workflow", #add arg for this
-                        'user_workdir': default_workdir, #add arg for this
+                        'workflow': "libe_workflow",  # add arg for this
+                        'user_workdir': default_workdir,  # add arg for this
                         'application': app.name,
                         'args': job.app_args,
                         'num_nodes': num_nodes,
                         'ranks_per_node': ranks_per_node}
 
         if stage_inout is not None:
-            #For now hardcode staging - for testing
+            # For now hardcode staging - for testing
             add_job_args['stage_in_url'] = "local:" + stage_inout + "/*"
             add_job_args['stage_out_url'] = "local:" + stage_inout
             add_job_args['stage_out_files'] = "*.out"
 
         job.process = dag.add_job(**add_job_args)
-        
+
         if (wait_on_run):
             self._wait_on_run(job)
 
         logger.info("Added job to Balsam database {}: "
-                     "Worker {} nodes {} ppn {}".
-                     format(job.name, self.workerID, num_nodes, ranks_per_node))
+                    "Worker {} nodes {} ppn {}".
+                    format(job.name, self.workerID, num_nodes, ranks_per_node))
 
-        #job.workdir = job.process.working_directory #Might not be set yet!!!!
+        # job.workdir = job.process.working_directory  # Might not be set yet!!!!
         self.list_of_jobs.append(job)
         return job
