eec8cbba8067234a12f46792606339b5e315b4f3
shudson <shudson@anl.gov>
2019-05-09 23:24:56 -0500

    Add new unit tests for resources.py

--- libensemble/tests/unit_tests/test_resources.py
+++ libensemble/tests/unit_tests/test_resources.py
@@ -42,35 +42,35 @@ def test_slurm_nodelist_empty():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = ""
     exp_out = [] #empty
     nodelist = Resources.get_slurm_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_slurm_nodelist_single():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "knl-0056"
     exp_out = ["knl-0056"]
     nodelist = Resources.get_slurm_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_slurm_nodelist_knl_seq():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "knl-[0009-0012]"
     exp_out = ['knl-0009', 'knl-0010', 'knl-0011', 'knl-0012']
     nodelist = Resources.get_slurm_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_slurm_nodelist_bdw_seq():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "bdw-[0009-0012]"
     exp_out = ['bdw-0009', 'bdw-0010', 'bdw-0011', 'bdw-0012']
     nodelist = Resources.get_slurm_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_slurm_nodelist_groups():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "knl-[0020-0022,0137-0139,1234]"
     exp_out = ['knl-0020', 'knl-0021', 'knl-0022', 'knl-0137', 'knl-0138', 'knl-0139', 'knl-1234']
     nodelist = Resources.get_slurm_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_slurm_nodelist_groups_longprefix():
@@ -78,50 +78,74 @@ def test_slurm_nodelist_groups_longprefix():
     exp_out = ['super-000020', 'super-000021', 'super-000022', 'super-000137', 'super-000138', 'super-000139',
                'super-001234', 'super-023456', 'super-023457', 'super-023458']
     nodelist = Resources.get_slurm_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_slurm_nodelist_reverse_grp():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "knl-[0020-0022,0139-0137,1234]"
     exp_out = ['knl-0020', 'knl-0021', 'knl-0022', 'knl-0137', 'knl-0138', 'knl-0139', 'knl-1234']
     nodelist = Resources.get_slurm_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_cobalt_nodelist_empty():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = ""
     exp_out = [] #empty
     nodelist = Resources.get_cobalt_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_cobalt_nodelist_single():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "56"
     exp_out = ["56"]
     nodelist = Resources.get_cobalt_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_cobalt_nodelist_seq():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "9-12"
     exp_out = ['9', '10', '11', '12']
     nodelist = Resources.get_cobalt_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_cobalt_nodelist_groups():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "20-22,137-139,1234"
     exp_out = ['20', '21', '22', '137', '138', '139', '1234']
     nodelist = Resources.get_cobalt_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
 
 def test_cobalt_nodelist_reverse_grp():
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "20-22,139-137,1234"
     exp_out = ['20', '21', '22', '137', '138', '139', '1234']
     nodelist = Resources.get_cobalt_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
-    assert nodelist == exp_out, "Nodelist returned is does not match expected"
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
 
+
+def test_lsf_nodelist_empty():
+    os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = ""
+    exp_out = [] #empty
+    nodelist = Resources.get_lsf_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
+
+
+def test_lsf_nodelist_single():
+    os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = 'batch5' + ' g06n02' * 42
+    exp_out = ["g06n02"]
+    nodelist = Resources.get_lsf_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
+
+
+def test_lsf_nodelist_seq():
+    os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = 'batch5' + ' g06n02' * 42 + ' h21n18' * 42
+    exp_out = ['g06n02', 'h21n18']
+    nodelist = Resources.get_lsf_nodelist(node_list_env = "LIBE_RESOURCES_TEST_NODE_LIST")
+    assert nodelist == exp_out, "Nodelist returned does not match expected"
+
+# These dont apply to the lsf lists as they are listed in full
+# def test_lsf_nodelist_groups():
+# def test_lsf_nodelist_reverse_grp():
 
 # Tests Resources.get_global_nodelist (This requires above tests to work)
 
@@ -143,6 +167,25 @@ def test_get_global_nodelist_frm_cobalt():
     assert global_nodelist == exp_out, "global_nodelist returned does not match expected"
 
 
+def test_get_global_nodelist_frm_lsf():
+    os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = 'batch5' + ' g06n02' * 42 + ' h21n18' * 42
+    exp_out = ['g06n02', 'h21n18']
+    global_nodelist = Resources.get_global_nodelist(rundir=os.getcwd(),
+                                                    nodelist_env_slurm = "THIS_ENV_VARIABLE_IS_DEF_NOT_SET",
+                                                    nodelist_env_cobalt = "THIS_ENV_VARIABLE_IS_DEF_NOT_SET",
+                                                    nodelist_env_lsf = "LIBE_RESOURCES_TEST_NODE_LIST")
+    assert global_nodelist == exp_out, "global_nodelist returned does not match expected"
+
+
+def test_get_global_nodelist_standalone():
+    mynode = socket.gethostname()
+    global_nodelist = Resources.get_global_nodelist(rundir=os.getcwd(),
+                                                    nodelist_env_slurm = "THIS_ENV_VARIABLE_IS_DEF_NOT_SET",
+                                                    nodelist_env_cobalt = "THIS_ENV_VARIABLE_IS_DEF_NOT_SET",
+                                                    nodelist_env_lsf = "THIS_ENV_VARIABLE_IS_DEF_NOT_SET")
+    assert global_nodelist == [mynode], "global_nodelist returned does not match expected"
+
+    
 def test_get_global_nodelist_frm_wrklst_file():
     # worker_list file should override env variables
     os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "20-22,137-139,1234" # Should not be this
@@ -379,22 +422,82 @@ def test_get_local_nodelist_distrib_mode_uneven_split():
     os.remove('worker_list')
 
 
+class Fake_comm():
+    def __init__(self, nworkers):
+        self.num_workers = nworkers
+    def get_num_workers(self):
+        return self.num_workers
+
+    
+def test_worker_resources():
+    os.environ["LIBE_RESOURCES_TEST_NODE_LIST"] = "knl-[0020-0022,0036,0137-0139,1234]"
+    resources = Resources(nodelist_env_slurm = "LIBE_RESOURCES_TEST_NODE_LIST", central_mode = True)
+    
+    # One worker per node
+    exp_nodelist1 = [['knl-0020'], ['knl-0021'], ['knl-0022'], ['knl-0036'], ['knl-0137'], ['knl-0138'], ['knl-0139'], ['knl-1234']]    
+    num_workers = 8
+    comm = Fake_comm(num_workers)    
+    for wrk in range(num_workers):
+        workerID = wrk + 1
+        worker = WorkerResources(workerID, comm, resources)
+        assert worker.num_workers == 8, 'worker.num_workers does not match'
+        assert worker.workerID == workerID, 'worker.workerID does not match'
+        assert worker.local_nodelist == exp_nodelist1[wrk], 'worker.local_nodelist does not match'
+        assert worker.local_node_count == 1, 'worker.local_node_count does not match'
+        assert worker.workers_per_node == 1, 'worker.workers_per_node does not match'
+
+    # Multiple nodes per worker
+    exp_nodelist2 = [['knl-0020', 'knl-0021', 'knl-0022', 'knl-0036'], ['knl-0137', 'knl-0138', 'knl-0139', 'knl-1234']]
+    num_workers = 2
+    comm2 = Fake_comm(num_workers)
+    for wrk in range(num_workers):
+        workerID = wrk + 1
+        worker = WorkerResources(workerID, comm2, resources)
+        assert worker.num_workers == 2, 'worker.num_workers does not match'
+        assert worker.workerID == workerID, 'worker.workerID does not match'
+        assert worker.local_nodelist == exp_nodelist2[wrk], 'worker.local_nodelist does not match'
+        assert worker.local_node_count == 4, 'worker.local_node_count does not match'
+        assert worker.workers_per_node == 1, 'worker.workers_per_node does not match'
+
+    # Multiple workers per node
+    num_workers = 16
+    comm3 = Fake_comm(num_workers)
+    for wrk in range(num_workers):
+        workerID = wrk + 1
+        worker = WorkerResources(workerID, comm3, resources)
+        assert worker.num_workers == 16, 'worker.num_workers does not match'
+        assert worker.workerID == workerID, 'worker.workerID does not match'
+        assert worker.local_nodelist == exp_nodelist1[wrk//2], 'worker.local_nodelist does not match'
+        assert worker.local_node_count == 1, 'worker.local_node_count does not match'
+        assert worker.workers_per_node == 2, 'worker.workers_per_node does not match'
+        
+        
 if __name__ == "__main__":
     setup_standalone_run()
-    test_slurm_nodelist_empty
+    
+    test_slurm_nodelist_empty()
     test_slurm_nodelist_single()
     test_slurm_nodelist_knl_seq()
     test_slurm_nodelist_bdw_seq()
     test_slurm_nodelist_groups()
     test_slurm_nodelist_groups_longprefix()
     test_slurm_nodelist_reverse_grp()
-    test_cobalt_nodelist_empty
+    
+    test_cobalt_nodelist_empty()
     test_cobalt_nodelist_single()
     test_cobalt_nodelist_seq()
     test_cobalt_nodelist_groups()
     test_cobalt_nodelist_reverse_grp()
+    
+    test_lsf_nodelist_empty()
+    test_lsf_nodelist_single()
+    test_lsf_nodelist_seq()
+    
     test_get_global_nodelist_frm_slurm()
     test_get_global_nodelist_frm_cobalt()
+    test_get_global_nodelist_frm_lsf()
+    test_get_global_nodelist_standalone()
+    
     test_get_global_nodelist_frm_wrklst_file()
     test_remove_libE_nodes()
     test_get_local_nodelist_central_mode()
@@ -402,4 +505,6 @@ if __name__ == "__main__":
     test_get_local_nodelist_distrib_mode_host_not_in_list()
     test_get_local_nodelist_distrib_mode()
     test_get_local_nodelist_distrib_mode_uneven_split()
+
+    test_worker_resources()    
     teardown_standalone_run()
