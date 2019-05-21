90ae1eb4f85905e4550965c11c55034d590131e3
shudson <shudson@anl.gov>
2019-05-21 16:44:11 -0500

    Remove dependencies

--- setup.py
+++ setup.py
@@ -48,9 +48,9 @@ setup(
 
     package_data={'libensemble.sim_funcs.branin': ['known_minima_and_func_values']},
 
-    install_requires=['mpi4py>=2.0',
-                      'numpy'
-                      ],
+    #install_requires=['mpi4py>=2.0',
+                      #'numpy'
+                      #],
 
     # If run tests through setup.py - downloads these but does not install
     tests_require=['pytest>=3.1',
