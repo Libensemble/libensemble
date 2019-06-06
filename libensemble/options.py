81071c2a2a74b05946918d6ae966e1a9c4d58979
John-Luke Navarro <johnluke@andrews.edu>
2019-06-06 16:28:26 -0500

    Initial commit. Started Options class for managing libEnsemble's global options in a dictionary. Started unit tests for Options.

---
+++ libensemble/options.py
@@ -0,0 +1,58 @@
+import os
+import logging
+from libensemble.comms.logs import LogConfig
+
+class Options:
+    """The Options Class provides methods for managing the global options dictionary.
+    Also contains method for returning relevant attributes in the form of libE_specs for compatibility.
+    Can receive new options as keyword arguments, a single dictionary, or multiple dictionaries.
+
+
+    Attributes
+    ----------
+    options: dictionary
+        options dictionary storing global options for libEnsemble
+    """
+
+    def __init__(self, *args, **kwargs):
+        """ Initializes options to any combination of dictionaries or keywords """
+        self.options = {}
+        for arg in args:
+            self.options.update(arg)
+        self.options.update(kwargs)
+
+    def __str__(self):
+        return str(self.options)
+
+    def set_logging_level(self, level):
+        """FROM libensemble.comms.logs. NEEDS TESTING"""
+        numeric_level = getattr(logging, level.upper(), 10)
+        self.log_level = numeric_level
+        if self.logger_set:
+            logger = logging.getLogger(self.name)
+            logger.setLevel(self.log_level)
+
+    def get_options(self, *opts):
+        if not opts:
+            return self.options
+        elif len(opts) == 1:
+            value = self.options.get(opts[0])
+            assert 'None' not in value, 'Requested option not found'
+            return value
+        else:
+            self.ret_opts = {}
+            for arg in opts:
+                self.ret_opts.update({arg: self.options.get(arg)})
+            assert None not in self.ret_opts.values(), 'Requested option not found'
+            return self.ret_opts
+
+    def set_options(self, new, **kwargs):
+        assert type(new) == 'dict'
+        self.options.update(new) if new \
+        else self.options.update(**kwargs)
+
+    def as_libE_specs():
+        pass
+
+    def to_python():
+        pass
---
+++ libensemble/tests/unit_tests/test_options.py
@@ -0,0 +1,54 @@
+# Tests ==============
+from libensemble.options import Options
+import logging
+
+def test_init():
+    topt = Options({'comms':'mpi'}, {'factor': 2}, logging='info')
+    assert topt
+
+def test_to_str():
+    topt = Options(logging='info')
+
+    assert str(topt) == "{'logging': 'info'}"
+
+def test_get_options_1():
+    topt = Options({'comms':'mpi'}, {'factor': 2}, logging='info')
+
+    assert topt.get_options() == {'comms': 'mpi', \
+                                'factor': 2, \
+                                'logging': 'info'}
+
+def test_get_options_2():
+    topt1 = Options({'comms': 'mpi'})
+    topt2 = Options(comms='mpi')
+
+    assert topt1.get_options('comms') == topt2.get_options('comms') == 'mpi'
+
+def test_get_options_3():
+    topt1 = Options({'comms': 'mpi'}, {'nworkers': 4}, logging='info')
+
+    assert topt1.get_options('comms', 'logging') == {'comms': 'mpi', \
+                                                    'logging': 'info'}
+
+def test_set_logging():
+    pass
+
+def test_set_option_1():
+    pass
+
+def test_set_option_2():
+    pass
+
+def as_libE_specs():
+    pass
+
+def to_python():
+    pass
+    
+if __name__ == '__main__':
+    test_init()
+    test_to_str()
+    test_get_options_1()
+    test_get_options_2()
+    test_get_options_3()
+    print('Passed')
