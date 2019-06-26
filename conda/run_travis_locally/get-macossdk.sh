07af6f353337985267b139a6ed5477a1b2510cf9
jlnav <johnlukenavarro@icloud.com>
2019-06-26 10:08:14 -0500

    updated debugging and config scripts

--- conda/run_travis_locally/get-macossdk.sh
+++ conda/run_travis_locally/get-macossdk.sh
@@ -1,2 +1,3 @@
 wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
 sudo tar xvf MacOSX10.9.sdk.tar.xz -C /opt
+export CONDA_BUILD_SYSROOT=/opt/MacOSX10.9.sdk
