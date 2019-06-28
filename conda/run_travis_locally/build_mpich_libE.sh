ebc7b2855ac0e9c59701ff8494bd7bb8ccc529e8
shudson <shudson@anl.gov>
2019-06-28 13:08:00 -0500

    Try remove no-update-deps on scipy and mpi4py

--- conda/run_travis_locally/build_mpich_libE.sh
+++ conda/run_travis_locally/build_mpich_libE.sh
@@ -62,7 +62,7 @@ conda install $MPI || return
 #conda install numpy || return #scipy includes numpy
 conda install --no-update-deps scipy || return
 conda install --no-update-deps  mpi4py || return
-conda install mumps-mpi=5.1.2=h5bebb2f_1007y || return # Force this version
+conda install mumps-mpi=5.1.2=h5bebb2f_1007 || return # Force this version
 conda install petsc4py petsc || return
 conda install --no-update-deps nlopt || return
 
