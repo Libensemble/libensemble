1ee1bf0c5a85214351d65943296330f3b545d106
jlnav <johnlukenavarro@icloud.com>
2019-07-01 16:59:33 -0500

    Convert string representations back to dict for consistent comparision across versions.

--- libensemble/tests/unit_tests/test_options.py
+++ libensemble/tests/unit_tests/test_options.py
@@ -1,5 +1,6 @@
 # Tests ==============
 from libensemble.options import GlobalOptions as go
+import ast
 
 
 def test_init():
@@ -8,8 +9,8 @@ def test_init():
 
 
 def test_to_str():
-    assert str(go.current_options()) == "{'comms': 'local', 'nprocesses': 4, 'logging': 'info'}"
-
+    assert ast.literal_eval(str(go.current_options())) == ast.literal_eval("{'comms': 'local', 'nprocesses': 4, 'logging': 'info'}")
+    # Prevent errors based on elements becoming unordered when becoming string
 
 def test_get_1():
     assert go.current_options().get() == {'comms': 'local', 'nprocesses': 4, 'logging': 'info'}
