d637a1c742021548decc7aefce1b6b99f4a6df2d
jlnav <johnlukenavarro@icloud.com>
2019-07-15 15:53:35 -0500

    Removed debugging files

--- conda/run_travis_locally/get-macossdk.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
-sudo tar xvf MacOSX10.9.sdk.tar.xz -C /opt
-export CONDA_BUILD_SYSROOT=/opt/MacOSX10.9.sdk
