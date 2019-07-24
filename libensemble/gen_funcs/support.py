9e8d5103bb61a8eca02973b234b835a275375cdc
Kaushik Kulkarni <kaushikcfd@gmail.com>
2019-07-24 09:17:17 -0500

    some more debug statements about send/receive

--- libensemble/gen_funcs/support.py
+++ libensemble/gen_funcs/support.py
@@ -1,4 +1,5 @@
 from libensemble.message_numbers import STOP_TAG, PERSIS_STOP, UNSET_TAG, EVAL_GEN_TAG
+import sys
 
 
 def sendrecv_mgr_worker_msg(comm, O, status=None):
@@ -11,6 +12,8 @@ def sendrecv_mgr_worker_msg(comm, O, status=None):
 def send_mgr_worker_msg(comm, O):
     """Send message from worker to manager.
     """
+    assert len(O) == 1
+    print(25*"-", "Sending {}".format(O[0][0]), 25*"-", flush=True)
     D = {'calc_out': O,
          'libE_info': {'persistent': True},
          'calc_status': UNSET_TAG,
@@ -22,9 +25,14 @@ def send_mgr_worker_msg(comm, O):
 def get_mgr_worker_msg(comm, status=None):
     """Get message to worker from manager.
     """
+    print('[Parent]: Expecting a message from Manager...', flush=True)
     tag, Work = comm.recv()
+    print('[Parent]: Received a tag {}'.format(tag), flush=True)
+    sys.stdout.flush()
     if tag in [STOP_TAG, PERSIS_STOP]:
         comm.push_back(tag, Work)
         return tag, None, None
+    print('[Parent]: Expecting a message from Manager...', flush=True)
     _, calc_in = comm.recv()
+    print('[Parent]: Received some message.', flush=True)
     return tag, Work, calc_in
