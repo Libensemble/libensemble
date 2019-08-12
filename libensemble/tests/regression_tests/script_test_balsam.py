3aa4cebff5d66306e2cce58f12924cce2f4364c1
jlnav <jnavarro@anl.gov>
2019-08-12 10:17:40 -0500

    balsam-specific sim func added

--- libensemble/tests/regression_tests/script_test_balsam.py
+++ libensemble/tests/regression_tests/script_test_balsam.py
@@ -7,11 +7,12 @@ import multiprocessing
 from libensemble.balsam_controller import BalsamJobController
 from libensemble.message_numbers import WORKER_DONE, WORKER_KILL_ON_ERR, WORKER_KILL_ON_TIMEOUT, JOB_FAILED
 from libensemble.libE import libE
-from libensemble.sim_funcs.job_control_hworld import job_control_hworld as sim_f
+from libensemble.sim_funcs.job_control_hworld_balsam import job_control_hworld as sim_f
 from libensemble.gen_funcs.uniform_sampling import uniform_random_sample as gen_f
 from libensemble.tests.regression_tests.common import per_worker_stream
 
 import mpi4py
+mpi4py.rc.recv_mprobe = False  # Disable matching probes
 from mpi4py import MPI
 
 
