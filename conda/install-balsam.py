68919a4bef99ed6e24e24f2d2de57fbb07385d3e
jlnav <jnavarro@anl.gov>
2019-08-13 12:13:44 -0500

    fixes version check

--- conda/install-balsam.py
+++ conda/install-balsam.py
@@ -4,11 +4,11 @@ import subprocess
 
 balsamclone = 'git clone https://github.com/balsam-alcf/balsam.git ../balsam'
 
-if sys.version[2] >= 6:
+if int(sys.version[2]) >= 6:
     subprocess.check_call(balsamclone.split())
     os.chdir('../balsam')
     subprocess.check_call('pip install -e .'.split())
     os.chdir('../libensemble')
-    os.rename('./conda/test_balsam.py', 
+    os.rename('./conda/test_balsam.py',
               './libensemble/tests/regression_tests/test_balsam.py')
     subprocess.check_call('./conda/configure-balsam-test.sh')
