e35e4d7e89849bee0df13bdcf84965ee128ee8d9
jlnav <jnavarro@anl.gov>
2019-08-16 09:35:56 -0500

    trying additional timing adjustment for timeout

--- libensemble/tests/run-tests.sh
+++ libensemble/tests/run-tests.sh
@@ -378,7 +378,7 @@ if [ "$root_found" = true ]; then
 
     TIMEOUT=""
     if [ -x "$(command -v timeout)" ] ; then
-        TIMEOUT="timeout 90s"
+        TIMEOUT="timeout 120s"
     fi
     #Build any sim/gen source code dependencies here .....
 
