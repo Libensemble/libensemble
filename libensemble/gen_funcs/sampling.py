642e6cc0367e51406bdae588aa64bb38444e6feb
Jeffrey Larson <jmlarson@anl.gov>
2019-10-30 13:30:02 -0500

    Moving all user gen_specs references to use gen_specs['user'] or dereferencing to user_specs = gen_specs['user']

--- libensemble/gen_funcs/sampling.py
+++ libensemble/gen_funcs/sampling.py
@@ -8,19 +8,19 @@ import numpy as np
 
 def uniform_random_sample_with_different_nodes_and_ranks(H, persis_info, gen_specs, _):
     """
-    Generates points uniformly over the domain defined by ``gen_specs['ub']`` and
-    ``gen_specs['lb']``. Also randomly requests a different ``number_of_nodes``
+    Generates points uniformly over the domain defined by ``gen_specs['user']['ub']`` and
+    ``gen_specs['user']['lb']``. Also randomly requests a different ``number_of_nodes``
     and ``ranks_per_node`` to be used in the evaluation of the generated point.
 
     :See:
         ``libensemble/tests/regression_tests/test_6-hump_camel_with_different_nodes_uniform_sample.py``
     """
-    ub = gen_specs['ub']
-    lb = gen_specs['lb']
+    ub = gen_specs['user']['ub']
+    lb = gen_specs['user']['lb']
     n = len(lb)
 
     if len(H) == 0:
-        b = gen_specs['initial_batch_size']
+        b = gen_specs['user']['initial_batch_size']
 
         O = np.zeros(b, dtype=gen_specs['out'])
         for i in range(0, b):
@@ -33,8 +33,8 @@ def uniform_random_sample_with_different_nodes_and_ranks(H, persis_info, gen_spe
     else:
         O = np.zeros(1, dtype=gen_specs['out'])
         O['x'] = len(H)*np.ones(n)
-        O['num_nodes'] = np.random.randint(1, gen_specs['max_num_nodes']+1)
-        O['ranks_per_node'] = np.random.randint(1, gen_specs['max_ranks_per_node']+1)
+        O['num_nodes'] = np.random.randint(1, gen_specs['user']['max_num_nodes']+1)
+        O['ranks_per_node'] = np.random.randint(1, gen_specs['user']['max_ranks_per_node']+1)
         O['priority'] = 10*O['num_nodes']
 
     return O, persis_info
@@ -42,19 +42,19 @@ def uniform_random_sample_with_different_nodes_and_ranks(H, persis_info, gen_spe
 
 def uniform_random_sample_obj_components(H, persis_info, gen_specs, _):
     """
-    Generates points uniformly over the domain defined by ``gen_specs['ub']``
-    and ``gen_specs['lb']`` but requests each ``obj_component`` be evaluated
+    Generates points uniformly over the domain defined by ``gen_specs['user']['ub']``
+    and ``gen_specs['user']['lb']`` but requests each ``obj_component`` be evaluated
     separately.
 
     :See:
         ``libensemble/tests/regression_tests/test_chwirut_uniform_sampling_one_residual_at_a_time.py``
     """
-    ub = gen_specs['ub']
-    lb = gen_specs['lb']
+    ub = gen_specs['user']['ub']
+    lb = gen_specs['user']['lb']
 
     n = len(lb)
-    m = gen_specs['components']
-    b = gen_specs['gen_batch_size']
+    m = gen_specs['user']['components']
+    b = gen_specs['user']['gen_batch_size']
 
     O = np.zeros(b*m, dtype=gen_specs['out'])
     for i in range(0, b):
@@ -71,17 +71,17 @@ def uniform_random_sample_obj_components(H, persis_info, gen_specs, _):
 
 def uniform_random_sample(H, persis_info, gen_specs, _):
     """
-    Generates ``gen_specs['gen_batch_size']`` points uniformly over the domain
-    defined by ``gen_specs['ub']`` and ``gen_specs['lb']``.
+    Generates ``gen_specs['user']['gen_batch_size']`` points uniformly over the domain
+    defined by ``gen_specs['user']['ub']`` and ``gen_specs['user']['lb']``.
 
     :See:
         ``libensemble/tests/regression_tests/test_6-hump_camel_uniform_sampling.py``
     """
-    ub = gen_specs['ub']
-    lb = gen_specs['lb']
+    ub = gen_specs['user']['ub']
+    lb = gen_specs['user']['lb']
 
     n = len(lb)
-    b = gen_specs['gen_batch_size']
+    b = gen_specs['user']['gen_batch_size']
 
     O = np.zeros(b, dtype=gen_specs['out'])
 
@@ -92,15 +92,15 @@ def uniform_random_sample(H, persis_info, gen_specs, _):
 
 def latin_hypercube_sample(H, persis_info, gen_specs, _):
     """
-    Generates ``gen_specs['gen_batch_size']`` in a Latin hypercube sample over
-    the domain defined by ``gen_specs['ub']`` and ``gen_specs['lb']``.
+    Generates ``gen_specs['user']['gen_batch_size']`` in a Latin hypercube sample over
+    the domain defined by ``gen_specs['user']['ub']`` and ``gen_specs['user']['lb']``.
     """
 
-    ub = gen_specs['ub']
-    lb = gen_specs['lb']
+    ub = gen_specs['user']['ub']
+    lb = gen_specs['user']['lb']
 
     n = len(lb)
-    b = gen_specs['gen_batch_size']
+    b = gen_specs['user']['gen_batch_size']
 
     O = np.zeros(b, dtype=gen_specs['out'])
 
