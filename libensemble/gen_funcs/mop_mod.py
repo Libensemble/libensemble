0a2a3e9d911fc54fe59000dab4f060997684f075
Tyler Chang <thchang@vt.edu>
2019-12-02 08:00:03 -0600

    added a debug statement for error

--- libensemble/gen_funcs/mop_mod.py
+++ libensemble/gen_funcs/mop_mod.py
@@ -68,7 +68,7 @@ def mop_mod_gen(H, persis_info, gen_specs, _):
         fp2.write_record(np.int32(d), np.int32(p))
         for i in range(n):
             if (np.float64((H['f'][i,:]) == np.zeros(p)).all()):
-                continue
+                print('here')
             else:
                 fp2.write_record(np.float64(H['x'][i, :]), np.float64(H['f'][i, :]))
         fp2.close()
