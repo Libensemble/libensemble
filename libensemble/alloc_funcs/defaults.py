d66f2cb4deb787e9416148659cf68ee3ec7fb3f5
jlnav <jnavarro@anl.gov>
2020-01-27 12:04:40 -0600

    move default alloc_specs from libE() definition to alloc_funcs.defaults

---
+++ libensemble/alloc_funcs/defaults.py
@@ -0,0 +1,5 @@
+from libensemble.alloc_funcs.give_sim_work_first import give_sim_work_first
+
+alloc_specs = {'alloc_f': give_sim_work_first,
+               'out': [('allocated', bool)],
+               'user': {'batch_mode': True, 'num_active_gens': 1}}
