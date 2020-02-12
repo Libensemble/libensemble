fea56d5db80fb00c784d63db030a6aa23a8e1428
jlnav <jnavarro@anl.gov>
2020-02-12 14:26:12 -0600

    plotting jobs -> tasks

--- postproc_scripts/balsam/plot_tasks_v_time.py
+++ postproc_scripts/balsam/plot_tasks_v_time.py
@@ -6,9 +6,9 @@ from matplotlib import pyplot as plt
 
 fig, ax = plt.subplots()
 
-plt.title('Balsam: Jobs completed v Time')
+plt.title('Balsam: Tasks completed v Time')
 plt.xlabel('Time of Day (H:M)')
-plt.ylabel('Num. Jobs Completed (Accum)')
+plt.ylabel('Num. Tasks Completed (Accum)')
 
 times, throughputs = models.throughput_report()
 plt.step(times, throughputs, where='post')
@@ -18,4 +18,4 @@ myFmt = mdates.DateFormatter('%H:%M')
 ax.xaxis.set_major_formatter(myFmt)
 fig.autofmt_xdate()
 
-plt.savefig('balsam_jobs_v_time.png')
+plt.savefig('balsam_tasks_v_time.png')
