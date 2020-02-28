594acf3499dd5e9ba4ab43a70facac19c0b16445
Jeffrey Larson <jmlarson@anl.gov>
2020-02-28 08:36:34 -0600

    Showing an example of what bug

--- libensemble/tests/regression_tests/test_evaluate_existing_sample.py
+++ libensemble/tests/regression_tests/test_evaluate_existing_sample.py
@@ -22,33 +22,34 @@ from libensemble.sim_funcs.borehole import borehole as sim_f, gen_borehole_input
 from libensemble.alloc_funcs.give_pregenerated_work import give_pregenerated_sim_work as alloc_f
 from libensemble.tools import parse_args, save_libE_output
 
-nworkers, is_master, libE_specs, _ = parse_args()
+for jjj in range(2):
+    nworkers, is_master, libE_specs, _ = parse_args()
 
-sim_specs = {'sim_f': sim_f, 'in': ['x'], 'out': [('f', float)]}
+    sim_specs = {'sim_f': sim_f, 'in': ['x'], 'out': [('f', float)]}
 
-gen_specs = {}
+    gen_specs = {}
 
-n_samp = 1000
-n = 8
+    n_samp = 1000
+    n = 8
 
-H0 = np.zeros(n_samp, dtype=[('x', float, 8), ('sim_id', int)])
+    H0 = np.zeros(n_samp, dtype=[('x', float, 8), ('sim_id', int)])
 
-np.random.seed(0)
-H0['x'] = gen_borehole_input(n_samp)
-H0['sim_id'] = range(n_samp)
+    np.random.seed(0)
+    H0['x'] = gen_borehole_input(n_samp)
+    H0['sim_id'] = range(n_samp)
 
-alloc_specs = {'alloc_f': alloc_f, 'out': [('x', float, n)]}
+    alloc_specs = {'alloc_f': alloc_f, 'out': [('x', float, n)]}
 
-exit_criteria = {'sim_max': len(H0)}
+    exit_criteria = {'sim_max': len(H0)}
 
-# Perform the run
-H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria,
-                            alloc_specs=alloc_specs, libE_specs=libE_specs,
-                            H0=H0)
+    # Perform the run
+    H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria,
+                                alloc_specs=alloc_specs, libE_specs=libE_specs,
+                                H0=H0)
 
-if is_master:
-    assert len(H) == len(H0)
-    assert np.array_equal(H0['x'], H['x'])
-    assert np.all(H['returned'])
-    print("\nlibEnsemble correctly didn't add anything to initial sample")
-    save_libE_output(H, persis_info, __file__, nworkers)
+    if is_master:
+        assert len(H) == len(H0)
+        assert np.array_equal(H0['x'], H['x'])
+        assert np.all(H['returned'])
+        print("\nlibEnsemble correctly didn't add anything to initial sample")
+        save_libE_output(H, persis_info, __file__, nworkers)
