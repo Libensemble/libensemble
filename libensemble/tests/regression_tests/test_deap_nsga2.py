081b10e01aedca60513be3a12399a4f84c2d8327
Jeffrey Larson <jmlarson@anl.gov>
2020-03-26 12:29:49 -0500

    Slightly relaxing localopt identification

--- libensemble/tests/regression_tests/test_deap_nsga2.py
+++ libensemble/tests/regression_tests/test_deap_nsga2.py
@@ -82,4 +82,4 @@ if is_master:
     script_name = os.path.splitext(os.path.basename(__file__))[0]
     assert flag == 0, script_name + " didn't exit correctly"
     assert sum(H['returned']) >= exit_criteria['sim_max'], script_name + " didn't evaluate the sim_max points."
-    assert min(H['fitness_values']) <= -1.0316, script_name + " didn't find the global minimum for this problem."
+    assert min(H['fitness_values']) <= -1.0315, script_name + " didn't find the global minimum for this problem."
