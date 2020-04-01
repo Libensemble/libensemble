69335f6484e83c6a0e8962f2f635f5285dedab8a
Jeffrey Larson <jmlarson@anl.gov>
2020-04-01 16:01:24 -0500

    Updating calling script

--- libensemble/sim_funcs/warpX/run_libE_on_warpX.py
+++ libensemble/sim_funcs/warpX/run_libE_on_warpX.py
@@ -13,106 +13,71 @@ from forces_simf import run_forces  # Sim func from current dir
 
 # Import libEnsemble modules
 from libensemble.libE import libE
+from libensemble.gen_funcs.persistent_aposmm import aposmm as gen_f
+from libensemble.alloc_funcs.persistent_aposmm_alloc import persistent_aposmm_alloc as alloc_f
 from libensemble.tools import parse_args, save_libE_output, add_unique_random_streams
 from libensemble import libE_logger
-
-USE_BALSAM = False
-PERSIS_GEN = False
-
-if PERSIS_GEN:
-    from libensemble.gen_funcs.persistent_uniform_sampling import persistent_uniform as gen_f
-    from libensemble.alloc_funcs.start_only_persistent import only_persistent_gens as alloc_f
-else:
-    from libensemble.gen_funcs.sampling import uniform_random_sample as gen_f
-    from libensemble.alloc_funcs.give_sim_work_first import give_sim_work_first as alloc_f
 
 libE_logger.set_level('INFO')  # INFO is now default
 
 nworkers, is_master, libE_specs, _ = parse_args()
 
-if is_master:
-    print('\nRunning with {} workers\n'.format(nworkers))
-
-sim_app = os.path.join(os.getcwd(), 'forces.x')
+sim_app = os.path.join(os.getcwd(), 'warpX.x')
 
 # Normally would be pre-compiled
-if not os.path.isfile('forces.x'):
-    if os.path.isfile('build_forces.sh'):
+if not os.path.isfile('warpX.x'):
+    if os.path.isfile('build_warpX.sh'):
         import subprocess
-        subprocess.check_call(['./build_forces.sh'])
+        subprocess.check_call(['./build_warpX.sh'])
 
 # Normally the sim_input_dir will exist with common input which is copied for each worker. Here it starts empty.
-# Create if no ./sim dir. See libE_specs['sim_input_dir']
-os.makedirs('./sim', exist_ok=True)
+# Create if no ./warpX dir. See libE_specs['sim_input_dir']
+os.makedirs('./warpX', exist_ok=True)
 
-# Create executor and register sim to it.
-if USE_BALSAM:
-    from libensemble.executors.balsam_executor import BalsamMPIExecutor
-    exctr = BalsamMPIExecutor()  # Use allow_oversubscribe=False to prevent oversubscription
-else:
-    from libensemble.executors.mpi_executor import MPIExecutor
-    exctr = MPIExecutor()  # Use allow_oversubscribe=False to prevent oversubscription
+n = 5  # Problem dimension
+from libensemble.executors.mpi_executor import MPIExecutor
+exctr = MPIExecutor()  # Use allow_oversubscribe=False to prevent oversubscription
 exctr.register_calc(full_path=sim_app, calc_type='sim')
 
-# Note: Attributes such as kill_rate are to control forces tests, this would not be a typical parameter.
-
 # State the objective function, its arguments, output, and necessary parameters (and their sizes)
-sim_specs = {'sim_f': run_forces,         # Function whose output is being minimized
+sim_specs = {'sim_f': run_warpX,          # Function whose output is being minimized
              'in': ['x'],                 # Name of input for sim_f
-             'out': [('energy', float)],  # Name, type of output from sim_f
-             'user': {'simdir_basename': 'forces',
-                      'keys': ['seed'],
+             'out': [('f', float)],       # Name, type of output from sim_f
+             'user': {'simdir_basename': 'warpX',
                       'cores': 2,
-                      'sim_particles': 1e3,
-                      'sim_timesteps': 5,
-                      'sim_kill_minutes': 10.0,
-                      'particle_variance': 0.2,
-                      'kill_rate': 0.5}
+                      'sim_particles': 1e3}
              }
 # end_sim_specs_rst_tag
 
+gen_out = [('x', float, n), ('x_on_cube', float, n), ('sim_id', int),
+           ('local_min', bool), ('local_pt', bool)]
 # State the generating function, its arguments, output, and necessary parameters.
 gen_specs = {'gen_f': gen_f,                  # Generator function
-             'in': ['sim_id'],                # Generator input
-             'out': [('x', float, (1,))],     # Name, type and size of data produced (must match sim_specs 'in')
-             'user': {'lb': np.array([0]),             # Lower bound for random sample array (1D)
-                      'ub': np.array([32767]),         # Upper bound for random sample array (1D)
-                      'gen_batch_size': 1000,          # How many random samples to generate in one call
+             'in': [],                        # Generator input
+             'out': gen_out,
+             'user': {'initial_sample_size': 100,
+                      'localopt_method': 'LN_BOBYQA',
+                      'xtol_abs': 1e-6,
+                      'ftol_abs': 1e-6,
+                      'lb': np.zeros(n),           # Lower bound for the n parameters
+                      'ub': 10*np.ones(n),         # Upper bound for the n parameters
                       }
              }
 
-alloc_specs = {'alloc_f': alloc_f,
-               'out': [('allocated', bool)],
-               'user': {'batch_mode': True,    # If true wait for all sims to process before generate more
-                        'num_active_gens': 1}  # Only one active generator at a time
-               }
+alloc_specs = {'alloc_f': alloc_f, 'out': [('given_back', bool)], 'user': {}}
 
-if PERSIS_GEN:
-    alloc_specs = {'alloc_f': alloc_f, 'out': [('given_back', bool)]}
-else:
-    alloc_specs = {'alloc_f': alloc_f,
-                   'out': [('allocated', bool)],
-                   'user': {'batch_mode': True,    # If true wait for all sims to process before generate more
-                            'num_active_gens': 1}  # Only one active generator at a time
-                   }
-
-libE_specs['save_every_k_gens'] = 1000  # Save every K steps
-libE_specs['sim_input_dir'] = './sim'   # Sim dir to be copied for each worker
-libE_specs['profile_worker'] = False    # Whether to have libE profile on (default False)
-libE_specs['use_worker_dirs'] = False   # Whether to create separate worker directories (default False)
+libE_specs['save_every_k_sims'] = 1     # Save each simulation evaluation
+libE_specs['sim_input_dir'] = './warpX' # Sim dir to be copied for each worker
 
 # Maximum number of simulations
 sim_max = 8
 exit_criteria = {'sim_max': sim_max}
 
 # Create a different random number stream for each worker and the manager
-persis_info = {}
-persis_info = add_unique_random_streams(persis_info, nworkers + 1)
+persis_info = add_unique_random_streams({}, nworkers + 1)
 
 H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria,
-                            persis_info=persis_info,
-                            alloc_specs=alloc_specs,
-                            libE_specs=libE_specs)
+                            persis_info, alloc_specs, libE_specs)
 
 # Save results to numpy file
 if is_master:
