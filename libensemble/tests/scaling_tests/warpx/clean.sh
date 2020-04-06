2dbb722735762254b537a7613b43c1211ab8356b
MaxThevenet <mthevenet@lbl.gov>
2020-04-06 09:04:52 -0700

    better clean

--- libensemble/tests/scaling_tests/warpx/clean.sh
+++ libensemble/tests/scaling_tests/warpx/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rm -rf __pycache__ ensemble libE_stats.txt ensemble run_libE_on_warpX_*_history_* run_libE_on_warpX_*_persis_info_* *~ ensemble.log
+rm -rf __pycache__ ensemble libE_stats.txt ensemble run_libE_on_warpX_*_history_* run_libE_on_warpX_*_persis_info_* *~ ensemble.log libE_history_for_run* libE_persis_info* libE_history_*
