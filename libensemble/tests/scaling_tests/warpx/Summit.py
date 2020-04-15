46c53367845d422e736234e101349401e2869eb7
Jeffrey Larson <jmlarson@anl.gov>
2020-04-14 21:13:23 -0500

    Syntax error

--- libensemble/tests/scaling_tests/warpx/Summit.py
+++ libensemble/tests/scaling_tests/warpx/Summit.py
@@ -4,6 +4,6 @@ machine_specs = \
         'sim_app': '$HOME/warpx/Bin/main2d.gnu.TPROF.MPI.CUDA.ex',
         'nodes': 1,
         'ranks_per_node': 6,
-        'extra_args': '-n 1 -a 2 -g 2 -c 2 --bind=packed:1 --smpiargs="-gpu"'
+        'extra_args': '-n 1 -a 2 -g 2 -c 2 --bind=packed:1 --smpiargs="-gpu"',
         'OMP_NUM_THREADS': '1',
     }
