303b337c8ea1e1246d95729d9c104b6a0d3fd2e4
jlnav <jnavarro@anl.gov>
2020-04-15 15:38:37 -0500

    try adding sleep

--- libensemble/tests/regression_tests/test_sim_dirs_with_exception.py
+++ libensemble/tests/regression_tests/test_sim_dirs_with_exception.py
@@ -16,6 +16,7 @@
 
 import numpy as np
 import os
+import time
 
 from libensemble.libE import libE
 from libensemble.tests.regression_tests.support import write_func as sim_f
@@ -24,6 +25,8 @@ from libensemble.tools import parse_args, add_unique_random_streams
 from libensemble.libE_manager import ManagerException
 
 nworkers, is_master, libE_specs, _ = parse_args()
+
+time.sleep(1)  # Previous directories may not be written out in time?
 
 sim_input_dir = './sim_input_dir'
 dir_to_copy = sim_input_dir + '/copy_this'
