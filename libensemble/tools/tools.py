cd60cbc3feaaa81a0beda49662885fb8937ad3c5
Jeffrey Larson <jmlarson@anl.gov>
2020-05-08 18:38:39 -0500

    flake8

--- libensemble/tools/tools.py
+++ libensemble/tools/tools.py
@@ -92,8 +92,8 @@ def save_libE_output(H, persis_info, calling_file, nworkers, mess='Run completed
     script_name = os.path.splitext(os.path.basename(calling_file))[0]
     short_name = script_name.split("test_", 1).pop()
     prob_str = 'length=' + str(len(H)) \
-                          + '_evals=' + str(sum(H['returned'])) \
-                          + '_workers=' + str(nworkers)
+                         + '_evals=' + str(sum(H['returned'])) \
+                         + '_workers=' + str(nworkers)
 
     h_filename = short_name + '_history_' + prob_str
     p_filename = short_name + '_persis_info_' + prob_str
