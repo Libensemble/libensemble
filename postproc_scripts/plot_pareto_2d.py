34b57a59c1190a36126cf50831e0cd623917c9e2
Jeffrey Larson <jmlarson@anl.gov>
2020-05-08 13:45:58 -0500

    flake8 for postproc scripts

--- postproc_scripts/plot_pareto_2d.py
+++ postproc_scripts/plot_pareto_2d.py
@@ -2,12 +2,13 @@ import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
+
 # Loop through objective points in f and extract the Pareto front.
 # input: f is a list (dimensions n X p) of n p-dimensional objective points.
 # output: a new list (dimensions m X p, m<n) of m nondominate points from f.
 def get_pf(f):
     eps = 10.0 ** (-8.0)
-    [n,p] = np.shape(f_vals)
+    [n, p] = np.shape(f_vals)
     pareto_f = []
     # Loop over all entries in f
     for p in f_vals:
@@ -15,16 +16,17 @@ def get_pf(f):
         # Check whether any q in f_vals dominates p
         for q in f_vals:
             # If they are equal, there is no dominance
-            if ( np.linalg.norm(p-q) < eps):
+            if (np.linalg.norm(p-q) < eps):
                 continue
             # If q dominates p + a small perturbation, there is dominance
-            elif ( (q < p + eps).all() ):
+            elif ((q < p + eps).all()):
                 dom = True
         # Add nondominated points to the list
         if (not dom):
             pareto_f.append(p)
     # Return the nondominated set
     return np.asarray(pareto_f)
+
 
 # Load the data
 if len(sys.argv) > 1:
@@ -45,15 +47,15 @@ else:
 # Plot the contents of argv[1] when P=2.
 obj1 = []
 obj2 = []
-for i in range(pts[:,0].size):
-   obj1.append(pts[i,0])
-   obj2.append(pts[i,1])
+for i in range(pts[:, 0].size):
+    obj1.append(pts[i, 0])
+    obj2.append(pts[i, 1])
 
 # Set up the figure environment.
 fig = plt.figure()
 
 # Sort the Pareto points.
-obj2.sort(key=dict(zip(obj2,obj1)).get)
+obj2.sort(key=dict(zip(obj2, obj1)).get)
 obj1.sort()
 
 # Plot the Pareto points.
--- postproc_scripts/plot_pareto_3d.py
+++ postproc_scripts/plot_pareto_3d.py
@@ -1,14 +1,14 @@
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
-from mpl_toolkits import mplot3d
+
 
 # Loop through objective points in f and extract the Pareto front.
 # input: f is a list (dimensions n X p) of n p-dimensional objective points.
 # output: a new list (dimensions m X p, m<n) of m nondominate points from f.
 def get_pf(f):
     eps = 10.0 ** (-8.0)
-    [n,p] = np.shape(f_vals)
+    [n, p] = np.shape(f_vals)
     pareto_f = []
     # Loop over all entries in f
     for p in f_vals:
@@ -16,16 +16,17 @@ def get_pf(f):
         # Check whether any q in f_vals dominates p
         for q in f_vals:
             # If they are equal, there is no dominance
-            if ( np.linalg.norm(p-q) < eps):
+            if (np.linalg.norm(p-q) < eps):
                 continue
             # If q dominates p + a small perturbation, there is dominance
-            elif ( (q < p + eps).all() ):
+            elif ((q < p + eps).all()):
                 dom = True
         # Add nondominated points to the list
         if (not dom):
             pareto_f.append(p)
     # Return the nondominated set
     return np.asarray(pareto_f)
+
 
 # Load the data
 if len(sys.argv) > 1:
@@ -47,17 +48,17 @@ else:
 obj1 = []
 obj2 = []
 obj3 = []
-for i in range(pts[:,0].size):
-   obj1.append(pts[i,0])
-   obj2.append(pts[i,1])
-   obj3.append(pts[i,2])
+for i in range(pts[:, 0].size):
+    obj1.append(pts[i, 0])
+    obj2.append(pts[i, 1])
+    obj3.append(pts[i, 2])
 
 # Set up the figure environment.
 fig = plt.figure()
 ax = plt.axes(projection="3d")
 
 # Scatter the Pareto points.
-ax.scatter(obj1, obj2, obj3)#, c=obj3)
+ax.scatter(obj1, obj2, obj3)
 ax.set_xlabel('F1')
 ax.set_ylabel('F2')
 ax.set_zlabel('F3')
