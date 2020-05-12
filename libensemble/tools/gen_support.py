7028ba0acc3e34a4dc799a1271ba38156a9eaef1
shudson <shudson@anl.gov>
2020-05-11 21:59:45 -0500

    Changes to comply with flake8 3.8.0

--- libensemble/tools/gen_support.py
+++ libensemble/tools/gen_support.py
@@ -1,17 +1,17 @@
 from libensemble.message_numbers import STOP_TAG, PERSIS_STOP, UNSET_TAG, EVAL_GEN_TAG
 
 
-def sendrecv_mgr_worker_msg(comm, O, status=None):
+def sendrecv_mgr_worker_msg(comm, output, status=None):
     """Send message from worker to manager and receive response.
     """
-    send_mgr_worker_msg(comm, O)
+    send_mgr_worker_msg(comm, output)
     return get_mgr_worker_msg(comm, status=status)
 
 
-def send_mgr_worker_msg(comm, O):
+def send_mgr_worker_msg(comm, output):
     """Send message from worker to manager.
     """
-    D = {'calc_out': O,
+    D = {'calc_out': output,
          'libE_info': {'persistent': True},
          'calc_status': UNSET_TAG,
          'calc_type': EVAL_GEN_TAG
