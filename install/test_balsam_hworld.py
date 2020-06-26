9cbe039bba19b28e5a6d02ee83825321a3d507dd
jlnav <jnavarro@anl.gov>
2020-06-26 16:18:00 -0500

    try new succeed condition, since other output lines are variable

--- install/test_balsam_hworld.py
+++ install/test_balsam_hworld.py
@@ -75,8 +75,9 @@ def print_job_output(outscript):
     limit = 60
 
     print('Blank output file found. Waiting for expected complete Balsam Job Output.')
-    lastlines = ['Task 4 done on worker 1\n', 'Task 4 done on worker 2\n',
-                 'Run completed.\n']
+    # lastlines = ['Task 4 done on worker 1\n', 'Task 4 done on worker 2\n',
+    #              'Run completed.\n']
+    succeed_line = 'Received:  [34 34 31 31 32 32 33 33  0  0]\n'
 
     lastposition = 0
 
@@ -91,7 +92,8 @@ def print_job_output(outscript):
         else:
             print(sleeptime, end=" ", flush=True)
 
-        if any(new.endswith(line) for line in lastlines):
+        # if any(new.endswith(line) for line in lastlines):
+        if succeed_line in new:
             break
 
         time.sleep(1)
