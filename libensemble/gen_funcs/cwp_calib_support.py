609d8e9f92570cc536a522166b3c532e87bcc8cc
Moses Chan <mosesyhc@u.northwestern.edu>
2020-09-28 08:01:42 -0500

    parameter obviation function

--- libensemble/gen_funcs/cwp_calib_support.py
+++ libensemble/gen_funcs/cwp_calib_support.py
@@ -46,7 +46,7 @@ def select_next_theta(model, cur_thetas, n_explore_theta, expect_impr_exit, pers
 
     expect_improvement = ((best_chi2 > new_chi2)*(best_chi2 - new_chi2)).mean(axis=1)
     # prob_improvement = (best_chi2 > new_chi2).mean(axis=1)
-    print('No. of thetas = {:d}'.format(cur_thetas.shape[0]))
+    print('\nNo. of thetas = {:d}'.format(cur_thetas.shape[0]))
     print('MAX EI = {:.2f}'.format(np.max(expect_improvement)))
     print('Best chi^2 = {:.2f}'.format(best_chi2))
 
@@ -60,3 +60,29 @@ def select_next_theta(model, cur_thetas, n_explore_theta, expect_impr_exit, pers
         # print('new chi2: {:.2f}'.format(np.min(new_chi2)))
 
     return np.atleast_2d(new_theta), stop_flag, persis_info
+
+
+def obviate_pend_thetas(model, cur_theta, data_status):
+    assert cur_theta.shape[0] == data_status.shape[0]
+
+    complete_theta_bool = np.any(data_status != 0, axis=1)
+
+    fpred, _ = emulation_prediction(model, cur_theta[complete_theta_bool])
+    fdraws = emulation_draws(model, cur_theta[~complete_theta_bool], options={'numsamples': 500})
+
+    cur_chi2 = np.sum(fpred ** 2, axis=1)
+    best_chi2 = np.min(cur_chi2)
+
+    incomplete_draw_chi2 = np.zeros((fdraws.shape[0], fdraws.shape[2]))
+    for k in np.arange(fdraws.shape[2]):
+        incomplete_draw_chi2[:, k] = np.sum(fdraws[:, :, k] ** 2, axis=1)
+
+    expect_improvement = np.full_like(complete_theta_bool, 0)
+    # meaningful when there are pending thetas
+    expect_improvement[~complete_theta_bool] = ((best_chi2 > incomplete_draw_chi2)*(best_chi2 - incomplete_draw_chi2)).mean(axis=1)
+    r_obviate = np.full_like(expect_improvement, False)
+    r_obviate[np.where(np.logical_and(expect_improvement < 10 ** (-9), ~complete_theta_bool))] = True
+    r_obviate[np.argmin(cur_chi2)] = False
+
+    # data_status[r_obviate, :] = -2  # assign cancellation
+    return np.array(np.where(r_obviate))  #, data_status
