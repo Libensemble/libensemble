9610d2529ad6531cc8437d95455381258480ad4c
Jeffrey Larson <jmlarson@anl.gov>
2020-10-16 12:27:13 -0500

    Showing an example of asynchronous uniform sampling

--- libensemble/gen_funcs/persistent_uniform_sampling.py
+++ libensemble/gen_funcs/persistent_uniform_sampling.py
@@ -7,10 +7,14 @@ from libensemble.tools.gen_support import sendrecv_mgr_worker_msg
 def persistent_uniform(H, persis_info, gen_specs, libE_info):
     """
     This generation function always enters into persistent mode and returns
-    ``gen_specs['gen_batch_size']`` uniformly sampled points.
+    ``gen_specs['gen_batch_size']`` uniformly sampled points the first time it
+    is called. Afterwards, it returns the number of points given. This can be
+    used in either a batch or asynchronous mode by adjusting the allocation
+    function.
 
     .. seealso::
         `test_persistent_uniform_sampling.py <https://github.com/Libensemble/libensemble/blob/develop/libensemble/tests/regression_tests/test_persistent_uniform_sampling.py>`_ # noqa
+        `test_persistent_uniform_sampling_async.py <https://github.com/Libensemble/libensemble/blob/develop/libensemble/tests/regression_tests/test_persistent_uniform_sampling_async.py>`_ # noqa
     """
     ub = gen_specs['user']['ub']
     lb = gen_specs['user']['lb']
@@ -23,5 +27,7 @@ def persistent_uniform(H, persis_info, gen_specs, libE_info):
         H_o = np.zeros(b, dtype=gen_specs['out'])
         H_o['x'] = persis_info['rand_stream'].uniform(lb, ub, (b, n))
         tag, Work, calc_in = sendrecv_mgr_worker_msg(libE_info['comm'], H_o)
+        if calc_in is not None:
+            b = len(calc_in)
 
     return H_o, persis_info, FINISHED_PERSISTENT_GEN_TAG
