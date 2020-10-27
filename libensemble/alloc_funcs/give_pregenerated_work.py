f9bb218b057ae2749620b0d13e6467c2e89c1602
Jeffrey Larson <jmlarson@anl.gov>
2020-10-27 14:32:00 -0500

    First attempt at allowing H0 to have un-given points

--- libensemble/alloc_funcs/give_pregenerated_work.py
+++ libensemble/alloc_funcs/give_pregenerated_work.py
@@ -14,11 +14,15 @@ def give_pregenerated_sim_work(W, H, sim_specs, gen_specs, alloc_specs, persis_i
     if not persis_info:
         persis_info['next_to_give'] = 0
 
-    assert persis_info['next_to_give'] < len(H), 'No more work to give inside give_pregenerated_sim_work.'
+    if persis_info['next_to_give'] >= len(H):
+        return Work, persis_info, 1
 
     for i in avail_worker_ids(W):
         # Give sim work
         sim_work(Work, i, sim_specs['in'], [persis_info['next_to_give']], [])
         persis_info['next_to_give'] += 1
+
+        if persis_info['next_to_give'] >= len(H):
+            break
 
     return Work, persis_info
