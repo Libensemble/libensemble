deefac5898cb39fd0957a727187853056747ca42
shudson <shudson@anl.gov>
2020-11-30 14:32:36 -0600

    Add sub-node run-line tests and restructure run-line tests
    Created run_line_check.py as a sim func
    Added test_zero_resource_workers_subnode.py
    Restructured runline tests to use run_line_check.py

--- libensemble/tests/regression_tests/test_mpi_runners_subnode.py
+++ libensemble/tests/regression_tests/test_mpi_runners_subnode.py
@@ -1,12 +1,14 @@
 # """
-# Runs libEnsemble testing the zero_resource_workers argument.
+# Runs libEnsemble testing the zero_resource_workers argument with 2 workers per node.
 #
-# Execute via one of the following commands (e.g. 3 workers):
-#    mpiexec -np 4 python3 test_zero_resource_workers.py
-#    python3 test_zero_resource_workers.py --nworkers 3 --comms local
-#    python3 test_zero_resource_workers.py --nworkers 3 --comms tcp
+# This test must be run on an even number of workers >= 2 (e.g. odd no. of procs when using mpi4py).
 #
-# The number of concurrent evaluations of the objective function will be 4-1=3.
+# Execute via one of the following commands (e.g. 2 workers):
+#    mpiexec -np 3 python3 test_zero_resource_workers_subnode.py
+#    python3 test_zero_resource_workers_subnode.py --nworkers 2 --comms local
+#    python3 test_zero_resource_workers_subnode.py --nworkers 2 --comms tcp
+#
+# The number of concurrent evaluations of the objective function will be 3-1=2.
 # """
 
 import sys
@@ -26,7 +28,7 @@ libE_logger.set_level('INFO')
 
 # Do not change these lines - they are parsed by run-tests.sh
 # TESTSUITE_COMMS: mpi local
-# TESTSUITE_NPROCS: 3 4
+# TESTSUITE_NPROCS: 3
 
 nworkers, is_manager, libE_specs, _ = parse_args()
 rounds = 1
@@ -39,17 +41,17 @@ libE_specs['zero_resource_workers'] = [1]
 log_file = 'ensemble_zrw_comms_' + str(comms) + '_wrks_' + str(nworkers) + '.log'
 libE_logger.set_filename(log_file)
 
-nodes_per_worker = 2
+nodes_per_worker = 0.5
 
 # For varying size test - relate node count to nworkers
-in_place = libE_specs['zero_resource_workers']
-n_gens = len(in_place)
-nsim_workers = nworkers-n_gens
+nsim_workers = nworkers
+
+if not (nsim_workers*nodes_per_worker).is_integer():
+    sys.exit("Sim workers ({}) must divide evenly into nodes".format(nsim_workers))
 
 comms = libE_specs['comms']
-nodes_per_worker = 2
-node_file = 'nodelist_zero_resource_workers_' + str(comms) + '_wrks_' + str(nworkers)
-nnodes = nsim_workers*nodes_per_worker
+node_file = 'nodelist_zero_resource_workers_subnode_' + str(comms) + '_wrks_' + str(nworkers)
+nnodes = int(nsim_workers*nodes_per_worker)
 
 if is_manager:
     create_node_file(num_nodes=nnodes, name=node_file)
@@ -59,15 +61,14 @@ if comms == 'mpi':
 
 
 # Mock up system
-customizer = {'mpi_runner': 'mpich',    # Select runner: mpich, openmpi, aprun, srun, jsrun
-              'runner_name': 'mpirun',  # Runner name: Replaces run command if not None
+customizer = {'mpi_runner': 'srun',    # Select runner: mpich, openmpi, aprun, srun, jsrun
+              'runner_name': 'srun',  # Runner name: Replaces run command if not None
               'cores_on_node': (16, 64),   # Tuple (physical cores, logical cores)
               'node_file': node_file}      # Name of file containing a node-list
 
 # Create executor and register sim to it.
-exctr = MPIExecutor(zero_resource_workers=in_place, central_mode=True,
-                    auto_resources=True, allow_oversubscribe=False,
-                    custom_info=customizer)
+exctr = MPIExecutor(central_mode=True, auto_resources=True,
+                    allow_oversubscribe=False, custom_info=customizer)
 exctr.register_calc(full_path=sim_app, calc_type='sim')
 
 
@@ -93,19 +94,23 @@ persis_info = add_unique_random_streams({}, nworkers + 1)
 exit_criteria = {'sim_max': (nsim_workers)*rounds}
 
 # Each worker has 2 nodes. Basic test list for portable options
-test_list_base = [{'testid': 'base1', 'nprocs': 2, 'nnodes': 1, 'ppn': 2, 'e_args': '--xarg 1'},  # Under use
-                  {'testid': 'base2'},  # Give no config and no extra_args
+test_list_base = [{'testid': 'base1'},  # Give no config and no extra_args
+                  {'testid': 'base2', 'nprocs': 5},
+                  {'testid': 'base3', 'nnodes': 1},
+                  {'testid': 'base4', 'ppn': 6},
                   ]
 
-exp_mpich = \
-    ['mpirun -hosts node-1 -np 2 --ppn 2 --xarg 1 /path/to/fakeapp.x --testid base1',
-     'mpirun -hosts node-1,node-2 -np 32 --ppn 16 /path/to/fakeapp.x --testid base2',
+exp_srun = \
+    ['srun -w node-1 --ntasks 8 --nodes 1 --ntasks-per-node 8 /path/to/fakeapp.x --testid base1',
+     'srun -w node-1 --ntasks 5 --nodes 1 --ntasks-per-node 5 /path/to/fakeapp.x --testid base2',
+     'srun -w node-1 --ntasks 8 --nodes 1 --ntasks-per-node 8 /path/to/fakeapp.x --testid base3',
+     'srun -w node-1 --ntasks 6 --nodes 1 --ntasks-per-node 6 /path/to/fakeapp.x --testid base4',
      ]
 
 test_list = test_list_base
-exp_list = exp_mpich
+exp_list = exp_srun
 sim_specs['user'] = {'tests': test_list, 'expect': exp_list,
-                     'nodes_per_worker': nodes_per_worker, 'persis_gens': n_gens}
+                     'nodes_per_worker': nodes_per_worker}
 
 
 # Perform the run
