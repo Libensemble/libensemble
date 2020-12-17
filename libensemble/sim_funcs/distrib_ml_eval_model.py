5c226fcbec896cab7dcc1c3d8b96af1b0231797b
jlnav <jnavarro@anl.gov>
2020-12-17 17:15:55 -0600

    initial eval_model routine, comment out much to ensure data transfer occurring, adjust fields

--- libensemble/sim_funcs/distrib_ml_eval_model.py
+++ libensemble/sim_funcs/distrib_ml_eval_model.py
@@ -6,18 +6,35 @@ import os
 import time
 import glob
 import numpy as np
+import tensorflow as tf
 
-__all__ = ['distrib_ml_build_model']
 
-def distrib_ml_build_model(H, persis_info, gen_specs, libE_info):
+__all__ = ['distrib_ml_eval_model']
 
-    comm = libE_info['comm']
-    num_procs = gen_specs['user']['num_procs']
-    app_args = gen_specs['user']['app_args']
-    dry_run = gen_specs['user']['dry_run']
-    time_limit = gen_specs['user']['time_limit']
-    epochs = app_args.split('epochs ')[-1]
+def distrib_ml_eval_model(H, persis_info, sim_specs, libE_info):
 
+    model_file = H['model_file']
+    print('inside sim, ', model_file)
+    H_o = np.zeros(1, dtype=sim_specs['out'])
+    H_o['evaluation'] = 0.1234
+
+    return H_o, persis_info
+
+    # _ , (mnist_test_images, mnist_test_labels) = \
+    #     tf.keras.datasets.mnist.load_data(path='mnist.npz')
+    #
+    # dataset = tf.data.Dataset.from_tensor_slices(
+    #     (tf.cast(mnist_test_images[..., tf.newaxis] / 255.0, tf.float32),
+    #              tf.cast(mnist_test_labels, tf.int64))
+    # )
+
+    # comm = libE_info['comm']
+    # num_procs = gen_specs['user']['num_procs']
+    # app_args = gen_specs['user']['app_args']
+    # dry_run = gen_specs['user']['dry_run']
+    # time_limit = gen_specs['user']['time_limit']
+    # epochs = app_args.split('epochs ')[-1]
+    #
     # exctr = MPIExecutor.executor
     # task = exctr.submit(calc_type='gen', num_procs=num_procs, app_args=app_args,
     #                     stdout='out.txt', stderr='err.txt', hyperthreads=True,
@@ -56,18 +73,11 @@ def distrib_ml_build_model(H, persis_info, gen_specs, libE_info):
     #     "Keras Application did not write final output to file."
     # assert f'Epoch {epochs}/{epochs}' in task.read_stdout(),
     #     "Keras Application did not complete all epochs."
-
-    current_dir = '/'.join(os.getcwd().split('/')[-2:])  # ensemble and gen dir
-
-    ### JUSTFORTEST ###
-    open('hello.txt', 'w')
-    calc_status = WORKER_DONE
-    model_file = ['hello.txt']
-    ### JUSTFORTEST ###
-
-    H_o = np.zeros(1, dtype=gen_specs['out'])
-    H_o['cstat'] = calc_status
-    H_o['model_file'] = current_dir + '/' + model_file[0]
-    print('producing in gen, ', H_o['model_file'])
-
-    return H_o, persis_info, calc_status
+    #
+    # current_dir = os.getcwd().split('/')[-1]
+    #
+    # H_o = np.zeros(1, dtype=gen_specs['out'])
+    # H_o['cstat'] = calc_status
+    # H_o['model_file'] = current_dir + '/' + model_file[0]
+    #
+    # return H_o, persis_info, calc_status
--- libensemble/tests/regression_tests/test_distrib_ml.py
+++ libensemble/tests/regression_tests/test_distrib_ml.py
@@ -17,13 +17,11 @@ import multiprocessing
 from libensemble.libE import libE
 from libensemble.sim_funcs.distrib_ml_eval_model import distrib_ml_eval_model as sim_f
 from libensemble.gen_funcs.distrib_ml_build_model import distrib_ml_build_model as gen_f
+from libensemble.tools import parse_args, add_unique_random_streams
 
 # Do not change these lines - they are parsed by run-tests.sh
 # TESTSUITE_COMMS: mpi local tcp
 # TESTSUITE_NPROCS: 2 3 4
-
-# JLN TODO: gen_f uses ML example, uses exctr to launch example that downloads data, generates keras model, writes to file
-# JLN TODO: sim_f loads model, evaluates with with test data or orders predictions
 
 nworkers, is_manager, libE_specs, _ = parse_args()
 
@@ -50,24 +48,29 @@ exctr.register_calc(full_path=gen_app, app_name='ml_keras_mnist')  # Named app
 
 sim_specs = {'sim_f': sim_f,
              'in': ['model_file'],
-             'out': [('prediction', int)]
+             'out': [('evaluation', float, (1,))]
             }
 
 gen_specs = {'gen_f': gen_f,
              'in': [],
-             'out': [('model_file', "<U50"), ('cstat', int)],
+             'out': [('model_file', "<U60", (1,)), ('cstat', int, (1,))],
              'user':{'num_procs': 4,
                      'app_args': "--device cpu --epochs 32",
                      'dry_run': True,
                      'time_limit': 1800}  # seconds
             }
 
+persis_info = add_unique_random_streams({}, nworkers + 1)  # JLN: I *really* don't think I need this!
+
 libE_specs['gen_dirs_make'] = True
-libE_specs['ensemble_dir_path'] = './' + time.asctime().replace(' ', '_')
+libE_specs['ensemble_dir_path'] = './mnist_ensemble_' + time.asctime().replace(' ', '_')
 
 exit_criteria = {'gen_max': 4}
-persis_info = {}
 
 # Perform the run
 H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info,
                             libE_specs=libE_specs)
+
+if is_manager:
+    print('Routine complete!')
+    print(H)
