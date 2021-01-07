833eb43b83463d6aea0227ee40a6a210085179d3
shudson <shudson@anl.gov>
2021-01-07 00:12:37 -0600

    Use VERSION in __init__.py

--- libensemble/__init__.py
+++ libensemble/__init__.py
@@ -4,7 +4,10 @@ libEnsemble.
 Library to coordinate the concurrent evaluation of dynamic ensembles of calculations.
 """
 
-__version__ = "0.7.1+dev"
+with open("VERSION", "r") as f:
+    version_str = f.read().strip('\n')
+
+__version__ = version_str
 __author__ = 'Jeffrey Larson, Stephen Hudson, Stefan M. Wild, David Bindel and John-Luke Navarro'
 __credits__ = 'Argonne National Laboratory'
 
