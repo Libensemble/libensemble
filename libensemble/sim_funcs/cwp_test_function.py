19fe69e3e9bf546921899cdacdc93e3fef75101f
Moses Chan <mosesyhc@u.northwestern.edu>
2021-02-10 15:44:10 -0600

    test sim function without failure

--- libensemble/sim_funcs/cwp_test_function.py
+++ libensemble/sim_funcs/cwp_test_function.py
@@ -12,7 +12,8 @@ def borehole(H, persis_info, sim_specs, _):
     """
 
     H_o = np.zeros(H['x'].shape[0], dtype=sim_specs['out'])
-    H_o['f'] = borehole_failmodel(H['x'], H['thetas'])
+    # H_o['f'] = borehole_failmodel(H['x'], H['thetas'])
+    H_o['f'] = borehole_model(H['x'], H['thetas'])
     return H_o, persis_info
 
 
