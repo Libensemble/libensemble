ac17d0c1fb03b3299971532b0b92dec6c0d64294
Jeffrey Larson <jmlarson@anl.gov>
2021-05-25 15:04:39 -0500

    Afternoon chat

--- libensemble/alloc_funcs/start_only_persistent.py
+++ libensemble/alloc_funcs/start_only_persistent.py
@@ -64,25 +64,15 @@ def only_persistent_gens(W, H, sim_specs, gen_specs, alloc_specs, persis_info):
 
                 H['given_back'][inds_since_last_gen] = True
 
-    task_avail = ~H['given'] & ~H['cancel_requested']
     for i in avail_worker_ids(W, persistent=False):
-
-        if np.any(task_avail):
-            if 'priority' in H.dtype.fields:
-                priorities = H['priority'][task_avail]
-                if gen_specs['user'].get('give_all_with_same_priority'):
-                    q_inds = (priorities == np.max(priorities))
-                else:
-                    q_inds = np.argmax(priorities)
-            else:
-                q_inds = 0
-
-            # perform sim evaluations (if they exist in History).
-            sim_ids_to_send = np.nonzero(task_avail)[0][q_inds]  # oldest point(s)
-            persis_info[i]['f_i_s_to_do'] = [1,3,67]
-            H[sim_ids_to_send]['f_i_done'][[1,3,67]] = True
-            sim_work(Work, i, sim_specs['in'], np.atleast_1d(sim_ids_to_send), persis_info.get(i))
-            task_avail[sim_ids_to_send] = False
+        counter = 0 
+        row,col = np.where(H['f_i_done'] == 0)
+        if counter < len(row):
+            # perform sim evaluations 
+            persis_info[i]['f_i_s_to_do'] = [col[counter]]
+            H[row[counter]]['f_i_done'][col[counter]] = True
+            sim_work(Work, i, sim_specs['in'], np.atleast_1d(row[counter]), persis_info.get(i))
+            counter += 1
 
         elif gen_count == 0:
             # Finally, call a persistent generator as there is nothing else to do.
