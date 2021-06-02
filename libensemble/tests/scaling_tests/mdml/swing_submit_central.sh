313bc80e06b4eb0263a84c089b97d4b41a374ad7
jlnav <jnavarro@anl.gov>
2021-06-02 12:48:36 -0500

    adding swing central script

---
+++ libensemble/tests/scaling_tests/mdml/swing_submit_central.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+#SBATCH --job-name=libE-test
+#SBATCH --account=STARTUP-USERNAME
+#SBATCH --nodes=2
+#SBATCH --gres=gpu:2
+#SBATCH --time=00:30:00
+
+# Make sure conda and environment are loaded and activated before sbatch
+
+module load gcc
+module load cuda/11.0.2-4szlv2t
+
+export EXE=run_libe_mdml.py
+export NUM_WORKERS=2
+
+export PYTHONNOUSERSITE=1
+
+python $EXE --comms local --nworkers $NUM_WORKERS
+
+echo The command is: $cmd
+echo End PBS script information.
+echo All further output is from the process being run and not the script.\n\n
+
+$cmd
+
+# Print the date again -- when finished
+echo Finished at: `date`
