0cbd76b502c200c816131a48340120d4ea2e896a
jlnav <jnavarro@anl.gov>
2021-06-16 11:11:31 -0500

    sim_count -> returned_count

--- libensemble/sim_funcs/executor_hworld.py
+++ libensemble/sim_funcs/executor_hworld.py
@@ -112,9 +112,9 @@ def executor_hworld(H, persis_info, sim_specs, libE_info):
             calc_status = TASK_FAILED
 
     else:
-        if sim_count >= 2:
+        if returned_count >= 2:
             calc_status = exctr.polling_loop(task, timeout=timeout)
-            if sim_count == 2 and task.stdout_exists() and 'Error' in task.read_stdout():
+            if returned_count == 2 and task.stdout_exists() and 'Error' in task.read_stdout():
                 calc_status = WORKER_KILL_ON_ERR
 
         else:
