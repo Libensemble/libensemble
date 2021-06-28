9a3cdbd0eb1c1bd992cdcfbcae7da39a41468d4d
Caleb GT <calebju4@gatech.edu>
2021-06-28 17:43:25 -0500

    Extra printing code for debugging (commented out printing for now)

--- libensemble/gen_funcs/persistent_prox_slide.py
+++ libensemble/gen_funcs/persistent_prox_slide.py
@@ -18,6 +18,7 @@ def opt_slide(H, persis_info, gen_specs, libE_info):
 
     # start with random x0
     x0 = persis_info['rand_stream'].uniform(low=lb, high=ub, size=(n,)) 
+    print('x0={}'.format(x0), flush=True)
     x_k = x0
     post_x_k = x0
 
@@ -30,6 +31,7 @@ def opt_slide(H, persis_info, gen_specs, libE_info):
     print('[{}/{}] x={}'.format(0, num_outer_iters, x0), flush=True)
 
     f_obj = 0
+    prev_f_obj = np.inf
 
     for k in range(num_outer_iters):
 
@@ -54,16 +56,29 @@ def opt_slide(H, persis_info, gen_specs, libE_info):
         neighbors_pre_x_k = calc_in['x']
         num_neighbors = len(neighbors_pre_x_k)
         # computes local grad_x { (W \otimes I)[x_1,x_2,...,x_m] }
+        # gradg = 2*R * ( num_neighbors * pre_x_k - np.sum(neighbors_pre_x_k, axis=0) )/((k+1)**0.5)
         gradg = 2*R * ( num_neighbors * pre_x_k - np.sum(neighbors_pre_x_k, axis=0) )
 
-        # print('num of inner iters: {}. norm of grad: {:.4e}'.format(T_k, la.norm(gradg)), flush=True)
-        x_k, x2_k, ct = PS(x_k, gradg, b_k, T_k, ct, f_i_idxs, gen_specs, libE_info, pre_x_k)
+        _x_k = x_k.copy()
 
+        # print('pre_x_k={}, x_k-1={}, gradg={}, b_k={:.4f}, T_k={:.4f}'.format(pre_x_k, x_k, gradg, b_k, T_k), flush=True)
+
+        # print('num of inner iters: {}. norm of grad: {:.4e}'.format(T_k, la.norm(gradg)), flush=True)
+        if k == 245:
+            # import ipdb; ipdb.set_trace()
+            pass
+        x_k, x2_k, ct = PS(_x_k, gradg, b_k, T_k, ct, f_i_idxs, gen_specs, libE_info, pre_x_k, persis_info['worker_num'])
+
+        # TEMP
         # recieved end signal 
+        # if k >= 5 or x_k is None:
         if x_k is None:
+            # import ipdb; ipdb.set_trace()
             return None, persis_info, FINISHED_PERSISTENT_GEN_TAG
 
-        post_x_k = (1.0-g_k) * post_x_k + (g_k * x2_k)
+        _post_x_k = (1.0-g_k) * post_x_k + (g_k * x2_k)
+
+        # print('x_k={}, tilde_x_k={}, post_x_k={}'.format(x_k, x2_k, post_x_k), flush=True)
 
         # guage progress
         l = len(f_i_idxs)
@@ -80,17 +95,25 @@ def opt_slide(H, persis_info, gen_specs, libE_info):
             return None, None, ct
         f_is = calc_in['f_i']
         f_obj = np.sum(f_is)
+
+        score = np.dot(gradg, x_k - pre_x_k) + f_obj + b_k/2.0 * la.norm(x_k-_x_k, ord=2)**2
+
+        post_x_k = _post_x_k
+
+        prev_f_obj = f_obj
         # gradf_is = calc_in['gradf_i']
         # gradf    = np.sum(gradf_is, axis=0)
 
         # print('[{}/{}] x={} ||gradf||={:.8f}'.format(k+1, num_outer_iters, post_x_k, la.norm(gradf, ord=2)), flush=True)
 
-        # print('[{}/{}] x={}'.format(k+1, num_outer_iters, post_x_k), flush=True)
-        print('[{}: {}/{}] f_is={:.8f}'.format(persis_info['worker_num'], k+1, num_outer_iters, f_obj), flush=True)
+        print('[{}/{} : {}] x={}'.format(k+1, num_outer_iters, T_k, post_x_k), flush=True)
+        # print('[{}: {}/{}] f_is={:.8f}'.format(persis_info['worker_num'], k+1, num_outer_iters, f_obj), flush=True)
+        # print('[{}: {}/{}] fobj={:.4e} obj={:.4e}'.format(persis_info['worker_num'], k+1, num_outer_iters, f_obj, score), flush=True)
+        # print('|x_k - pre_x_k|={:4e}'.format(la.norm(x_k - pre_x_k)**2))
 
     return None, persis_info, FINISHED_PERSISTENT_GEN_TAG
 
-def PS(x, gradg, beta, T, ct, f_i_idxs, gen_specs, libE_info, pre_x_k):
+def PS(x, gradg, beta, T, ct, f_i_idxs, gen_specs, libE_info, pre_x_k, wid):
     """ Prox-sliding procedure (see https://arxiv.org/pdf/1911.10645) with
         entropy as the distance generating function for Bregman divergence.
     """
@@ -98,6 +121,8 @@ def PS(x, gradg, beta, T, ct, f_i_idxs, gen_specs, libE_info, pre_x_k):
     u2 = x
     n = len(x)
     l = len(f_i_idxs)
+
+    max_gradf = 0
 
     for t in range(1,T+1):
 
@@ -128,14 +153,17 @@ def PS(x, gradg, beta, T, ct, f_i_idxs, gen_specs, libE_info, pre_x_k):
         gradf_is = calc_in['gradf_i']
         gradf    = np.sum(gradf_is, axis=0)
 
+        # print("[{}] u={}, |f'(u)|={:.8f}".format(wid, u, la.norm(gradf, ord=2)), flush=True)
+
         u_next = get_l2_argmin(x, u, gradf, gradg, beta, p_t)
+        # u_next = get_entropy_argmin(x, u, gradf, gradg, beta, p_t)
         # if np.any(np.isnan(u_next)) or np.any(np.abs(u_next) > 10):
         #     import ipdb; ipdb.set_trace()
 
-        cons = 0.5 * np.dot(pre_x_k, gradg)
-        score_u_prev = cons + np.dot(gradg, u-pre_x_k) + np.dot(gradf,u) + beta/2.0*la.norm(x-u, 2)**2
-        score_x = cons + np.dot(gradg, x-pre_x_k) + np.dot(gradf,x) + beta*p_t/2.0*la.norm(x-u, 2)**2
-        score_u_curr = cons + np.dot(gradg, u_next-pre_x_k) + np.dot(gradf,u_next) + beta/2.0*la.norm(u_next-x,2)**2 + beta*p_t/2.0*la.norm(u_next-u,2)**2
+        # cons = 0.5 * np.dot(pre_x_k, gradg)
+        # score_u_prev = cons + np.dot(gradg, u-pre_x_k) + np.dot(gradf,u) + beta/2.0*la.norm(x-u, 2)**2
+        # score_x = cons + np.dot(gradg, x-pre_x_k) + np.dot(gradf,x) + beta*p_t/2.0*la.norm(x-u, 2)**2
+        # score_u_curr = cons + np.dot(gradg, u_next-pre_x_k) + np.dot(gradf,u_next) + beta/2.0*la.norm(u_next-x,2)**2 + beta*p_t/2.0*la.norm(u_next-u,2)**2
         # print('[Best? {}] u_prev={:.4e}, x={:.4e}, argmin={:.4e}'.format(
         #     score_u_curr <= score_u_prev and score_u_curr <= score_x,
         #     score_u_prev, score_x, score_u_curr), flush=True)
@@ -150,13 +178,23 @@ def get_l2_argmin(x, u_prev, gradf, gradg, beta, p_t):
     u_next = u_next / (beta * (1.0+p_t))
     return u_next
 
-def get_entropy_argmin():
+    # project
+    n = len(x)
+    u_norm = la.norm(u_next, ord=2)
+    B = 2*(n**0.5)
+    if u_norm > B:
+        u_next /= (u_norm/B)
+    return u_next
+
+def get_entropy_argmin(x, u, gradf, gradg, beta, p_t):
+    n = len(x) 
+
     const = 1.0/(1+p_t) * (np.log(x) + (1+p_t) * np.ones(n, dtype=float)) \
             - 2/(beta * (1+p_t))*gradg
 
-    assert len(grad_f) == len(u), print("len(grad_f)={}, expected {}".format(len(grad_f), len(u)))
+    assert len(gradf) == len(u), "len(grad_f)={}, expected {}".format(len(grad_f), len(u))
 
-    dyn = p_t/(1+p_t) * np.log(u) - 1.0/( beta * (1+p_t) ) * grad_f
+    dyn = p_t/(1+p_t) * np.log(u) - 1.0/( beta * (1+p_t) ) * gradf
 
     u_next = np.exp( const + dyn )
 
