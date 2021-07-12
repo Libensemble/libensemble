4f3d4a570a554d9c3443b3cb2eac2f98b52a6dac
shudson <shudson@anl.gov>
2021-07-12 12:05:49 -0500

    Update unit tests for manager rset assignment

--- libensemble/tests/unit_tests/test_resource_scheduler.py
+++ libensemble/tests/unit_tests/test_resource_scheduler.py
@@ -2,14 +2,14 @@ import sys
 import numpy as np
 from libensemble.resources.scheduler import ResourceScheduler
 
-#SH TODO: The assignment of resource sets will change when moved to manager only assignment.
 
 class MyResources:
+    """Simulate resources"""
 
     rset_dtype = [('assigned', int),  # Holds worker ID assigned to or zero
                   ('group', int)]     # Group ID this resource set belongs to
 
-    #Basic layout
+    # Basic layout
     def __init__(self, num_rsets, num_groups, ):
         self.num_rsets = num_rsets
         self.num_groups = num_groups
@@ -19,7 +19,6 @@ class MyResources:
         self.rsets['assigned'] = 0
         for i in range(num_rsets):
             self.rsets['group'][i] = i // self.rsets_per_node
-        #print(self.rsets['group'])
         self.rsets_free = self.num_rsets
         print(self.rsets)
 
@@ -51,33 +50,28 @@ def test_schdule_find_gaps_1node():
     resources = MyResources(8, 1)
     sched = ResourceScheduler(user_resources=resources)
 
-    rset_team = sched.assign_resources(rsets_req=2, worker_id=3)
+    rset_team = sched.assign_resources(rsets_req=2)
     assert rset_team == [0, 1]
-    assert list(resources.rsets['assigned']) == [3, 3, 0, 0, 0, 0, 0, 0]
 
-    rset_team = sched.assign_resources(rsets_req=3, worker_id=1)
+    rset_team = sched.assign_resources(rsets_req=3)
     assert rset_team == [2, 3, 4]
-    assert list(resources.rsets['assigned']) == [3, 3, 1, 1, 1, 0, 0, 0]
 
     # Check not enough slots
-    rset_team = sched.assign_resources(rsets_req=4, worker_id=5)
+    rset_team = sched.assign_resources(rsets_req=4)
     assert rset_team is None
-    assert list(resources.rsets['assigned']) == [3, 3, 1, 1, 1, 0, 0, 0]
 
-    rset_team = sched.assign_resources(rsets_req=2, worker_id=4)
+    rset_team = sched.assign_resources(rsets_req=2)
     assert rset_team == [5, 6]
-    assert list(resources.rsets['assigned']) == [3, 3, 1, 1, 1, 4, 4, 0]
 
     # Simulate resources freed up on return from worker
-    resources.free_rsets(worker=1)
-    # Now can fit worker 5. But free up means new alloc call
+    resources.rsets['assigned'] = [3, 3, 0, 0, 0, 4, 4, 0]
+
+    # Create new scheduler to simulate new alloc call
     del sched
     sched = ResourceScheduler(user_resources=resources)
 
-    #import pdb;pdb.set_trace()
-    rset_team = sched.assign_resources(rsets_req=4, worker_id=5)
+    rset_team = sched.assign_resources(rsets_req=4)
     assert rset_team == [2, 3, 4, 7]
-    assert list(resources.rsets['assigned']) == [3, 3, 5, 5, 5, 4, 4, 5]
     del resources
 
 
@@ -86,13 +80,13 @@ def test_schdule_find_gaps_2nodes():
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
     resources = MyResources(8, 2)
     sched = ResourceScheduler(user_resources=resources)
-    inputs = [2,3,1,2]
-    exp_out = [[0,1],[4,5,6],[7],[2,3]]
+    inputs = [2, 3, 1, 2]
+    exp_out = [[0, 1], [4, 5, 6], [7], [2, 3]]
     for i in range(4):
-        rset_team = sched.assign_resources(rsets_req=inputs[i], worker_id=i+1)
+        rset_team = sched.assign_resources(rsets_req=inputs[i])
         assert rset_team == exp_out[i], \
             'Expected {}, Received rset_team {}'.format(exp_out[i], rset_team)
-    rset_team = sched.assign_resources(rsets_req=1, worker_id=5)
+    rset_team = sched.assign_resources(rsets_req=1)
     assert rset_team is None
     del resources
 
@@ -104,7 +98,7 @@ def test_across_nodes_even_split():
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
     resources = MyResources(8, 2)
     sched = ResourceScheduler(user_resources=resources)
-    rset_team = sched.assign_resources(rsets_req=6, worker_id=1)
+    rset_team = sched.assign_resources(rsets_req=6)
     # Expecting even split
     assert rset_team == [0, 1, 2, 4, 5, 6], \
         'Even split test did not get expected result {}'.format(rset_team)
@@ -112,10 +106,11 @@ def test_across_nodes_even_split():
     # This time it must use 3 nodes for even split (though 2 would cover uneven).
     resources = MyResources(15, 3)
     sched = ResourceScheduler(user_resources=resources)
-    rset_team = sched.assign_resources(rsets_req=9, worker_id=1)
+    rset_team = sched.assign_resources(rsets_req=9)
     # Expecting even split
     assert rset_team == [0, 1, 2, 5, 6, 7, 10, 11, 12], \
         'Even split test did not get expected result {}'.format(rset_team)
+    del resources
 
 
 def test_across_nodes_roundup_option():
@@ -125,15 +120,16 @@ def test_across_nodes_roundup_option():
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
     resources = MyResources(8, 2)
     sched = ResourceScheduler(user_resources=resources)
-    rset_team = sched.assign_resources(rsets_req=5, worker_id=1)
+    rset_team = sched.assign_resources(rsets_req=5)
     # Expecting even split
     assert rset_team == [0, 1, 2, 4, 5, 6], \
         'Even split test did not get expected result {}'.format(rset_team)
+    del resources
 
 # SH TODO: Further tests for testing uneven splits - and uneven sized resource sets.
 
+
 if __name__ == "__main__":
-    #test_schdule_find_gaps______noloop()
     test_schdule_find_gaps_1node()
     test_schdule_find_gaps_2nodes()
     test_across_nodes_even_split()
