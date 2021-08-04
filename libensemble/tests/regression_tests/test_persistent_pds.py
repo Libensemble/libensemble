4bf92f63d88736f740574307d84d475b5e4c3b7e
Caleb GT <calebju4@gatech.edu>
2021-08-04 15:04:23 -0500

    Caller function for prima-dual sliding working

--- libensemble/tests/regression_tests/test_persistent_pds.py
+++ libensemble/tests/regression_tests/test_persistent_pds.py
@@ -21,10 +21,13 @@ import scipy.sparse as spp
 
 from libensemble.libE import libE
 # from libensemble.sim_funcs.chwirut2 import chwirut_eval as sim_f
-# from libensemble.sim_funcs.geomedian import geomedian_eval as sim_f
+from libensemble.sim_funcs.geomedian import geomedian_eval as sim_f
 # from libensemble.sim_funcs.convex_funnel import convex_funnel_eval as sim_f
 # from libensemble.sim_funcs.alt_rosenbrock import alt_rosenbrock_eval as sim_f
-from libensemble.sim_funcs.rosenbrock import rosenbrock_eval as sim_f
+# from libensemble.sim_funcs.rosenbrock import rosenbrock_eval as sim_f
+# from libensemble.sim_funcs.linear_regression import linear_regression_eval as sim_f
+# from libensemble.sim_funcs.logistic_regression import logistic_regression_eval as sim_f
+# from libensemble.sim_funcs.nesterov_quadratic import nesterov_quadratic_eval as sim_f
 from libensemble.gen_funcs.persistent_pds import opt_slide as gen_f
 from libensemble.alloc_funcs.start_persistent_consensus import start_consensus_persistent_gens as alloc_f
 from libensemble.tools import parse_args, save_libE_output, add_unique_random_streams
@@ -36,8 +39,8 @@ nworkers, is_manager, libE_specs, _ = parse_args()
 if nworkers < 2:
     sys.exit("Cannot run with a persistent worker if only one worker -- aborting...")
 
-m = 2  # must match with m in sim_f
-n = 4
+m = 10
+n = 9
 num_gens = 2
 
 sim_specs = {'sim_f': sim_f,
@@ -56,12 +59,11 @@ gen_specs = {'gen_f': gen_f,
                      ('get_grad', bool),
                      ],
              'user': {
-                      'lb' : -np.ones(n),
-                      'ub' : np.ones(n),
+                      'lb' : np.zeros(n),
+                      'ub' : np.zeros(n),
                       }
              }
 
-# TODO: Do we need ret_to_gen?
 alloc_specs = {'alloc_f': alloc_f, 
                'out'    : [], 
                'user'   : {'m': m,
@@ -69,25 +71,48 @@ alloc_specs = {'alloc_f': alloc_f,
                            },
                }
 
+# Problem definition
 k = 1
-A = spp.eye(2)  - get_k_reach_chain_matrix(num_gens,k)
+A = spp.diags([1,1]) - get_k_reach_chain_matrix(num_gens,k)
 lam_max = np.amax(la.eig(A.toarray())[0])
+np.random.seed(0)
+
+if True: # linear regression
+    X = np.array([np.random.normal(loc=0, scale=1.0, size=n) for _ in range(m)]).T
+    y = np.dot(X.T, np.ones(n)) + np.cos(np.dot(X.T, np.ones(n))) + np.random.normal(loc=0, scale=0.25, size=m)
+    c = 0.1
+    X_norms = la.norm(X, ord=2, axis=0)**2
+
+    L = (2/m)*(np.amax(X_norms)+c)
+if True: # logistic regression
+    y = np.append(2*np.ones(m//2), np.zeros(m-m//2))-1
+    X = np.array([np.random.normal(loc=y[i]*np.ones(n), scale=1.0, size=n) for i in range(m)]).T
+    c = 0.1
+
+    XXT_sum = np.outer(X[:,0], X[:,0])
+    for i in range(1,m):
+        XXT_sum += np.outer(X[:,i],X[:,i])
+    eig_max = np.amax(la.eig(XXT_sum)[0].real)
+    L = eig_max/m
+if True: # geometric median
+    B = np.random.random((m,n))
+    B = np.ones((m,n))
+    L = 1
 
 persis_info = {}
 persis_info['print_progress'] = 0
 persis_info['A'] = A
-persis_info['params'] = {
+persis_info['gen_params'] = {
                 'mu': 0,      # strong convexity term
-                'L': 1,       # Lipschitz smoothness
+                'L': L,       # Lipschitz smoothness
                 'Vx_0x': 0.5*n**0.5, # Bregman divergence of x_0 and x_*
-                'eps': 25e-5,   # error / tolerance
+                'eps': 1e-3,   # error / tolerance
                 'A_norm': lam_max # ||A \otimes I||_2 = ||A||_2
                 }
-
+# persis_info['sim_params'] = { 'X': X, 'y': y, 'c': c }
+persis_info['sim_params'] = { 'B': B }
 persis_info = add_unique_random_streams(persis_info, nworkers + 1)
 
-# exit_criteria = {'gen_max': 200, 'elapsed_wallclock_time': 300, 'stop_val': ('f', 3000)}
-# exit_criteria = {'sim_max': 50000}
 exit_criteria = {'elapsed_wallclock_time': 600}
 
 # Perform the run
