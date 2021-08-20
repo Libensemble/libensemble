d9d22726d3166911599501868c6d267a12f98503
shudson <shudson@anl.gov>
2021-08-19 19:51:29 -0500

    Hoist batch give option

--- libensemble/alloc_funcs/start_only_persistent.py
+++ libensemble/alloc_funcs/start_only_persistent.py
@@ -45,6 +45,7 @@ def only_persistent_gens(W, H, sim_specs, gen_specs, alloc_specs, persis_info):
     user = alloc_specs.get('user', {})
     active_recv_gen = user.get('active_recv_gen', False)  # Persistent gen can handle irregular communications
     init_sample_size = user.get('init_sample_size', 0)   # Always batch return until this many evals complete
+    batch_give = gen_specs['user'].get('give_all_with_same_priority', False)  # SH TODO: Should this be gen_specs not alloc_specs?
 
     # Asynchronous return to generator
     async_return = user.get('async_return', False) and sum(H['returned']) >= init_sample_size
@@ -68,6 +69,9 @@ def only_persistent_gens(W, H, sim_specs, gen_specs, alloc_specs, persis_info):
                                  persistent=True, active_recv=active_recv_gen)
                 H['given_back'][point_ids] = True
 
+    # SH TODO - note - on way to moving all direct H operations to alloc support - do we want to do this?
+    #                  may not want to have some here and some there!
+
     # SH TODO: Now the give_sim_work_first bit
     task_avail = ~H['given'] & ~H['cancel_requested']
     avail_workers = support.avail_worker_ids(persistent=False, zero_resource_workers=False)
@@ -76,9 +80,7 @@ def only_persistent_gens(W, H, sim_specs, gen_specs, alloc_specs, persis_info):
         if not np.any(task_avail):
             break
 
-        batch_give = gen_specs['user'].get('give_all_with_same_priority', False)
         sim_ids_to_send = support.points_by_priority(points_avail=task_avail, batch=batch_give)
-
         try:
             support.sim_work(Work, wid, sim_specs['in'], sim_ids_to_send, persis_info.get(wid))
         except InsufficientFreeResources:
