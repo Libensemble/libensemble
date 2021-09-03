188ace2aaae694091bef403997697ca09dbb3d98
Jeffrey Larson <jmlarson@anl.gov>
2021-09-03 09:51:02 -0500

    Asserting l1 regularization for svm sim_f

--- libensemble/sim_funcs/svm.py
+++ libensemble/sim_funcs/svm.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def EvaluateFunction(theta, component, X, b, c, reg):
     """
-    Evaluates linear regression with l2 regularization
+    Evaluates svm with l1 regularization
     """
     i = component
     m = len(b)
@@ -13,19 +13,21 @@ def EvaluateFunction(theta, component, X, b, c, reg):
 
     f_i = max(0, 1-b_i*np.dot(X_i, theta))
 
-    if reg is None:
-        reg_val = 0
-    elif reg == 'l1':
-        reg_val = (c/m) * np.sum(np.abs(theta))
-    else:
-        reg_val = (c/m) * np.dot(theta, theta)
+    assert reg == 'l1', "Only l1 regularization allowed"
+
+    # if reg is None:
+    #     reg_val = 0
+    # elif reg == 'l1':
+    reg_val = (c/m) * np.sum(np.abs(theta))
+    # else:
+    #     reg_val = (c/m) * np.dot(theta, theta)
 
     return f_i+reg_val
 
 
 def EvaluateJacobian(theta, component, X, b, c, reg):
     """
-    Evaluates linear regression with l2 regularization
+    Evaluates svm with l1 regularization
     """
 
     i = component
@@ -41,12 +43,14 @@ def EvaluateJacobian(theta, component, X, b, c, reg):
     else:
         df_i = -b_i * X_i
 
-    if reg is None:
-        reg_val = 0
-    elif reg == 'l1':
-        reg_val = (c/m) * np.sign(theta)
-    else:
-        reg_val = (2*c/m) * theta
+    assert reg == 'l1', "Only l1 regularization allowed"
+
+    # if reg is None:
+    #     reg_val = 0
+    # elif reg == 'l1':
+    reg_val = (c/m) * np.sign(theta)
+    # else:
+    #     reg_val = (2*c/m) * theta
 
     return df_i+reg_val
 
