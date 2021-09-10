b258caf59cabebc9bcb6a83647d1ace2dcc2ab3f
shudson <shudson@anl.gov>
2021-09-10 16:58:54 -0500

    Clean up alloc_support

--- libensemble/tools/alloc_support.py
+++ libensemble/tools/alloc_support.py
@@ -1,23 +1,23 @@
 import numpy as np
 from libensemble.message_numbers import EVAL_SIM_TAG, EVAL_GEN_TAG
 from libensemble.resources.resources import Resources
-from libensemble.resources.scheduler import ResourceScheduler, InsufficientFreeResources
+from libensemble.resources.scheduler import ResourceScheduler, InsufficientFreeResources  # noqa: F401
 
-# General aim is to not allow user options (via {sim/gen/alloc}_specs) to be hidden in here.
-# One exception is scheduler_opts... Now I'm extracting outside and passing in.
-# persis_info['gen_resources'] is here. Could move outside, but then have to pass requested rsets through to gen_work.....
-# H and W are passed in for convenience on init. They should be used read-only.
 
 class AllocException(Exception):
-    "Raised for any exception in the alloc support"
+    """Raised for any exception in the alloc support"""
 
 
 class AllocSupport:
-    """A helper class to assist with writing allocation functions, containing methods for
-    common operations like populating work units, determining which workers are available,
-    evaluating what values need to be distributed to workers, and others.
+    """A helper class to assist with writing allocation functions.
+
+    This class contains methods for common operations like populating work
+    units, determining which workers are available, evaluating what values
+    need to be distributed to workers, and others.
+
     Note that since the ``alloc_f`` is called periodically by the Manager, this
-    class instance (if used) will be recreated/destroyed on each loop."""
+    class instance (if used) will be recreated/destroyed on each loop.
+    """
 
     gen_counter = 0
 
@@ -37,7 +37,6 @@ class AllocSupport:
         :param user_resources: Optional, A user supplied ``resources`` object.
         :param user_scheduler: Optional, A user supplied ``user_scheduler`` object.
         """
-
         self.W = W
         self.H = H
         self.persis_info = persis_info
@@ -48,38 +47,35 @@ class AllocSupport:
             wrk_resources = self.resources.resource_manager
             self.sched = user_scheduler or ResourceScheduler(wrk_resources, scheduler_opts)
 
-
     def assign_resources(self, rsets_req):
         """Schedule resource sets to a work record if possible.
 
-        Raises ``InsufficientFreeResources`` if the
-        required resources are not currently available, or
-        ``InsufficientResourcesError`` if the required resources
-        do not exist.
+        For default scheduler, if more than one group (node) is required,
+        will try to find even split, otherwise allocates whole nodes.
+
+        Raises ``InsufficientFreeResources`` if the required resources are not
+        currently available, or ``InsufficientResourcesError`` if the required
+        resources do not exist.
 
         :param rsets_req: Int. Number of resource sets to request.
         :returns: List of Integers. Resource set indices assigned.
-
         """
-
         rset_team = None
         if self.resources is not None:
             rset_team = self.sched.assign_resources(rsets_req)
         return rset_team
 
-
-    # SH TODO: Decision on these functions - Keep as is / make static / init with W (use self.W)
     def avail_worker_ids(self, persistent=None, active_recv=False, zero_resource_workers=None):
         """Returns available workers as a list of IDs, filtered by the given options.
 
-        :param persistent: Optional int. Only return workers with given ``persis_state`` (1 for sim, 2 for gen).
+        :param persistent: Optional int. Only return workers with given ``persis_state`` (1=sim, 2=gen).
         :param active_recv: Optional Boolean. Only return workers with given active_recv state.
-        :param zero_resource_workers: Optional Boolean. If specified, only return workers that require no resources
+        :param zero_resource_workers: Optional Boolean. Only return workers that require no resources
+        :returns: List of worker IDs
 
         If there are no zero resource workers defined, then the ``zero_resource_workers`` argument will
         be ignored.
         """
-
         def fltr(wrk, field, option):
             """Filter by condition if supplied"""
             if option is None:
@@ -100,14 +96,14 @@ class AllocSupport:
 
         def fltr_recving():
             if active_recv:
-                return wrk['active_recv']  # SH TODO: must be persistent - could check here
+                return wrk['active_recv']
             else:
                 return not wrk['active']
 
         if active_recv and not persistent:
             raise AllocException("Cannot ask for non-persistent active receive workers")
 
-        # SH if there are no zero resource workers - then ignore zrw (i.e. use only if they exist)
+        # If there are no zero resource workers - then ignore zrw (i.e. use only if they exist)
         no_zrw = not any(self.W['zero_resource_worker'])
         wrks = []
         for wrk in self.W:
@@ -115,71 +111,56 @@ class AllocSupport:
                 wrks.append(wrk['worker_id'])
         return wrks
 
-
     def count_gens(self):
         """Returns the number of active generators."""
         return sum(self.W['active'] == EVAL_GEN_TAG)
-
 
     def test_any_gen(self):
         """Returns ``True`` if a generator worker is active."""
         return any(self.W['active'] == EVAL_GEN_TAG)
 
-
     def count_persis_gens(self):
         """Return the number of active persistent generators."""
         return sum(self.W['persis_state'] == EVAL_GEN_TAG)
-
 
     def sim_work(self, Work, wid, H_fields, H_rows, persis_info, **libE_info):
         """Add sim work record to given ``Work`` dictionary.
 
         :param Work: :doc:`Work dictionary<../data_structures/work_dict>`
-        :param wid: Worker ID.
+        :param wid: Int. Worker ID.
         :param H_fields: Which fields from :ref:`H<datastruct-history-array>` to send
-        :param H_rows: Which rows of ``H`` to send. Oftentimes, these are non-given, non-cancelled points (``~H['given'] & ~H['cancel_requested']``)
-        :param persis_info: Current :ref:`persis_info<datastruct-persis-info>` dictionary
+        :param H_rows: Which rows of ``H`` to send.
+        :param persis_info: Worker specific :ref:`persis_info<datastruct-persis-info>` dictionary
 
         :returns: None, but ``Work`` is updated.
 
         Additional passed parameters are inserted into ``libE_info`` in the resulting work record.
         """
 
-        #Should distinguish that it is persis_info for this worker (e.g. wpersis_info ?)
-        #why brackets round np.max???
-
-        # Honor rset_team if passed (do we want to check those rsets have been assigned?)
+        # Honor rset_team if passed
         rset_team = libE_info.get('rset_team', None)
 
         if self.manage_resources and rset_team is None:
-            #import pdb;pdb.set_trace()
             if self.W[wid-1]['persis_state']:
                 # IF in persistent state - do not give more resources.
                 libE_info['rset_team'] = []
             else:
                 num_rsets_req = (np.max(self.H[H_rows]['resource_sets']))
-                #print('\nrset_team being called for sim. Requesting {} rsets'.format(num_rsets_req))
 
                 # If more than one group (node) required, finds even split, or allocates whole nodes
                 rset_team = self.assign_resources(num_rsets_req)
 
-                # Assign points to worker and remove from task_avail list.
-                #print('resource team {} for SIM assigned to worker {}'.format(rset_team, wid), flush=True)
+                # print('resource team {} for SIM assigned to worker {}'.format(rset_team, wid), flush=True)
                 libE_info['rset_team'] = rset_team
 
         H_fields = AllocSupport._check_H_fields(H_fields)
         libE_info['H_rows'] = np.atleast_1d(H_rows)
 
         Work[wid] = {'H_fields': H_fields,
-                   'persis_info': persis_info,
-                   'tag': EVAL_SIM_TAG,
-                   'libE_info': libE_info}
+                     'persis_info': persis_info,
+                     'tag': EVAL_SIM_TAG,
+                     'libE_info': libE_info}
 
-        #print('Work is {}\n'.format(Work[wid]))
-        #print('Packed for worker: {}. Resource team for sim: {}\n'.format(wid, rset_team), flush=True)
-
-
-    # SH TODO: Find/extract commonaility of sim/gen_work.
     def gen_work(self, Work, wid, H_fields, H_rows, persis_info, **libE_info):
         """Add gen work record to given ``Work`` dictionary.
 
@@ -187,33 +168,29 @@ class AllocSupport:
         :param wid: Worker ID.
         :param H_fields: Which fields from :ref:`H<datastruct-history-array>` to send
         :param H_rows: Which rows of ``H`` to send.
-        :param persis_info: Current :ref:`persis_info<datastruct-persis-info>` dictionary
+        :param persis_info: Worker specific :ref:`persis_info<datastruct-persis-info>` dictionary
 
         :returns: None, but ``Work`` is updated.
 
         Additional passed parameters are inserted into ``libE_info`` in the resulting work record.
         """
 
-        # Honor rset_team if passed (do we want to check those rsets have been assigned?)
+        # Honor rset_team if passed
         rset_team = libE_info.get('rset_team', None)
 
         if self.manage_resources and rset_team is None:
             if self.W[wid-1]['persis_state']:
                 # IF in persistent state - do not give more resources.
-                # persis gen/sims requset for more resources would be dealt with separately.
                 # SH TODO: This should be done with sim also - but add when adding persistent sims...
                 libE_info['rset_team'] = []
             else:
-                # SH TODO: How would you provide resources to a gen? Maybe via persis_info if variable?
-                #          Need test where gen_resources is not zero!
-                gen_resources = self.persis_info.get('gen_resources', 0)  # This is manager persis_info
+                gen_resources = self.persis_info.get('gen_resources', 0)
                 rset_team = self.assign_resources(gen_resources)
                 # Even if empty list, presence of non-None rset_team stops manager giving default resources
                 libE_info['rset_team'] = rset_team
-                #print('resource team {} for GEN assigned to worker {}'.format(rset_team, wid), flush=True)
+                # print('resource team {} for GEN assigned to worker {}'.format(rset_team, wid), flush=True)
 
         # Must come after resources - as that may exit with InsufficientFreeResources
-        # SH TODO: Review counter - what if gen work package is cancelled!!!
         AllocSupport.gen_counter += 1  # Count total gens
         libE_info['gen_count'] = AllocSupport.gen_counter
 
@@ -221,47 +198,14 @@ class AllocSupport:
         libE_info['H_rows'] = np.atleast_1d(H_rows)
 
         Work[wid] = {'H_fields': H_fields,
-                   'persis_info': persis_info,
-                   'tag': EVAL_GEN_TAG,
-                   'libE_info': libE_info}
-
-
-    #def get_points_to_evaluate(self):
-        #"""Return points yet to be evaluated"""
-        #return ~self.H['given'] & ~self.H['cancel_requested']
-
-
-    #def get_evaluated_points(self, gen=None):
-        #"""Return points that have returned from sim but not yet been given back to gen."""
-        #pt_filter = True
-        #if gen is not None:
-            #gen_inds = (self.H['gen_worker'] == gen)
-            #pt_filter = gen_inds
-        #return np.logical_and.reduce((self.H['returned'], ~self.H['given_back'], pt_filter))
-
-
-    #def all_returned(self, gen=None):
-        #"""Check if all expected points have returned from sim
-
-        #:param H: A :doc:`history array<../data_structures/history_array>`
-        #:param pt_filter: Optional boolean array filtering expected returned points: Default: All True
-
-        #:returns: Boolean. True if all expected points have been returned
-        #"""
-        #pt_filter = True
-        #if gen is not None:
-            #gen_inds = (self.H['gen_worker'] == gen)
-            #pt_filter = gen_inds
-
-        ## Exclude cancelled points that were not already given out
-        #excluded_points = self.H['cancel_requested'] & ~self.H['given']
-        #return np.all(self.H['returned'][pt_filter & ~excluded_points])
-
+                     'persis_info': persis_info,
+                     'tag': EVAL_GEN_TAG,
+                     'libE_info': libE_info}
 
     def all_returned(self, pt_filter=None, low_bound=None):
         """Returns ``True`` if all expected points have returned from sim
 
-        :param pt_filter: Optional boolean array filtering expected returned points in H.
+        :param pt_filter: Optional boolean array filtering expected returned points in ``H``.
         :param low_bound: Optional lower bound for testing all returned.
         :returns: True if all expected points have been returned
         """
@@ -283,7 +227,6 @@ class AllocSupport:
         excluded_points = H['cancel_requested'] & ~H['given']
         return np.all(H['returned'][pfilter & ~excluded_points])
 
-
     def points_by_priority(self, points_avail, batch=False):
         """Returns indices of points to give by priority
 
@@ -302,12 +245,11 @@ class AllocSupport:
             q_inds = 0
         return np.nonzero(points_avail)[0][q_inds]
 
-
     @staticmethod
     def _check_H_fields(H_fields):
         """Ensure no duplicates in H_fields"""
         if len(H_fields) != len(set(H_fields)):
-            #logger.debug("Removing duplicate field when packing work request".format(H_fields))
+            # logger.debug("Removing duplicate field when packing work request".format(H_fields))
             H_fields = list(set(H_fields))
-            #H_fields = list(OrderedDict.fromkeys(H_fields))  # Maintain order
+            # H_fields = list(OrderedDict.fromkeys(H_fields))  # Maintain order
         return H_fields
