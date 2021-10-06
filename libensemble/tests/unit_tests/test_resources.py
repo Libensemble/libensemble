5ad44355ac2a812d457590e5d4935fd7426684ef
shudson <shudson@anl.gov>
2021-10-06 09:36:31 -0500

    fixup unit test

--- libensemble/tests/unit_tests/test_resources.py
+++ libensemble/tests/unit_tests/test_resources.py
@@ -573,27 +573,28 @@ def test_get_local_nodelist_distrib_mode_uneven_split():
 
 def test_map_workerid_to_index():
     num_workers = 4
+    num_rsets = 4
 
     zero_resource_list = []
-    index_list = ResourceManager.get_index_list(num_workers, zero_resource_list)
+    index_list = ResourceManager.get_index_list(num_workers, num_rsets, zero_resource_list)
     for workerID in range(1, num_workers+1):
         index = index_list[workerID-1]
         assert index == workerID - 1, "index incorrect. Received: " + str(index)
 
     zero_resource_list = [1]
-    index_list = ResourceManager.get_index_list(num_workers, zero_resource_list)
+    index_list = ResourceManager.get_index_list(num_workers, num_rsets, zero_resource_list)
     for workerID in range(2, num_workers+1):
         index = index_list[workerID-1]
         assert index == workerID - 2, "index incorrect. Received: " + str(index)
 
     zero_resource_list = [1, 2]
-    index_list = ResourceManager.get_index_list(num_workers, zero_resource_list)
+    index_list = ResourceManager.get_index_list(num_workers, num_rsets, zero_resource_list)
     for workerID in range(3, num_workers+1):
         index = index_list[workerID-1]
         assert index == workerID - 3, "index incorrect. Received: " + str(index)
 
     zero_resource_list = [1, 3]
-    index_list = ResourceManager.get_index_list(num_workers, zero_resource_list)
+    index_list = ResourceManager.get_index_list(num_workers, num_rsets, zero_resource_list)
 
     workerID = 2
     index = index_list[workerID-1]
