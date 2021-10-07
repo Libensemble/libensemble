bcde610805845bd8f4cbff26fbe0e8762b96a682
shudson <shudson@anl.gov>
2021-10-07 17:09:27 -0500

    Fix spacing in persis_return message

--- libensemble/tools/tools.py
+++ libensemble/tools/tools.py
@@ -55,7 +55,7 @@ _PERSIS_RETURN_WARNING = \
      "A persistent worker has returned history data on shutdown. This data is\n" +
      "not currently added to the manager's history to avoid possibly overwriting, but\n" +
      "will be added to the manager's history in a future release. If you want to\n" +
-     "overwrite/append, you can set the libE_specs option ``use_persis_return_gen``" +
+     "overwrite/append, you can set the libE_specs option ``use_persis_return_gen``\n" +
      "or ``use_persis_return_sim``"
      '\n' + 79*'*' + '\n\n')
 
