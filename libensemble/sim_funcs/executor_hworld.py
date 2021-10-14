47677a3712dc4d127271db35406d38980c39beb6
jlnav <jnavarro@anl.gov>
2021-10-14 13:05:01 -0500

    dont import ensemble if ModuleNotFound, use custom polling loop for balsams

--- libensemble/sim_funcs/executor_hworld.py
+++ libensemble/sim_funcs/executor_hworld.py
@@ -113,7 +113,7 @@ def executor_hworld(H, persis_info, sim_specs, libE_info):
             calc_status = TASK_FAILED
 
     else:
-        if returned_count >= 2:
+        if returned_count >= 2 and not use_balsam:
             calc_status = exctr.polling_loop(task, timeout=timeout, delay=0.3, poll_manager=True)
             if returned_count == 2 and task.stdout_exists() and 'Error' in task.read_stdout():
                 calc_status = WORKER_KILL_ON_ERR
