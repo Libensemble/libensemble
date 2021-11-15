2c82c45115eb73978609a7a2dfcd7facd744e468
jlnav <jnavarro@anl.gov>
2021-11-15 16:46:57 -0600

    test mpi4py separately?

--- libensemble/tests/unit_tests/test_mpi4py.py
+++ libensemble/tests/unit_tests/test_mpi4py.py
@@ -1,5 +1,9 @@
 def test_mpi4py():
-    from libensemble.sim_funcs.helloworld import size, rank, name
+    from mpi4py import MPI
+
+    size = MPI.COMM_WORLD.Get_size()
+    rank = MPI.COMM_WORLD.Get_rank()
+    name = MPI.Get_processor_name()
 
     assert size == 1
     assert rank == 0
