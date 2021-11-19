b81923ab1499d2c7a81eec8b889163cd85def8f4
Remi Lehe <remi.lehe@normalesup.org>
2021-11-19 09:48:11 -0800

    Add multi-task example

--- libensemble/tests/scaling_tests/persistent_gp/run_example.py
+++ libensemble/tests/scaling_tests/persistent_gp/run_example.py
@@ -14,9 +14,18 @@ from libensemble.executors.mpi_executor import MPIExecutor
 from libensemble.tools import save_libE_output, add_unique_random_streams
 from libensemble.tools import parse_args
 from libensemble.message_numbers import WORKER_DONE
-from libensemble.gen_funcs.persistent_gp import persistent_gp_mf_gen_f
+from libensemble.gen_funcs.persistent_ax_multitask import persistent_gp_mt_ax_gen_f
 
 nworkers, is_master, libE_specs, _ = parse_args()
+
+mt_params = {
+    'name_hifi': 'expensive_model',
+    'name_lofi': 'cheap_model',
+    'n_init_hifi': 4,
+    'n_init_lofi': 4,
+    'n_opt_hifi': 2,
+    'n_opt_lofi': 4,
+}
 
 def run_simulation(H, persis_info, sim_specs, libE_info):
     # Extract input parameters
@@ -24,7 +33,11 @@ def run_simulation(H, persis_info, sim_specs, libE_info):
     x0 = values[0]
     x1 = values[1]
     # Extract fidelity parameter
-    z = H['z'][0]
+    task = H['task'][0]
+    if task == 'expensive_model':
+        z = 8
+    elif task == 'cheap_model':
+        z = 1
 
     libE_output = np.zeros(1, dtype=sim_specs['out'])
     calc_status = WORKER_DONE
@@ -36,25 +49,24 @@ def run_simulation(H, persis_info, sim_specs, libE_info):
 
 sim_specs = {
     'sim_f': run_simulation,
-    'in': ['x', 'z'],
+    'in': ['x', 'task'],
     'out': [ ('f', float) ],
 }
 
 gen_specs = {
     # Generator function. Will randomly generate new sim inputs 'x'.
-    'gen_f': persistent_gp_mf_gen_f,
+    'gen_f': persistent_gp_mt_ax_gen_f,
     # Generator input. This is a RNG, no need for inputs.
-    'in': ['sim_id', 'x', 'f', 'z'],
-    'persis_in': ['sim_id', 'x', 'f', 'z'],
+    'in': ['sim_id', 'x', 'f', 'task'],
+    'persis_in': ['sim_id', 'x', 'f', 'task'],
     'out': [
         # parameters to input into the simulation.
         ('x', float, (2,)),
-        ('z', float),
+        ('task', str, max([len(mt_params['name_hifi']), len(mt_params['name_lofi'])])),
         ('resource_sets', int)
     ],
     'user': {
         'range': [1, 8],
-        'cost_func': lambda z: z[0],
         # Total max number of sims running concurrently.
         'gen_batch_size': nworkers-1,
         # Lower bound for the n parameters.
@@ -63,18 +75,19 @@ gen_specs = {
         'ub': np.array([ 15, 15 ])
     }
 }
+gen_specs['user'] = {**gen_specs['user'], **mt_params}
 
 alloc_specs = {
     'alloc_f': only_persistent_gens,
     'out': [('given_back', bool)],
-    'user': {'async_return': True}
+    'user': {'async_return': False}
 }
 
 # libE logger
 logger.set_level('INFO')
 
 # Exit criteria
-exit_criteria = {'sim_max': 10}  # Exit after running sim_max simulations
+exit_criteria = {'sim_max': 20}  # Exit after running sim_max simulations
 
 # Create a different random number stream for each worker and the manager
 persis_info = add_unique_random_streams({}, nworkers + 1)
