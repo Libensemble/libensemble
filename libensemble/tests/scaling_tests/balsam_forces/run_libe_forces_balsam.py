ae3755d3eef24f2e9148d2bfcd79363014989df3
Jeffrey Larson <jmlarson@anl.gov>
2022-02-08 08:20:05 -0600

    Black on forces balsam runscript

--- libensemble/tests/scaling_tests/balsam_forces/run_libe_forces_balsam.py
+++ libensemble/tests/scaling_tests/balsam_forces/run_libe_forces_balsam.py
@@ -12,22 +12,30 @@ forces.from_yaml('balsam_forces.yaml')
 
 class RemoteForces(ApplicationDefinition):
     site = 'three'
-    command_template = \
-        '/Users/jnavarro/Desktop/libensemble/' + \
-        'libensemble/libensemble/tests/scaling_tests/balsam_forces/forces.x' + \
-        ' {{sim_particles}} {{sim_timesteps}} {{seed}} {{kill_rate}}' + \
-        ' > out.txt 2>&1'
+    command_template = (
+        '/Users/jnavarro/Desktop/libensemble/'
+        + 'libensemble/libensemble/tests/scaling_tests/balsam_forces/forces.x'
+        + ' {{sim_particles}} {{sim_timesteps}} {{seed}} {{kill_rate}}'
+        + ' > out.txt 2>&1'
+    )
 
 
 exctr = NewBalsamMPIExecutor()
 exctr.register_app(RemoteForces, app_name='forces')
-exctr.submit_allocation(site_id=239, num_nodes=1, wall_time_min=30,
-                        queue='local', project='local')
+exctr.submit_allocation(
+    site_id=239,
+    num_nodes=1,
+    wall_time_min=30,
+    queue='local',
+    project='local',
+)
 
-forces.gen_specs['user'].update({
-    'lb': np.array([0]),
-    'ub': np.array([32767])
-})
+forces.gen_specs['user'].update(
+    {
+        'lb': np.array([0]),
+        'ub': np.array([32767]),
+    }
+)
 
 forces.persis_info.add_random_streams()
 
