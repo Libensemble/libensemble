955e9fc5b70853c9f533fd6a9d3fd5aa2bfbbaac
jlnav <jnavarro@anl.gov>
2022-02-09 11:13:15 -0600

    experiment with only manager process defining app, also not re-defining if already exists

--- libensemble/tests/scaling_tests/balsam_forces/run_libe_forces_balsam.py
+++ libensemble/tests/scaling_tests/balsam_forces/run_libe_forces_balsam.py
@@ -7,36 +7,38 @@ from libensemble.executors import NewBalsamMPIExecutor
 from balsam.api import ApplicationDefinition
 
 forces = Ensemble()
-forces.from_yaml('balsam_forces.yaml')
+forces.from_yaml("balsam_forces.yaml")
 
-
-class RemoteForces(ApplicationDefinition):
-    site = 'jln_theta'
-    command_template = (
-        '/home/jnavarro/'
-        + 'libensemble/libensemble/tests/scaling_tests/balsam_forces/forces.x'
-        + ' {{sim_particles}} {{sim_timesteps}} {{seed}} {{kill_rate}}'
-        + ' > out.txt 2>&1'
-    )
-
-
-exctr = NewBalsamMPIExecutor()
-exctr.register_app(RemoteForces, app_name='forces')
-exctr.submit_allocation(
-    site_id=246,
-    num_nodes=1,
-    wall_time_min=30,
-    queue='debug-cache-quad',
-    project='CSC250STMS07',
-)
-
-forces.gen_specs['user'].update(
+forces.gen_specs["user"].update(
     {
-        'lb': np.array([0]),
-        'ub': np.array([32767]),
+        "lb": np.array([0]),
+        "ub": np.array([32767]),
     }
 )
 
 forces.persis_info.add_random_streams()
+
+if forces.is_manager():
+    RemoteForces = ApplicationDefinition.load_by_site("jln_theta").get("RemoteForce")
+    if not RemoteForces:
+
+        class RemoteForces(ApplicationDefinition):
+            site = "jln_theta"
+            command_template = (
+                "/home/jnavarro/"
+                + "libensemble/libensemble/tests/scaling_tests/balsam_forces/forces.x"
+                + " {{sim_particles}} {{sim_timesteps}} {{seed}} {{kill_rate}}"
+                + " > out.txt 2>&1"
+            )
+
+    exctr = NewBalsamMPIExecutor()
+    exctr.register_app(RemoteForces, app_name="forces")
+    exctr.submit_allocation(
+        site_id=246,
+        num_nodes=1,
+        wall_time_min=30,
+        queue="debug-cache-quad",
+        project="CSC250STMS07",
+    )
 
 forces.run()
