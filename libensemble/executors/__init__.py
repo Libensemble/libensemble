40085c4fc2760934714f8fe54c99b238ea91ea8d
jlnav <jnavarro@anl.gov>
2022-03-02 11:13:35 -0600

    yet again rearrange pkg_resources logic

--- libensemble/executors/__init__.py
+++ libensemble/executors/__init__.py
@@ -4,14 +4,14 @@ from libensemble.executors.mpi_executor import MPIExecutor
 import os
 import pkg_resources
 
-if pkg_resources.get_distribution('balsam-flow'):
-    try:
+try:
+    if pkg_resources.get_distribution('balsam-flow'):
         if 'BALSAM_DB_PATH' in os.environ:
             from libensemble.executors.balsam_executor import BalsamMPIExecutor
         else:
             from libensemble.executors.new_balsam_executor import NewBalsamExecutor
 
-    except ModuleNotFoundError:  # One version of Balsam installed, but not the other
-        pass
+except ModuleNotFoundError:  # One version of Balsam installed, but not the other
+    pass
 
 __all__ = ['BalsamMPIExecutor', 'Executor', 'MPIExecutor', 'NewBalsamExecutor']
