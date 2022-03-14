fb25c854e7bf4f1d3875238240f883b8fe36c82c
jlnav <jnavarro@anl.gov>
2022-03-14 17:07:41 -0500

    reformatting

--- examples/tutorials/forces_with_executor/tutorial_forces_simf_simple.py
+++ examples/tutorials/forces_with_executor/tutorial_forces_simf_simple.py
@@ -1,27 +1,25 @@
-import os
-import time
 import numpy as np
 
 from libensemble.executors.executor import Executor
-from libensemble.message_numbers import WORKER_DONE, WORKER_KILL, TASK_FAILED
+from libensemble.message_numbers import WORKER_DONE, TASK_FAILED
 
 MAX_SEED = 32767
+
 
 def run_forces(H, persis_info, sim_specs, libE_info):
     calc_status = 0
 
-    sim_particles_in = H['x'][0][0]
-    seed = int(np.rint(sim_particles_in))
+    particles = str(int(H["x"][0][0]))
 
-    particles = str(int(sim_particles_in))
-    args = particles + ' ' + str(10) + ' ' + particles
+    # num particles, timesteps, also using num particles as seed
+    args = particles + " " + str(10) + " " + particles
 
     exctr = Executor.executor
-    task = exctr.submit(app_name='forces', app_args=args, wait_on_start=True)
+    task = exctr.submit(app_name="forces", app_args=args, wait_on_start=True)
     task.wait(timeout=60)
 
     # Stat file to check for bad runs
-    statfile = 'forces{}.stat'.format(particles)
+    statfile = "forces{}.stat".format(particles)
 
     try:
         data = np.loadtxt(statfile)
@@ -31,8 +29,8 @@ def run_forces(H, persis_info, sim_specs, libE_info):
         final_energy = np.nan
         calc_status = TASK_FAILED
 
-    outspecs = sim_specs['out']
+    outspecs = sim_specs["out"]
     output = np.zeros(1, dtype=outspecs)
-    output['energy'][0] = final_energy
+    output["energy"][0] = final_energy
 
     return output, persis_info, calc_status
--- examples/tutorials/forces_with_executor/tutorial_run_libe_forces_simple.py
+++ examples/tutorials/forces_with_executor/tutorial_run_libe_forces_simple.py
@@ -14,34 +14,39 @@ nworkers, is_manager, libE_specs, _ = parse_args()  # Convenience function
 exctr = MPIExecutor()
 
 # Normally would be pre-compiled
-if not os.path.isfile('forces.x'):
-    if os.path.isfile('build_forces.sh'):
+if not os.path.isfile("forces.x"):
+    if os.path.isfile("build_forces.sh"):
         import subprocess
-        subprocess.check_call(['./build_forces.sh'])
+
+        subprocess.check_call(["./build_forces.sh"])
 
 # Register simulation executable with executor
-sim_app = os.path.join(os.getcwd(), 'forces.x')
-exctr.register_app(full_path=sim_app, app_name='forces')
+sim_app = os.path.join(os.getcwd(), "forces.x")
+exctr.register_app(full_path=sim_app, app_name="forces")
 
 # State the sim_f, its arguments, output, and parameters (and their sizes)
-sim_specs = {'sim_f': run_forces,         # sim_f, imported above
-             'in': ['x'],                 # Name of input for sim_f
-             'out': [('energy', float)],  # Name, type of output from sim_f
-             }
+sim_specs = {
+    "sim_f": run_forces,  # sim_f, imported above
+    "in": ["x"],  # Name of input for sim_f
+    "out": [("energy", float)],  # Name, type of output from sim_f
+}
 
 # State the gen_f, its arguments, output, and necessary parameters.
-gen_specs = {'gen_f': uniform_random_sample,  # Generator function
-             'in': ['sim_id'],                # Generator input
-             'out': [('x', float, (1,))],     # Name, type and size of data from gen_f
-             'user': {'lb': np.array([1000]),             # User parameters for the gen_f
-                      'ub': np.array([3000]),
-                      'gen_batch_size': 8
-                      }
-             }
+gen_specs = {
+    "gen_f": uniform_random_sample,  # Generator function
+    "in": ["sim_id"],  # Generator input
+    "out": [("x", float, (1,))],  # Name, type and size of data from gen_f
+    "user": {
+        "lb": np.array([1000]),  # User parameters for the gen_f
+        "ub": np.array([3000]),
+        "gen_batch_size": 8,
+    },
+}
 
-exit_criteria = {'sim_max': 8}
+exit_criteria = {"sim_max": 8}
 
 persis_info = add_unique_random_streams({}, nworkers + 1)
 
-H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria,
-                            persis_info=persis_info, libE_specs=libE_specs)
+H, persis_info, flag = libE(
+    sim_specs, gen_specs, exit_criteria, persis_info=persis_info, libE_specs=libE_specs
+)
