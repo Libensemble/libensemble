a35d9944f80fa0411bd2318ad04da336f9794227
Jeffrey Larson <jmlarson@anl.gov>
2022-04-12 15:53:30 -0500

    Seeing if just a UserWarning is sufficient

--- libensemble/tests/regression_tests/test_persistent_gp_multitask_ax.py
+++ libensemble/tests/regression_tests/test_persistent_gp_multitask_ax.py
@@ -31,7 +31,7 @@ from libensemble.gen_funcs.persistent_ax_multitask import persistent_gp_mt_ax_ge
 import warnings
 
 # Ax uses a deprecated warn command.
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
 
 nworkers, is_manager, libE_specs, _ = parse_args()
 
