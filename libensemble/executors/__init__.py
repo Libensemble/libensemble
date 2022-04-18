46edbf0bd0b63f2c8cdfd1244e313b94606d04f7
jlnav <jnavarro@anl.gov>
2022-04-18 11:37:19 -0500

    fix executor init logic to resolve Balsam2 having new name on pypi

--- libensemble/executors/__init__.py
+++ libensemble/executors/__init__.py
@@ -1,17 +1,18 @@
 from libensemble.executors.executor import Executor
 from libensemble.executors.mpi_executor import MPIExecutor
 
-import os
 import pkg_resources
 
 try:
-    if pkg_resources.get_distribution('balsam-flow'):
-        if 'BALSAM_DB_PATH' in os.environ:
-            from libensemble.executors.legacy_balsam_executor import LegacyBalsamMPIExecutor
-        else:
-            from libensemble.executors.balsam_executor import BalsamExecutor
+    if pkg_resources.get_distribution("balsam-flow"):  # Balsam up through 0.5.0
+        from libensemble.executors.legacy_balsam_executor import LegacyBalsamMPIExecutor
+    if pkg_resources.get_distribution("balsam"):  # Balsam 0.7.0 onward (Balsam 2)
+        from libensemble.executors.balsam_executor import BalsamExecutor
 
-except (ModuleNotFoundError, pkg_resources.DistributionNotFound):  # One version of Balsam installed, but not the other
+except (
+    ModuleNotFoundError,
+    pkg_resources.DistributionNotFound,
+):  # One version of Balsam installed, but not the other
     pass
 
-__all__ = ['LegacyBalsamMPIExecutor', 'Executor', 'MPIExecutor', 'BalsamExecutor']
+__all__ = ["LegacyBalsamMPIExecutor", "Executor", "MPIExecutor", "BalsamExecutor"]
