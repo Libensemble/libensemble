b189a3be532cceb318a874bedd2ef2f71f5588dd
Jeffrey Larson <jmlarson@anl.gov>
2022-04-21 13:14:12 -0500

    Black this one test

--- libensemble/tests/unit_tests/test_history.py
+++ libensemble/tests/unit_tests/test_history.py
@@ -15,41 +15,58 @@ if tuple(np.__version__.split('.')) >= ('1', '15'):
 # - compare from npy file - stored
 
 
-fields = [('local_pt', '?'), ('num_active_runs', '<i8'), ('local_min', '?'), ('priority', '<f8'), ('x_on_cube', '<f8'), ('fvec', '<f8', (3,)), ('f', '<f8')]
-fields2 = [ ('g', '<f8'), ('x', '<f8'), ('priority', '<f8') ]
+fields = [
+    ('f', '<f8'),
+    ('fvec', '<f8', (3,)),
+    ('local_min', '?'),
+    ('local_pt', '?'),
+    ('num_active_runs', '<i8'),
+    ('priority', '<f8'),
+    ('x_on_cube', '<f8'),
+]
+fields2 = [('g', '<f8'), ('x', '<f8'), ('priority', '<f8')]
 
-wrs_H0 = np.zeros(3, dtype = libE_fields)
-wrs_H0[['sim_started','sim_ended']] = True
+wrs_H0 = np.zeros(3, dtype=libE_fields)
+wrs_H0[['sim_started', 'sim_ended']] = True
 wrs_H0['sim_ended_time'] = [1.1, 1.2, 1.3]
 wrs_H0['sim_id'] = range(3)
 wrs_H0['sim_started_time'] = [0.1, 0.2, 0.3]
 wrs_H0['sim_worker'] = [1, 2, 3]
 
-exp_H0_H = np.zeros(5, dtype = libE_fields + fields)
+exp_H0_H = np.zeros(5, dtype=libE_fields + fields)
 exp_H0_H['gen_informed_time'] = [0, 0, 0, inf, inf]
-exp_H0_H[['sim_started','sim_ended']][:3] = True
+exp_H0_H[['sim_started', 'sim_ended']][:3] = True
 exp_H0_H['sim_ended_time'][:3] = [1.1, 1.2, 1.3]
 exp_H0_H['sim_id'] = [0, 1, 2, -1, -1]
-exp_H0_H['sim_started_time'] = [0.1,0.2,0.3, inf, inf]
+exp_H0_H['sim_started_time'] = [0.1, 0.2, 0.3, inf, inf]
 exp_H0_H['sim_worker'][:3] = [1, 2, 3]
 
-wrs = np.zeros(10, dtype = libE_fields + fields)
-wrs[['sim_started_time','gen_informed_time']] = inf
+wrs = np.zeros(10, dtype=libE_fields + fields)
+wrs[['sim_started_time', 'gen_informed_time']] = inf
 wrs['sim_id'] = -1
 
-wrs2 = np.zeros(10, dtype = libE_fields + fields2)
-wrs2[['sim_started_time','gen_informed_time']] = inf
+wrs2 = np.zeros(10, dtype=libE_fields + fields2)
+wrs2[['sim_started_time', 'gen_informed_time']] = inf
 wrs2['sim_id'] = -1
 
-exp_x_in_setup2 = np.zeros(10, dtype = libE_fields + fields2)
-exp_x_in_setup2[['gen_informed_time','gen_started_time','sim_started_time']] = inf
+exp_x_in_setup2 = np.zeros(10, dtype=libE_fields + fields2)
+exp_x_in_setup2[['gen_informed_time', 'gen_started_time', 'sim_started_time']] = inf
 exp_x_in_setup2['gen_worker'] = 3
 exp_x_in_setup2['gen_worker'][0] = 2
-exp_x_in_setup2['sim_id'] = range(0,10)
+exp_x_in_setup2['sim_id'] = range(10)
 
-x = [4.17022005e-01, 7.20324493e-01, 1.14374817e-04, 3.02332573e-01,
-        1.46755891e-01, 9.23385948e-02, 1.86260211e-01, 3.45560727e-01,
-        3.96767474e-01, 5.38816734e-01]
+x = [
+    4.17022005e-01,
+    7.20324493e-01,
+    1.14374817e-04,
+    3.02332573e-01,
+    1.46755891e-01,
+    9.23385948e-02,
+    1.86260211e-01,
+    3.45560727e-01,
+    3.96767474e-01,
+    5.38816734e-01,
+]
 
 exp_x_in_setup2['x'] = x
 
@@ -57,9 +74,10 @@ safe_mode = True
 
 
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
-    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+    return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
-def compare_hists(H1, H2, ignore=[]): 
+
+def compare_hists(H1, H2, ignore=[]):
     for field in sorted(set(H1.dtype.names + H2.dtype.names)):
         if field not in ignore:
             assert np.allclose(H1[field], H2[field]), "Array does not match expected"
@@ -168,7 +186,7 @@ def test_update_history_x_in():
 
     hist.update_history_x_in(gen_worker, H_o, safe_mode, np.inf)
     # Compare by column
-    exp_x = exp_x_in_setup2[:size+1]
+    exp_x = exp_x_in_setup2[: size + 1]
 
     compare_hists(hist.H, exp_x, ['gen_ended_time'])
 
@@ -242,7 +260,7 @@ def test_update_history_x_in_sim_ids():
     hist.update_history_x_in(gen_worker, H_o, safe_mode, np.inf)
 
     # Compare by column
-    exp_x = exp_x_in_setup2[:size+1]
+    exp_x = exp_x_in_setup2[: size + 1]
 
     compare_hists(hist.H, exp_x, ['gen_ended_time'])
 
@@ -306,7 +324,9 @@ def test_update_history_x_out():
     hist.update_history_x_out(q_inds=my_qinds, sim_worker=4)
 
     # Try to avoid tautological testing - compare columns
-    assert np.array_equal(hist.H['sim_started'], np.array([True, True, True, False, True, False, False, True, False, True]))
+    assert np.array_equal(
+        hist.H['sim_started'], np.array([True, True, True, False, True, False, False, True, False, True])
+    )
     assert np.array_equal(hist.H['sim_worker'], np.array([2, 3, 3, 0, 4, 0, 0, 4, 0, 4]))
     assert np.all(~hist.H['sim_ended'])  # Should still be unaffected.
 
@@ -328,11 +348,13 @@ def test_update_history_f():
     a = np.arange(9) - 4
     calc_out['g'] = sim_specs['sim_f'](a)  # np.linalg.norm
     exp_vals[0] = calc_out['g'][0]
-    D_recv = {'calc_out': calc_out,
-              'persis_info': {},
-              'libE_info': {'H_rows': sim_ids},
-              'calc_status': WORKER_DONE,
-              'calc_type': 2}
+    D_recv = {
+        'calc_out': calc_out,
+        'persis_info': {},
+        'libE_info': {'H_rows': sim_ids},
+        'calc_status': WORKER_DONE,
+        'calc_type': 2,
+    }
 
     hist.update_history_f(D_recv, safe_mode)
     assert isclose(exp_vals[0], hist.H['g'][0])
@@ -352,11 +374,13 @@ def test_update_history_f():
     a = np.arange(9) - 2
     calc_out['g'][1] = sim_specs['sim_f'](a)  # np.linalg.norm
     exp_vals[2] = calc_out['g'][1]
-    D_recv = {'calc_out': calc_out,
-              'persis_info': {},
-              'libE_info': {'H_rows': sim_ids},
-              'calc_status': WORKER_DONE,
-              'calc_type': 2}
+    D_recv = {
+        'calc_out': calc_out,
+        'persis_info': {},
+        'libE_info': {'H_rows': sim_ids},
+        'calc_status': WORKER_DONE,
+        'calc_type': 2,
+    }
 
     hist.update_history_f(D_recv, safe_mode)
     assert np.allclose(exp_vals, hist.H['g'])
@@ -381,11 +405,13 @@ def test_update_history_f_vec():
     calc_out['fvec'] = sim_specs['sim_f'](a, axis=0)  # np.linalg.norm
     exp_fs[0] = calc_out['f'][0]
     exp_fvecs[0] = calc_out['fvec'][0]
-    D_recv = {'calc_out': calc_out,
-              'persis_info': {},
-              'libE_info': {'H_rows': sim_ids},
-              'calc_status': WORKER_DONE,
-              'calc_type': 2}
+    D_recv = {
+        'calc_out': calc_out,
+        'persis_info': {},
+        'libE_info': {'H_rows': sim_ids},
+        'calc_status': WORKER_DONE,
+        'calc_type': 2,
+    }
 
     hist.update_history_f(D_recv, safe_mode)
 
@@ -414,11 +440,13 @@ def test_update_history_f_vec():
     calc_out['fvec'][1] = sim_specs['sim_f'](a, axis=0)  # np.linalg.norm
     exp_fvecs[2] = calc_out['fvec'][1]
 
-    D_recv = {'calc_out': calc_out,
-              'persis_info': {},
-              'libE_info': {'H_rows': sim_ids},
-              'calc_status': WORKER_DONE,
-              'calc_type': 2}
+    D_recv = {
+        'calc_out': calc_out,
+        'persis_info': {},
+        'libE_info': {'H_rows': sim_ids},
+        'calc_status': WORKER_DONE,
+        'calc_type': 2,
+    }
 
     hist.update_history_f(D_recv, safe_mode)
 
@@ -449,11 +477,13 @@ def test_update_history_f_vec():
     exp_fvecs[4] = [0.0, 0.0, 0.0]  # Point to a new array - so can fill in elements
     exp_fvecs[4][:2] = calc_out['fvec'][1]  # Change first two values
 
-    D_recv = {'calc_out': calc_out,
-              'persis_info': {},
-              'libE_info': {'H_rows': sim_ids},
-              'calc_status': WORKER_DONE,
-              'calc_type': 2}
+    D_recv = {
+        'calc_out': calc_out,
+        'persis_info': {},
+        'libE_info': {'H_rows': sim_ids},
+        'calc_status': WORKER_DONE,
+        'calc_type': 2,
+    }
 
     hist.update_history_f(D_recv, safe_mode)
 
