87055134f7e30920af4759378d8c60e448dada4f
Jeffrey Larson <jmlarson@anl.gov>
2022-04-26 20:02:39 -0500

    Black a few single files

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ class Run_TestSuite(TestCommand):
     def run_tests(self):
         import os
         import sys
+
         py_version = sys.version_info[0]
         print('Python version from setup.py is', py_version)
         run_string = "libensemble/tests/run-tests.sh -p " + str(py_version)
@@ -27,6 +28,7 @@ class ToxTest(TestCommand):
 
     def run_tests(self):
         import tox
+
         tox.cmdline()
 
 
@@ -38,38 +40,50 @@ setup(
     author='Jeffrey Larson, Stephen Hudson, Stefan M. Wild, David Bindel and John-Luke Navarro',
     author_email='libensemble@lists.mcs.anl.gov',
     license='BSD 3-clause',
-
-    packages=['libensemble',
-              'libensemble.gen_funcs',
-              'libensemble.sim_funcs',
-              'libensemble.sim_funcs.branin',
-              'libensemble.alloc_funcs',
-              'libensemble.tests',
-              'libensemble.comms',
-              'libensemble.utils',
-              'libensemble.tools',
-              'libensemble.executors',
-              'libensemble.resources',
-              'libensemble.tests.unit_tests',
-              'libensemble.tests.regression_tests'],
-
+    packages=[
+        'libensemble',
+        'libensemble.gen_funcs',
+        'libensemble.sim_funcs',
+        'libensemble.sim_funcs.branin',
+        'libensemble.alloc_funcs',
+        'libensemble.tests',
+        'libensemble.comms',
+        'libensemble.utils',
+        'libensemble.tools',
+        'libensemble.executors',
+        'libensemble.resources',
+        'libensemble.tests.unit_tests',
+        'libensemble.tests.regression_tests',
+    ],
     package_data={'libensemble.sim_funcs.branin': ['known_minima_and_func_values']},
-
     install_requires=['numpy', 'psutil'],
-
     # If run tests through setup.py - downloads these but does not install
-    tests_require=['pytest>=3.1',
-                   'pytest-cov>=2.5',
-                   'pytest-pep8>=1.0',
-                   'pytest-timeout',
-                   'mock'
-                   ],
-
+    tests_require=[
+        'pytest>=3.1',
+        'pytest-cov>=2.5',
+        'pytest-pep8>=1.0',
+        'pytest-timeout',
+        'mock',
+    ],
     extras_require={
-        'extras': ['pyyaml', 'scipy', 'nlopt', 'mpi4py', 'petsc', 'petsc4py',
-                   'DFO-LS', 'mpmath', 'dragonfly-opt', 'ax-platform'],
-        'docs': ['sphinx', 'sphinxcontrib.bibtex', 'sphinx_rtd_theme']},
-
+        'extras': [
+            'pyyaml',
+            'scipy',
+            'nlopt',
+            'mpi4py',
+            'petsc',
+            'petsc4py',
+            'DFO-LS',
+            'mpmath',
+            'dragonfly-opt',
+            'ax-platform',
+        ],
+        'docs': [
+            'sphinx',
+            'sphinxcontrib.bibtex',
+            'sphinx_rtd_theme',
+        ],
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -86,8 +100,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Scientific/Engineering',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-
-    cmdclass={'test': Run_TestSuite, 'tox': ToxTest}
+    cmdclass={'test': Run_TestSuite, 'tox': ToxTest},
 )
