7ae1bd9fb2c29ae4eca8c5d0629a069680937885
Jeffrey Larson <jmlarson@anl.gov>
2022-04-27 14:26:05 -0500

    More tests

--- libensemble/tests/unit_tests/test_scipy.py
+++ libensemble/tests/unit_tests/test_scipy.py
@@ -11,9 +11,26 @@ def test_cdist_issue():
 
     """There is an issue (at least in scipy 1.1.0) with cdist segfaulting."""
 
-    H = np.zeros(20, dtype=[('x', '<f8', (2,)), ('m', '<i8'), ('a', '<f8'), ('b', '?'), ('c', '?'),
-                            ('d', '<f8'), ('e', '<f8'), ('fa', '<f8'), ('g', '<i8'), ('h', '<i8'), ('i', '?'),
-                            ('j', '<i8'), ('k', '?'), ('f', '<f8'), ('l', '?')])
+    H = np.zeros(
+        20,
+        dtype=[
+            ('x', '<f8', (2,)),
+            ('m', '<i8'),
+            ('a', '<f8'),
+            ('b', '?'),
+            ('c', '?'),
+            ('d', '<f8'),
+            ('e', '<f8'),
+            ('fa', '<f8'),
+            ('g', '<i8'),
+            ('h', '<i8'),
+            ('i', '?'),
+            ('j', '<i8'),
+            ('k', '?'),
+            ('f', '<f8'),
+            ('l', '?'),
+        ],
+    )
     np.random.seed(1)
     H['x'] = np.random.uniform(0, 1, (20, 2))
     dist_1 = cdist(np.atleast_2d(H['x'][3]), H['x'], 'euclidean')
