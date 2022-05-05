38f282aaa2c4831983923dba2048c65f208ad377
Jeffrey Larson <jmlarson@anl.gov>
2022-05-05 14:23:44 -0500

    black conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -190,7 +190,11 @@ html_logo = './images/libE_logo_white.png'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'navigation_depth': 3, 'collapse_navigation': False, 'logo_only': True,}
+html_theme_options = {
+    'navigation_depth': 3,
+    'collapse_navigation': False,
+    'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
