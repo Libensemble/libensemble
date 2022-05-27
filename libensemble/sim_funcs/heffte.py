615ae2c3a67e6532521a9362bd880127203ba3c5
Jeffrey Larson <jmlarson@anl.gov>
2022-05-27 14:34:26 -0500

    Adding heffte

---
+++ libensemble/sim_funcs/heffte.py
@@ -0,0 +1,21 @@
+"""
+Calls the branin function. Default behavior uses the python function, but
+uncommenting lines will write x.in to file, call branin.py, and then read f.out.
+"""
+import numpy as np
+import os 
+import subprocess 
+
+def call_and_process_heffte(H, persis_info, sim_specs, _):
+    """Evaluates a heffte string and parses the output"""
+
+    H_o = np.zeros(1, dtype=sim_specs["out"])
+
+    devnull = open(os.devnull, 'w')
+
+    p = subprocess.run(H["exec_and_args"][0].split(' '), cwd='./', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    time = float(p.stdout.decode().split('Time per run: ')[1].split(' ')[0])
+
+    H_o['run_time'] = time
+    return H_o, persis_info
