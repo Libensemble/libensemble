9835f663be50a89c2c005558fbcca955bebc09f3
Jeffrey Larson <jmlarson@anl.gov>
2022-05-27 08:13:32 -0500

    Moving rosenbrock plotting to a postproc_script

--- libensemble/tests/regression_tests/test_persistent_uniform_sampling.py
+++ libensemble/tests/regression_tests/test_persistent_uniform_sampling.py
@@ -73,44 +73,6 @@ for run in range(2):
     H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info, alloc_specs, libE_specs)
 
     if is_manager:
-        print(H["gen_ended_time"])
         assert len(np.unique(H["gen_ended_time"])) == num_batches
 
         save_libE_output(H, persis_info, __file__, nworkers)
-
-        if run == 1:
-            import matplotlib.cm as cm
-            import matplotlib.pyplot as plt
-            import matplotlib.colors as LogNorm
-
-            from libensemble.sim_funcs.rosenbrock import EvaluateFunction as obj_f
-
-            delta = 0.025
-            x = np.arange(-3.0, 3.0 + delta, delta)
-            y = np.arange(-2.0, 2.0 + delta, delta)
-            X, Y = np.meshgrid(x, y)
-            Z = np.zeros_like(X)
-
-            for i in range(np.shape(X)[0]):
-                for j in range(np.shape(X)[1]):
-                    Z[i, j] = obj_f(np.array([X[i, j], Y[i, j]]), np.nan)
-
-            fig, ax = plt.subplots()
-            levels = [0.1, 1, 5, 10, 30, 50, 70]
-            colormap = plt.get_cmap("hsv")
-            scale = 100  # Scaling Rosenbrock function values makes it much easier to see meaningful contours.
-
-            pts = H["x"]
-
-            CS = ax.contourf(X, Y, Z / 100, levels, cmap=colormap, alpha=0.2)
-            CS = ax.contour(X, Y, Z / 100, levels, cmap=colormap)
-            ax.clabel(CS, inline=True, fontsize=10)
-
-            for i in range(num_batches):
-                ax.scatter(
-                    pts[i * batch : (i + 1) * batch, 0],
-                    pts[i * batch : (i + 1) * batch, 1],
-                    color="b",
-                    edgecolors="k",
-                )
-                plt.savefig("plot_after_batch_" + str(i))
