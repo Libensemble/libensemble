dc1f5e8b3703aecdd02232e8873a8930d0adfc30
Jeffrey Larson <jmlarson@anl.gov>
2022-05-27 14:55:42 -0500

    Bulk rename of files to speed testing

---
+++ libensemble/tests/regression_tests/test_heffte.py
@@ -0,0 +1,59 @@
+"""
+Test libEnsemble's integration with calling the heFFTe exectuable with various
+configurations.
+"""
+
+# Do not change these lines - they are parsed by run-tests.sh
+# TESTSUITE_COMMS: mpi local tcp
+# TESTSUITE_NPROCS: 2 4
+# TESTSUITE_EXTRA: true
+
+import numpy as np
+import itertools
+
+# Import libEnsemble items for this test
+from libensemble.libE import libE
+from libensemble.sim_funcs.heffte import call_and_process_heffte as sim_f
+from libensemble.alloc_funcs.give_pregenerated_work import give_pregenerated_sim_work as alloc_f
+from libensemble.tools import parse_args, save_libE_output
+
+fixed = ["mpirun -np 4 ./speed3d_c2c fftw double 128 128 128"]
+arg1 = ["-no-reorder", "-reorder"]
+arg2 = ["-a2a", "-a2av", "-p2p", "-p2p_pl"]
+
+l = list(itertools.product(fixed, arg1, arg2))
+
+full_list = list(map(" ".join, l))
+
+max_len = max([len(item) for item in full_list])
+
+nworkers, is_manager, libE_specs, _ = parse_args()
+
+sim_specs = {
+    "sim_f": sim_f,
+    "in": ["exec_and_args"],
+    "out": [("run_time", float)],
+}
+
+gen_specs = {}
+
+n_samp = len(full_list)
+
+H0 = np.zeros(n_samp, dtype=[("exec_and_args", str, max_len), ("sim_id", int), ("sim_started", bool)])
+
+H0["exec_and_args"] = full_list
+H0["sim_id"] = range(n_samp)
+
+alloc_specs = {"alloc_f": alloc_f, "out": [("exec_and_args", str, max_len)]}
+
+exit_criteria = {"sim_max": len(H0)}
+
+# Perform the run
+H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, alloc_specs=alloc_specs, libE_specs=libE_specs, H0=H0)
+
+if is_manager:
+    assert len(H) == len(H0)
+    assert np.all(H["sim_ended"])
+    print(H["run_time"])
+    print("\nlibEnsemble correctly didn't add anything to initial sample")
+    save_libE_output(H, persis_info, __file__, nworkers)
