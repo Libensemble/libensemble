ab5a0fc44d4a49112d8a5228d9b0f74b09831db3
jlnav <jnavarro@anl.gov>
2022-06-01 10:03:01 -0500

    missed the legacy balsam executor script

--- libensemble/tests/regression_tests/script_test_balsam_hworld.py
+++ libensemble/tests/regression_tests/script_test_balsam_hworld.py
@@ -8,7 +8,12 @@ import mpi4py
 from mpi4py import MPI
 
 from libensemble.executors.legacy_balsam_executor import LegacyBalsamMPIExecutor
-from libensemble.message_numbers import WORKER_DONE, WORKER_KILL_ON_ERR, WORKER_KILL_ON_TIMEOUT, TASK_FAILED
+from libensemble.message_numbers import (
+    WORKER_DONE,
+    WORKER_KILL_ON_ERR,
+    WORKER_KILL_ON_TIMEOUT,
+    TASK_FAILED,
+)
 from libensemble.libE import libE
 from libensemble.sim_funcs.executor_hworld import executor_hworld
 from libensemble.gen_funcs.sampling import uniform_random_sample
@@ -33,7 +38,9 @@ sim_app = "./my_simtask.x"
 sim_app2 = six_hump_camel.__file__
 
 exctr = LegacyBalsamMPIExecutor()
-exctr.register_app(full_path=sim_app, calc_type="sim")  # Default 'sim' app - backward compatible
+exctr.register_app(
+    full_path=sim_app, calc_type="sim"
+)  # Default 'sim' app - backward compatible
 exctr.register_app(full_path=sim_app2, app_name="six_hump_camel")  # Named app
 exctr.register_app(full_path=sim_app2, app_name="sim_hump_camel_dry_run")
 
@@ -64,28 +71,43 @@ exit_criteria = {"sim_max": nworkers * 5}
 
 # Perform the run
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info, libE_specs=libE_specs)
+    H, persis_info, flag = libE(
+        sim_specs, gen_specs, exit_criteria, persis_info, libE_specs=libE_specs
+    )
 
     if is_manager:
         print("\nChecking expected task status against Workers ...\n")
         calc_status_list_in = np.asarray(
-            [WORKER_DONE, WORKER_KILL_ON_ERR, WORKER_DONE, WORKER_KILL_ON_TIMEOUT, TASK_FAILED]
+            [
+                WORKER_DONE,
+                WORKER_KILL_ON_ERR,
+                WORKER_DONE,
+                WORKER_KILL_ON_TIMEOUT,
+                TASK_FAILED,
+            ]
         )
         calc_status_list = np.repeat(calc_status_list_in, nworkers)
 
         print("Expecting: {}".format(calc_status_list))
         print("Received:  {}\n".format(H["cstat"]))
 
-        assert np.array_equal(H["cstat"], calc_status_list), "Error - unexpected calc status. Received: " + str(H["cstat"])
+        assert np.array_equal(
+            H["cstat"], calc_status_list
+        ), "Error - unexpected calc status. Received: " + str(H["cstat"])
 
         # Check dry_run submissions inside ensemble.log
         with open("ensemble.log", "r") as f:
             lines = f.readlines()
 
-        test = len([i for i in lines if "Test (No submit) Runline:" in i]) == (len(calc_status_list_in)) * nworkers
-        assert test, "Dry run runlines not listed in ensemble.log for each dry_run submission instance."
+        test = (
+            len([i for i in lines if "Test (No submit) Runline:" in i])
+            == (len(calc_status_list_in)) * nworkers
+        )
+        assert (
+            test
+        ), "Dry run runlines not listed in ensemble.log for each dry_run submission instance."
 
         # Cleanup (maybe cover del_apps() and del_tasks())
         exctr.del_apps()
