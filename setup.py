cce3988cffad4216209336fa47f00e0b288f4730
jlnav <jnavarro@anl.gov>
2022-07-01 14:51:36 -0500

    moves balsam executors one module lower, to try avoiding unneeded messages?

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
         "libensemble.utils",
         "libensemble.tools",
         "libensemble.executors",
+        "libensemble.executors.balsam_executors",
         "libensemble.resources",
         "libensemble.tests.unit_tests",
         "libensemble.tests.regression_tests",
