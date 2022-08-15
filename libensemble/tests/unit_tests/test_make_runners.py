286b7e6a1f290051da0aa3e669f566781d5d98c1
jlnav <jnavarro@anl.gov>
2022-08-15 15:37:27 -0500

    finishing first draft of testing runners/funcx code, plus black

--- libensemble/tests/unit_tests/test_make_runners.py
+++ libensemble/tests/unit_tests/test_make_runners.py
@@ -20,7 +20,12 @@ def get_ufunc_args():
     H["sim_started_time"][-L:] = np.inf
 
     sim_ids = np.zeros(1, dtype=int)
-    Work = {"tag": EVAL_SIM_TAG, "persis_info": {}, "libE_info": {"H_rows": sim_ids}, "H_fields": sim_specs["in"]}
+    Work = {
+        "tag": EVAL_SIM_TAG,
+        "persis_info": {},
+        "libE_info": {"H_rows": sim_ids},
+        "H_fields": sim_specs["in"],
+    }
     calc_in = H[Work["H_fields"]][Work["libE_info"]["H_rows"]]
     return calc_in, sim_specs, gen_specs
 
@@ -29,12 +34,14 @@ def test_normal_runners():
     calc_in, sim_specs, gen_specs = get_ufunc_args()
 
     runners = Runners(sim_specs, gen_specs)
-    assert not runners.has_funcx_sim and not runners.has_funcx_gen, \
-        "funcX use should not be detected without setting endpoint fields"
+    assert (
+        not runners.has_funcx_sim and not runners.has_funcx_gen
+    ), "funcX use should not be detected without setting endpoint fields"
 
     ro = runners.make_runners()
-    assert all([i in ro for i in [EVAL_SIM_TAG, EVAL_GEN_TAG]]), \
-        "Both user function tags should be included in runners dictionary"
+    assert all(
+        [i in ro for i in [EVAL_SIM_TAG, EVAL_GEN_TAG]]
+    ), "Both user function tags should be included in runners dictionary"
 
 
 def test_normal_no_gen():
@@ -43,43 +50,65 @@ def test_normal_no_gen():
     runners = Runners(sim_specs, {})
     ro = runners.make_runners()
 
-    assert not ro[2], \
-        "generator function shouldn't be provided if not using gen_specs"
+    assert not ro[2], "generator function shouldn't be provided if not using gen_specs"
 
 
-def test_manager_exception():
-    """Checking dump of history and pickle file on abort"""
-    sim_specs, gen_specs, exit_criteria = setup.make_criteria_and_specs_0()
-    remove_file_if_exists(hfile_abort)
-    remove_file_if_exists(pfile_abort)
+def test_funcx_runner_init():
+    calc_in, sim_specs, gen_specs = get_ufunc_args()
 
-    with mock.patch("libensemble.manager.manager_main") as managerMock:
-        managerMock.side_effect = Exception
-        # Collision between libE.py and libE() (after mods to __init__.py) means
-        #   libensemble.libE.comms_abort tries to refer to the function, not file
-        with mock.patch("libensemble.comms_abort") as abortMock:
-            abortMock.side_effect = Exception
-            # Need fake MPI to get past the Manager only check and dump history
-            with pytest.raises(Exception):
-                libE_specs = {"mpi_comm": fake_mpi, "disable_resource_manager": True}
-                libE(sim_specs, gen_specs, exit_criteria, libE_specs=libE_specs)
-                pytest.fail("Expected exception")
-            assert os.path.isfile(hfile_abort), "History file not dumped"
-            assert os.path.isfile(pfile_abort), "Pickle file not dumped"
-            os.remove(hfile_abort)
-            os.remove(pfile_abort)
-
-            # Test that History and Pickle files NOT created when disabled
-            with pytest.raises(Exception):
-                libE_specs = {"mpi_comm": fake_mpi, "save_H_and_persis_on_abort": False}
-                libE(sim_specs, gen_specs, exit_criteria, libE_specs=libE_specs)
-                pytest.fail("Expected exception")
-            assert not os.path.isfile(hfile_abort), "History file dumped"
-            assert not os.path.isfile(pfile_abort), "Pickle file dumped"
+    sim_specs["funcx_endpoint"] = "1234"
+    runners = Runners(sim_specs, gen_specs)
+    assert (
+        runners.funcx_exctr is not None
+    ), "FuncXExecutor should have been instantiated when funcx_endpoint found in specs"
 
 
+def test_funcx_runner_pass():
+    calc_in, sim_specs, gen_specs = get_ufunc_args()
+
+    sim_specs["funcx_endpoint"] = "1234"
+    runners = Runners(sim_specs, gen_specs)
+
+    #  Creating Mock funcXExecutor and funcX future object - no exception
+    funcx_mock = mock.Mock()
+    funcx_future = mock.Mock()
+    funcx_mock.submit.return_value = funcx_future
+    funcx_future.exception.return_value = None
+    funcx_future.result.return_value = (True, True)
+
+    runners.funcx_exctr = funcx_mock
+    ro = runners.make_runners()
+
+    libE_info = {"H_rows": np.array([2, 3, 4]), "workerID": 1, "comm": "fakecomm"}
+    out, persis_info = ro[1](calc_in, {}, libE_info)
+
+    assert all([out, persis_info]), "funcX runner correctly returned results"
+
+
+def test_funcx_runner_fail():
+    calc_in, sim_specs, gen_specs = get_ufunc_args()
+
+    gen_specs["funcx_endpoint"] = "4321"
+    runners = Runners(sim_specs, gen_specs)
+
+    #  Creating Mock funcXExecutor and funcX future object - no exception
+    funcx_mock = mock.Mock()
+    funcx_future = mock.Mock()
+    funcx_mock.submit.return_value = funcx_future
+    funcx_future.exception.return_value = Exception
+
+    runners.funcx_exctr = funcx_mock
+    ro = runners.make_runners()
+
+    libE_info = {"H_rows": np.array([2, 3, 4]), "workerID": 1, "comm": "fakecomm"}
+
+    with pytest.raises(Exception):
+        out, persis_info = ro[2](calc_in, {}, libE_info)
+        pytest.fail("Expected exception")
 
 
 if __name__ == "__main__":
     test_normal_runners()
-    test_no_gen()
+    test_normal_no_gen()
+    test_funcx_runner_pass()
+    test_funcx_runner_fail()
