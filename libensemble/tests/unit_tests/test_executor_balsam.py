b6c7fe80ff44a451ca06c0181bc181647ed589ed
jlnav <jnavarro@anl.gov>
2022-08-18 10:17:18 -0500

    cleanup

--- libensemble/tests/unit_tests/test_executor_balsam.py
+++ libensemble/tests/unit_tests/test_executor_balsam.py
@@ -2,18 +2,12 @@
 # Integration Test of executor module for libensemble
 # Test does not require running full libensemble
 import os
-import re
 import sys
-import time
 import mock
 import pytest
 
-from libensemble.resources.mpi_resources import MPIResourcesException
-from libensemble.executors.executor import Executor, ExecutorException, TimeoutExpired
-from libensemble.executors.executor import NOT_STARTED_STATES, Application
+from libensemble.executors.executor import Executor, Application
 
-NCORES = 1
-py_startup = "simdir/py_startup.py"
 
 # fake Balsam app
 class TestLibeApp:
@@ -48,21 +42,26 @@ def teardown_module(module):
 def setup_executor():
     """Set up a Balsam Executor with sim app"""
     from libensemble.executors.balsam_executors import BalsamExecutor
-    exctr = BalsamExecutor()
 
+    exctr = BalsamExecutor()  # noqa F841
 
 
 # Tests ========================================================================================
 
+
+@pytest.mark.extra
 def test_register_app():
     """Test of registering an App"""
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
     setup_executor()
     exctr = Executor.executor
     exctr.register_app(TestLibeApp, calc_type="sim", precedent="fake/dir")
-    assert isinstance(exctr.apps["python"], Application), \
-        "Application object not created based on registered Balsam AppDef"
+    assert isinstance(
+        exctr.apps["python"], Application
+    ), "Application object not created based on registered Balsam AppDef"
 
+
+@pytest.mark.extra
 def test_submit_app_defaults():
     """Test of submitting an App"""
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
@@ -70,13 +69,14 @@ def test_submit_app_defaults():
     with mock.patch("libensemble.executors.balsam_executors.balsam_executor.Job"):
         task = exctr.submit(calc_type="sim")
 
-    assert task in exctr.list_of_tasks, \
-        "new task not added to executor's list of tasks"
+    assert task in exctr.list_of_tasks, "new task not added to executor's list of tasks"
 
-    assert task == exctr.get_task(task.id), \
-        "task retrieved via task ID doesn't match new task"
+    assert task == exctr.get_task(
+        task.id
+    ), "task retrieved via task ID doesn't match new task"
 
 
+@pytest.mark.extra
 def test_submit_app_workdir():
     """Test of submitting an App with a workdir"""
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
@@ -84,59 +84,74 @@ def test_submit_app_workdir():
     with mock.patch("libensemble.executors.balsam_executors.balsam_executor.Job"):
         task = exctr.submit(calc_type="sim", workdir="output", machinefile="nope")
 
-    assert task.workdir == os.path.join(exctr.workflow_name, "output"), \
-        "workdir not properly defined for new task"
+    assert task.workdir == os.path.join(
+        exctr.workflow_name, "output"
+    ), "workdir not properly defined for new task"
 
+
+@pytest.mark.extra
 def test_submit_app_dry():
     """Test of dry-run submitting an App"""
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
     exctr = Executor.executor
     task = exctr.submit(calc_type="sim", dry_run=True)
 
-    assert all([task.dry_run, task.done()]), \
-        "new task from dry_run wasn't marked as such, or set as done"
+    assert all(
+        [task.dry_run, task.done()]
+    ), "new task from dry_run wasn't marked as such, or set as done"
 
+
+@pytest.mark.extra
 def test_submit_app_wait():
     """Test of exctr.submit blocking until app is running"""
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
     exctr = Executor.executor
-    with mock.patch("libensemble.executors.balsam_executors.balsam_executor.Job") as job:
+    with mock.patch(
+        "libensemble.executors.balsam_executors.balsam_executor.Job"
+    ) as job:
         job.return_value.state = "RUNNING"
         task = exctr.submit(calc_type="sim", wait_on_start=True)
 
-    assert task.running(), \
-        "new task is not marked as running after wait_on_start"
+    assert task.running(), "new task is not marked as running after wait_on_start"
 
+
+@pytest.mark.extra
 def test_submit_revoke_alloc():
     """Test creating and revoking BatchJob objects through the executor"""
     exctr = Executor.executor
-    with mock.patch("libensemble.executors.balsam_executors.balsam_executor.BatchJob") as batchjob:
-        alloc = exctr.submit_allocation(site_id="libe-unit-test", num_nodes=1, wall_time_min=30)
+    with mock.patch("libensemble.executors.balsam_executors.balsam_executor.BatchJob"):
+        alloc = exctr.submit_allocation(
+            site_id="libe-unit-test", num_nodes=1, wall_time_min=30
+        )
 
-        assert alloc in exctr.allocations, \
-            "batchjob object not appended to executor's list of allocations"
+        assert (
+            alloc in exctr.allocations
+        ), "batchjob object not appended to executor's list of allocations"
 
         alloc.scheduler_id = 1
         exctr.revoke_allocation(alloc)
 
+
+@pytest.mark.extra
 def test_task_wait():
     """Test of killing (cancelling) a balsam app"""
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
     exctr = Executor.executor
-    with mock.patch("libensemble.executors.balsam_executors.balsam_executor.Job") as job:
+    with mock.patch(
+        "libensemble.executors.balsam_executors.balsam_executor.Job"
+    ) as job:
         task = exctr.submit(calc_type="sim")
         job.return_value.state = "JOB_FINISHED"
 
     task.wait(timeout=3)
-    assert task.state == "FINISHED", \
-        "task was not finished after wait method"
+    assert task.state == "FINISHED", "task was not finished after wait method"
 
-    assert not task.running(), \
-        "task shouldn't be running after wait method returns"
+    assert not task.running(), "task shouldn't be running after wait method returns"
 
-    assert task.done(), \
-        "task should be 'done' after wait method"
+    assert task.done(), "task should be 'done' after wait method"
 
+
+@pytest.mark.extra
 def test_task_kill():
     """Test of killing (cancelling) a balsam app"""
     print("\nTest: {}\n".format(sys._getframe().f_code.co_name))
@@ -145,8 +160,10 @@ def test_task_kill():
         task = exctr.submit(calc_type="sim")
 
     task.kill()
-    assert task.finished and task.state == "USER_KILLED", \
-        "task not set as killed after kill method"
+    assert (
+        task.finished and task.state == "USER_KILLED"
+    ), "task not set as killed after kill method"
+
 
 if __name__ == "__main__":
     setup_module(__file__)
@@ -158,3 +175,4 @@ if __name__ == "__main__":
     test_submit_revoke_alloc()
     test_task_wait()
     test_task_kill()
+    teardown_module(__file__)
