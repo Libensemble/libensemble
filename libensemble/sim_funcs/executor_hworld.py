61100651645e89cd618e2efce14286158e5ad991
jlnav <jnavarro@anl.gov>
2022-08-18 15:35:47 -0500

    first attempt at simple executor functionality test, plus the adjustments to executor_hworld to make every test work

--- libensemble/sim_funcs/executor_hworld.py
+++ libensemble/sim_funcs/executor_hworld.py
@@ -78,100 +78,107 @@ def executor_hworld(H, persis_info, sim_specs, libE_info):
     wait = False
     args_for_sim = "sleep 1"
 
-    if ELAPSED_TIMEOUT:
-        args_for_sim = "sleep 60"  # Manager kill - if signal received else completes
-        timeout = 65.0
+    if "six_hump_camel" not in exctr.default_app("sim").full_path:
 
-    else:
-        global sim_ended_count
-        sim_ended_count += 1
-        timeout = 6.0
-        launch_shc = False
-        print(sim_ended_count)
-
-        if sim_ended_count == 1:
-            args_for_sim = "sleep 1"  # Should finish
-        elif sim_ended_count == 2:
-            args_for_sim = "sleep 1 Error"  # Worker kill on error
-        elif sim_ended_count == 3:
-            wait = True
-            args_for_sim = "sleep 1"  # Should finish
-            launch_shc = True
-        elif sim_ended_count == 4:
-            args_for_sim = "sleep 8"  # Worker kill on timeout
-            timeout = 1.0
-        elif sim_ended_count == 5:
-            args_for_sim = "sleep 2 Fail"  # Manager kill - if signal received else completes
-
-    if USE_BALSAM:
-        task = exctr.submit(
-            calc_type="sim",
-            num_procs=cores,
-            app_args=args_for_sim,
-            hyperthreads=True,
-            machinefile="notused",
-            stdout="notused",
-            wait_on_start=True,
-        )
-    else:
-        task = exctr.submit(calc_type="sim", num_procs=cores, app_args=args_for_sim, hyperthreads=True)
-
-    if wait:
-        task.wait()
-        if not task.finished:
-            calc_status = UNSET_TAG
-        if task.state == "FINISHED":
-            calc_status = WORKER_DONE
-        elif task.state == "FAILED":
-            calc_status = TASK_FAILED
-
-    else:
-        if not ELAPSED_TIMEOUT:
-            if sim_ended_count >= 2 and not USE_BALSAM:
-                calc_status = exctr.polling_loop(task, timeout=timeout, delay=0.3, poll_manager=True)
-                if sim_ended_count == 2 and task.stdout_exists() and "Error" in task.read_stdout():
-                    calc_status = WORKER_KILL_ON_ERR
-
-            else:
-                task, calc_status = custom_polling_loop(exctr, task, timeout)
+        if ELAPSED_TIMEOUT:
+            args_for_sim = "sleep 60"  # Manager kill - if signal received else completes
+            timeout = 65.0
 
         else:
-            calc_status = exctr.polling_loop(task, timeout=timeout, delay=0.3, poll_manager=True)
+            global sim_ended_count
+            sim_ended_count += 1
+            timeout = 6.0
+            launch_shc = False
+            print(sim_ended_count)
 
-    if USE_BALSAM:
-        task.read_file_in_workdir("ensemble.log")
-        try:
-            task.read_stderr()
-        except ValueError:
-            pass
+            if sim_ended_count == 1:
+                args_for_sim = "sleep 1"  # Should finish
+            elif sim_ended_count == 2:
+                args_for_sim = "sleep 1 Error"  # Worker kill on error
+            elif sim_ended_count == 3:
+                wait = True
+                args_for_sim = "sleep 1"  # Should finish
+                launch_shc = True
+            elif sim_ended_count == 4:
+                args_for_sim = "sleep 8"  # Worker kill on timeout
+                timeout = 1.0
+            elif sim_ended_count == 5:
+                args_for_sim = "sleep 2 Fail"  # Manager kill - if signal received else completes
 
-        task = exctr.submit(
-            app_name="sim_hump_camel_dry_run",
-            num_procs=cores,
-            app_args=args_for_sim,
-            hyperthreads=True,
-            machinefile="notused",
-            stdout="notused",
-            wait_on_start=True,
-            dry_run=True,
-            stage_inout=os.getcwd(),
-        )
+        if USE_BALSAM:
+            task = exctr.submit(
+                calc_type="sim",
+                num_procs=cores,
+                app_args=args_for_sim,
+                hyperthreads=True,
+                machinefile="notused",
+                stdout="notused",
+                wait_on_start=True,
+            )
+        else:
+            task = exctr.submit(calc_type="sim", num_procs=cores, app_args=args_for_sim, hyperthreads=True)
 
-        task.poll()
-        task.wait()
-
-    # This is temp - return something - so doing six_hump_camel_func again...
-    batch = len(H["x"])
-    H_o = np.zeros(batch, dtype=sim_specs["out"])
-    for i, x in enumerate(H["x"]):
-        H_o["f"][i] = six_hump_camel_func(x)
-        if launch_shc:
-            # Test launching a named app.
-            app_args = " ".join(str(val) for val in list(x[:]))
-            task = exctr.submit(app_name="six_hump_camel", num_procs=1, app_args=app_args)
+        if wait:
             task.wait()
-            output = np.float64(task.read_stdout())
-            assert np.isclose(H_o["f"][i], output)
+            if not task.finished:
+                calc_status = UNSET_TAG
+            if task.state == "FINISHED":
+                calc_status = WORKER_DONE
+            elif task.state == "FAILED":
+                calc_status = TASK_FAILED
+
+        else:
+            if not ELAPSED_TIMEOUT:
+                if sim_ended_count >= 2 and not USE_BALSAM:
+                    calc_status = exctr.polling_loop(task, timeout=timeout, delay=0.3, poll_manager=True)
+                    if sim_ended_count == 2 and task.stdout_exists() and "Error" in task.read_stdout():
+                        calc_status = WORKER_KILL_ON_ERR
+
+                else:
+                    task, calc_status = custom_polling_loop(exctr, task, timeout)
+
+            else:
+                calc_status = exctr.polling_loop(task, timeout=timeout, delay=0.3, poll_manager=True)
+
+        if USE_BALSAM:
+            task.read_file_in_workdir("ensemble.log")
+            try:
+                task.read_stderr()
+            except ValueError:
+                pass
+
+            task = exctr.submit(
+                app_name="sim_hump_camel_dry_run",
+                num_procs=cores,
+                app_args=args_for_sim,
+                hyperthreads=True,
+                machinefile="notused",
+                stdout="notused",
+                wait_on_start=True,
+                dry_run=True,
+                stage_inout=os.getcwd(),
+            )
+
+            task.poll()
+            task.wait()
+
+    else:
+        launch_shc = True
+        calc_status = UNSET_TAG
+
+        # This is temp - return something - so doing six_hump_camel_func again...
+        batch = len(H["x"])
+        H_o = np.zeros(batch, dtype=sim_specs["out"])
+        for i, x in enumerate(H["x"]):
+            H_o["f"][i] = six_hump_camel_func(x)
+            if launch_shc:
+                # Test launching a named app.
+                app_args = " ".join(str(val) for val in list(x[:]))
+                task = exctr.submit(app_name="six_hump_camel", num_procs=1, app_args=app_args)
+                task.wait()
+                output = np.float64(task.read_stdout())
+                assert np.isclose(H_o["f"][i], output)
+                calc_status = WORKER_DONE
 
     # This is just for testing at calling script level - status of each task
     H_o["cstat"] = calc_status
