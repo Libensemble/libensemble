50b2e9c980cd773c188e0a16b8da48b1a76fda55
jlnav <jnavarro@anl.gov>
2022-08-23 13:23:00 -0500

    finish first approach for new RC class, that returns values normally if unix, or reads from file if windows

--- libensemble/gen_funcs/__init__.py
+++ libensemble/gen_funcs/__init__.py
@@ -1,13 +1,39 @@
+import csv
 import platform
 from dataclasses import dataclass
 from typing import Union, List, Optional
 
 
 @dataclass
-class rc:
+class RC:
     """Runtime configuration options."""
 
-    aposmm_optimizers: Optional[
+    _aposmm_optimizers: Optional[
         Union[str, List[str]]
     ] = None  # optional string or list of strings
-    is_unix: bool = platform.system() in ["Linux", "Darwin"]
+    _is_unix: bool = platform.system() in ["Linux", "Darwin"]
+
+    @property
+    def aposmm_optimizers(self):
+        if self._is_unix:
+            return self._aposmm_optimizers
+        else:
+            with open(".aposmm_opt.csv") as f:
+                optreader = csv.reader(f)
+                for opt in optreader:
+                    return opt  # should only be one row
+
+    @aposmm_optimizers.setter
+    def aposmm_optimizers(self, values):
+        self._aposmm_optimizers = values
+
+        if not self._is_unix:
+            with open(".aposmm_opt.csv", "w") as f:
+                optwriter = csv.writer(f)
+                if isinstance(values, list):
+                    optwriter.writerow(values)
+                else:
+                    optwriter.writerow([values])
+
+
+rc = RC()
