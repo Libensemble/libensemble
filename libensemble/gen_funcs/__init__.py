37edb0763508d7a2f854453430599ffff2230169
jlnav <jnavarro@anl.gov>
2022-09-22 14:23:39 -0500

    attempt to simplify rc class

--- libensemble/gen_funcs/__init__.py
+++ libensemble/gen_funcs/__init__.py
@@ -1,9 +1,6 @@
 import os
 import csv
-import time
 import logging
-import platform
-from typing import Union, List, Optional
 
 logger = logging.getLogger(__name__)
 
@@ -14,31 +11,19 @@ class RC:
     def __init__(self):
 
         self._opt_modules: Optional[Union[str, List[str]]] = None  # optional string or list of strings
-        self._is_windows: bool = platform.system() not in ["Linux", "Darwin"]
-        self._csv_path = __file__.replace("__init__.py", ".opt_modules.csv")
+        self._csv_path: str = __file__.replace("__init__.py", ".opt_modules.csv")
         self._csv_exists: bool = os.path.isfile(self._csv_path)
 
-        if self._is_windows and self._csv_exists:
+        if self._csv_exists:
             with open(self._csv_path) as f:
                 optreader = csv.reader(f)
                 self._opt_modules = [opt for opt in optreader][0]  # should only be one row
 
-    @property
+    @property  # getter
     def aposmm_optimizers(self):
-        timeout = 5
-        start = time.time()
-        if not self._is_windows or not self._csv_exists:
+        if self._opt_modules or not self._csv_exists:
             return self._opt_modules
-        else:  # is windows and csv exists
-            while not os.path.isfile(self._csv_path):  # self._csv_exists not updated after init
-                time.sleep(0.1)
-                if time.time() - start > timeout:
-                    logger.warning("Unable to determine set optimization methods by timeout. Using nlopt as default.")
-                    return "nlopt"
-
-            while not os.stat(self._csv_path).st_size:
-                time.sleep(0.001)  # avoiding race where file may exist but values not written into it yet
-
+        else:
             with open(self._csv_path) as f:
                 optreader = csv.reader(f)
                 return [opt for opt in optreader][0]
@@ -50,7 +35,7 @@ class RC:
         if not isinstance(values, list):
             values = [values]
 
-        if self._is_windows and self._csv_exists and values != current_opt:  # avoid rewriting constantly
+        if self._csv_exists and values != current_opt:  # avoid rewriting constantly
             with open(self._csv_path, "w") as f:
                 optwriter = csv.writer(f)
                 optwriter.writerow(values)
