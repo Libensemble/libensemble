7c03c6ed0cd30b1680f7665fa19a68ad37a1677f
jlnav <jnavarro@anl.gov>
2022-12-20 13:06:29 -0600

    initial commit for moving six mpi_runners regression tests into pytest framework. renames test_make_runners to be more accurate. adds initial test_misc_features

---
+++ libensemble/tests/unit_tests/test_misc_features.py
@@ -0,0 +1,24 @@
+import time
+import pytest
+import platform
+import numpy as np
+
+import libensemble.tests.unit_tests.setup as setup
+
+
+@pytest.mark.extra
+def test_profiling():
+    pass
+
+@pytest.mark.extra
+def test_deliberate_error():
+    pass
+
+@pytest.mark.extra
+def test_worker_exception():
+    pass
+
+if __name__ == "__main__":
+    test_term_test_1()
+    test_term_test_2()
+    test_term_test_3()
