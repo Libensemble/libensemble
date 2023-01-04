b3079c242f734600baed81f1fe695722200410c4
Jeffrey Larson <jmlarson@anl.gov>
2023-01-04 13:18:02 -0600

    Removing extra files

--- libensemble/tests/regression_tests/test_ytopt_heffte.py
+++ libensemble/tests/regression_tests/test_ytopt_heffte.py
@@ -1,46 +1,80 @@
 """
 Runs libEnsemble to call the ytopt ask/tell interface in a generator function,
-and the rosenbrock function as a (synthetic) simulator function.
+and the ytopt findRunTime interface in a simulator function.
+
+Execute locally via one of the following commands (e.g. 3 workers):
+   mpiexec -np 4 python run_ytopt_xsbench.py
+   python run_ytopt_xsbench.py --nworkers 3 --comms local
+
+The number of concurrent evaluations of the objective function will be 4-1=3.
 """
 
-# Do not change these lines - they are parsed by run-tests.sh
-# TESTSUITE_COMMS: mpi
-# TESTSUITE_NPROCS: 4
-
+import os
+import sys
+import glob
+import secrets
 import numpy as np
+import time
+
+sys.path.append('./ytopt-libe-speed3d/')
 
 # Import libEnsemble items for this test
 from libensemble.libE import libE
-from libensemble.sim_funcs.heffte import call_and_process_heffte as sim_f
-from libensemble.gen_funcs.ytopt_gen_xsbench import persistent_ytopt as gen_f
 from libensemble.alloc_funcs.start_only_persistent import only_persistent_gens as alloc_f
-from libensemble.tools import parse_args, save_libE_output
+from libensemble.tools import parse_args, save_libE_output, add_unique_random_streams
+
+from ytopt_obj import init_obj  # Simulator function, calls Plopper
+from ytopt_asktell import persistent_ytopt  # Generator function, communicates with ytopt optimizer
 
 import ConfigSpace as CS
 import ConfigSpace.hyperparameters as CSH
+from ConfigSpace import ConfigurationSpace, EqualsCondition
 from ytopt.search.optimizer import Optimizer
 
-nworkers, is_manager, libE_specs, user_args_in = parse_args()
-user_args_in = ["learner=RF", "max-evals=10"]
-if len(user_args_in):
-    user_args = {}
-    for entry in user_args_in:
-        sp = entry.split("=")
-        assert len(sp) == 2, "Incorrect arg format"
-        field = sp[0]
-        value = sp[1]
-        user_args[field] = value
 
+# Parse comms, default options from commandline
+nworkers, is_manager, libE_specs, user_args_in = parse_args()
 num_sim_workers = nworkers - 1  # Subtracting one because one worker will be the generator
+
+user_args_in = ["--learner=RF", "--max-evals=10"]
+assert len(user_args_in), "learner, etc. not specified, e.g. --learner RF"
+user_args = {}
+for entry in user_args_in:
+    if entry.startswith("--"):
+        if "=" not in entry:
+            key = entry.strip("--")
+            value = user_args_in[user_args_in.index(entry) + 1]
+        else:
+            split = entry.split("=")
+            key = split[0].strip("--")
+            value = split[1]
+
+    user_args[key] = value
+
+req_settings = ["learner", "max-evals"]
+assert all([opt in user_args for opt in req_settings]), "Required settings missing. Specify each setting in " + str(
+    req_settings
+)
+
+# Set options so workers operate in unique directories
+here = os.getcwd() + "/ytopt-libe-speed3d/"
+
+libE_specs["use_worker_dirs"] = True
+libE_specs["sim_dirs_make"] = False  # Otherwise directories separated by each sim call
+libE_specs["ensemble_dir_path"] = "./ensemble_" + secrets.token_hex(nbytes=4)
+
+# Copy or symlink needed files into unique directories
+libE_specs["sim_dir_symlink_files"] = [
+    here + f for f in ["speed3d.sh", "speed3d_c2c", "exe.pl", "plopper.py", "processexe.pl"]
+]
 
 # Declare the sim_f to be optimized, and the input/outputs
 sim_specs = {
-    "sim_f": sim_f,
+    "sim_f": init_obj,
     "in": ["p0", "p1", "p2", "p3"],
-    "out": [("RUN_TIME", float)],
+    "out": [("RUNTIME", float), ("elapsed_sec", float)],
 }
 
-# Initialize the ytopt ask/tell interface (to be used by the gen_f)
 cs = CS.ConfigurationSpace(seed=1234)
 p0 = CSH.CategoricalHyperparameter(name="p0", choices=["-no-reorder", "-reorder"], default_value="-no-reorder")
 p1 = CSH.CategoricalHyperparameter(name="p1", choices=["-a2a", "-a2av", "-p2p", "-p2p_pl"], default_value="-a2a")
@@ -53,21 +87,21 @@ cs.add_hyperparameters([p0, p1, p2, p3])
 ytoptimizer = Optimizer(
     num_workers=num_sim_workers,
     space=cs,
-    learner="RF",
+    learner=user_args["learner"],
     liar_strategy="cl_max",
     acq_func="gp_hedge",
     set_KAPPA=1.96,
-    set_SEED=12345,
+    set_SEED=2345,
     set_NI=10,
 )
 
-# Declare the gen_f that will generator points for the sim_f, and the various input/outputs
+# Declare the gen_f that will generate points for the sim_f, and the various input/outputs
 gen_specs = {
-    "gen_f": gen_f,
-    "out": [("p0", str, 20), ("p1", str, 20), ("p2", str, 20), ("p3", str, 20)],
-    "persis_in": sim_specs["in"] + ["RUN_TIME", "sim_ended_time", "sim_started_time"],
+    "gen_f": persistent_ytopt,
+    "out": [("p0", "<U24", (1,)), ("p1", "<U24", (1,)), ("p2", "<U24", (1,)), ("p3", "<U24", (1,))],
+    "persis_in": sim_specs["in"] + ["RUNTIME"] + ["elapsed_sec"],
     "user": {
-        "ytoptimizer": ytoptimizer,
+        "ytoptimizer": ytoptimizer,  # provide optimizer to generator function
         "num_sim_workers": num_sim_workers,
     },
 }
@@ -77,12 +111,27 @@ alloc_specs = {
     "user": {"async_return": True},
 }
 
-exit_criteria = {"sim_max": 10}
+# Specify when to exit. More options: https://libensemble.readthedocs.io/en/main/data_structures/exit_criteria.html
+exit_criteria = {"gen_max": int(user_args["max-evals"])}
+
+# Added as a workaround to issue that's been resolved on develop
+persis_info = add_unique_random_streams({}, nworkers + 1)
 
 # Perform the libE run
-H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, alloc_specs=alloc_specs, libE_specs=libE_specs)
+H, persis_info, flag = libE(
+    sim_specs, gen_specs, exit_criteria, persis_info, alloc_specs=alloc_specs, libE_specs=libE_specs
+)
 
+# Save History array to file
 if is_manager:
-    assert np.sum(H["sim_ended"]) == exit_criteria["sim_max"]
-    print("\nlibEnsemble has perform the correct number of evaluations")
-    save_libE_output(H, persis_info, __file__, nworkers)
+    print("\nlibEnsemble has completed evaluations.")
+    # save_libE_output(H, persis_info, __file__, nworkers)
+
+    # print("\nSaving just sim_specs[['in','out']] to a CSV")
+    # H = np.load(glob.glob('*.npy')[0])
+    # H = H[H["sim_ended"]]
+    # H = H[H["returned"]]
+    # dtypes = H[gen_specs['persis_in']].dtype
+    # b = np.vstack(map(list, H[gen_specs['persis_in']]))
+    # print(b)
+    # np.savetxt('results.csv',b, header=','.join(dtypes.names), delimiter=',',fmt=','.join(['%s']*b.shape[1]))
