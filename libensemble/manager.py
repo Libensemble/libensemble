09af1c78518be5e6f396dbad67807b58d45ef453
jlnav <jnavarro@anl.gov>
2023-02-28 13:14:01 -0600

    tiny bugfix and take a test fix from another branch

--- libensemble/manager.py
+++ libensemble/manager.py
@@ -220,7 +220,7 @@ class Manager:
         try:
             temp_EnsembleDirectory.make_copyback_check()
         except OSError as e:  # Ensemble dir exists and isn't empty.
-            logger.manager_warning(_USER_CALC_DIR_WARNING.format(temp_EnsembleDirectory.prefix))
+            logger.manager_warning(_USER_CALC_DIR_WARNING.format(temp_EnsembleDirectory.ensemble_dir))
             self._kill_workers()
             raise ManagerException(
                 "Manager errored on initialization",
