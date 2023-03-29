a092c8080d81a9b442ad01302b46a4ae65d89912
shudson <shudson@anl.gov>
2023-03-29 13:37:47 -0500

    Fix checksum for MPI runs

--- libensemble/tests/scaling_tests/forces/forces_gpu/run_libe_forces.py
+++ libensemble/tests/scaling_tests/forces/forces_gpu/run_libe_forces.py
@@ -65,8 +65,8 @@ gen_specs = {
         # ("resource_sets", int)  # Uncomment for var resources
     ],
     "user": {
-        "lb": np.array([50000]),  # User parameters for the gen_f
-        "ub": np.array([100000]),
+        "lb": np.array([50000]),  # fewest particles (changing will change checksum)
+        "ub": np.array([100000]), # max particles (changing will change checksum)
         "gen_batch_size": 8,
         # "max_resource_sets": nworkers  # Uncomment for var resources
     },
@@ -79,7 +79,7 @@ libE_specs["sim_dirs_make"] = True
 # libE_specs["stats_fmt"] = {"show_resource_sets": True}
 
 # Instruct libEnsemble to exit after this many simulations
-exit_criteria = {"sim_max": 8}
+exit_criteria = {"sim_max": 8}  # changing will change checksum
 
 # Seed random streams for each worker, particularly for gen_f
 persis_info = add_unique_random_streams({}, nworkers + 1)
@@ -88,9 +88,10 @@ persis_info = add_unique_random_streams({}, nworkers + 1)
 H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info=persis_info, libE_specs=libE_specs)
 
 # This is for configuration of this test (inc. lb/ub and sim_max values)
-if exit_criteria["sim_max"] == 8:
-    chksum = np.sum(H["energy"])
-    assert np.isclose(chksum, 96288744.35136001), f"energy check sum is {chksum}"
-    print("Checksum passed")
-else:
-    print("Run complete. A checksum has not been provided for the given sim_max")
+if is_manager:
+    if exit_criteria["sim_max"] == 8:
+        chksum = np.sum(H["energy"])
+        assert np.isclose(chksum, 96288744.35136001), f"energy check sum is {chksum}"
+        print("Checksum passed")
+    else:
+        print("Run complete. A checksum has not been provided for the given sim_max")
