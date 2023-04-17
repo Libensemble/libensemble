fe309081c9f0b66305d2d835ad7cec9ca9a1bbbb
shudson <shudson@anl.gov>
2023-04-17 11:15:42 -0500

    Remove unused import

--- libensemble/executors/mpi_executor.py
+++ libensemble/executors/mpi_executor.py
@@ -18,7 +18,6 @@ from libensemble.executors.executor import Executor, ExecutorException, Task
 from libensemble.executors.mpi_runner import MPIRunner
 from libensemble.resources.mpi_resources import get_MPI_variant
 from libensemble.executors.executor import Executor, Task, ExecutorException
-from libensemble.resources.platforms import known_systems, get_mpiexec_platforms
 from libensemble.resources.resources import Resources
 
 logger = logging.getLogger(__name__)
