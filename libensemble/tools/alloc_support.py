fa745f7582bf0960849b5dbb841e80c81b7a0625
Jeffrey Larson <jmlarson@anl.gov>
2023-07-11 10:52:26 -0500

    Removing decorator

--- libensemble/tools/alloc_support.py
+++ libensemble/tools/alloc_support.py
@@ -4,11 +4,7 @@ import numpy as np
 
 from libensemble.message_numbers import EVAL_GEN_TAG, EVAL_SIM_TAG
 from libensemble.resources.resources import Resources
-from libensemble.resources.scheduler import (  # noqa: F401
-    InsufficientFreeResources,
-    InsufficientResourcesError,
-    ResourceScheduler,
-)
+from libensemble.resources.scheduler import InsufficientFreeResources, InsufficientResourcesError, ResourceScheduler
 from libensemble.utils.misc import extract_H_ranges
 
 logger = logging.getLogger(__name__)
