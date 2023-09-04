9cee243fcf72a8e3eda5b52c76b651b05f0254ca
shudson <shudson@anl.gov>
2023-09-04 00:43:39 -0500

    Convert forces_gpu_persis_gen to OO interface

--- libensemble/tests/scaling_tests/forces/forces_gpu_persis_gen/run_libe_forces.py
+++ libensemble/tests/scaling_tests/forces/forces_gpu_persis_gen/run_libe_forces.py
@@ -6,12 +6,16 @@ particles is increased considerably to give perceptible time on the GPUs when
 live-checking GPU usage.
 
 The forces.c application should be built by setting the GPU preprocessor condition
-in addition to openMP GPU flags for the given system. See examples in
-../forces_app/build_forces.sh. We recommend running forces.x standalone first
+(usually -DGPU) in addition to openMP GPU flags for the given system. See examples
+in ../forces_app/build_forces.sh. We recommend running forces.x standalone first
 and confirm it is running on the GPU (this is given clearly in the output).
 
 An alternative variable resource generator is available (search 'var resources'
 in this script and uncomment relevant lines).
+
+To mock on a non-GPU system, uncomment the resource_info line in libE_specs. You
+will compile forces without -DGPU option. It is recommended that the lb/ub for
+particle counts are reduced for CPU performance.
 """
 
 import os
@@ -20,92 +24,84 @@ import sys
 import numpy as np
 from forces_simf import run_forces  # Sim func from current dir
 
+from libensemble import Ensemble
 from libensemble.alloc_funcs.start_only_persistent import only_persistent_gens as alloc_f
 from libensemble.executors import MPIExecutor
+from libensemble.tools import add_unique_random_streams, parse_args
+from libensemble.specs import AllocSpecs, ExitCriteria, GenSpecs, LibeSpecs, SimSpecs
 
 # Fixed resources (one resource set per worker) - persistent gen
 from libensemble.gen_funcs.persistent_sampling import persistent_uniform as gen_f
-from libensemble.libE import libE
-from libensemble.tools import add_unique_random_streams, parse_args
 
 # Uncomment for var resources (checksum will change due to rng differences)
 # from libensemble.gen_funcs.persistent_sampling_var_resources import uniform_sample as gen_f
 
+if __name__ == "__main__":
 
-# Parse number of workers, comms type, etc. from arguments
-nworkers, is_manager, libE_specs, _ = parse_args()
+    # Initialize MPI Executor
+    exctr = MPIExecutor()
+    sim_app = os.path.join(os.getcwd(), "../forces_app/forces.x")
 
-nsim_workers = nworkers - 1
-libE_specs["num_resource_sets"] = nsim_workers  # Persistent gen does not need resources
+    if not os.path.isfile(sim_app):
+        sys.exit("forces.x not found - please build first in ../forces_app dir")
 
-# To test on system without GPUs - compile forces without -DGPU and mock GPUs with this line.
-# libE_specs["resource_info"] = {"gpus_on_node": 4}
+    exctr.register_app(full_path=sim_app, app_name="forces")
 
-# Initialize MPI Executor instance
-exctr = MPIExecutor()
+    # Parse number of workers, comms type, etc. from arguments
+    ensemble = Ensemble(parse_args=True)
+    nsim_workers = ensemble.nworkers - 1  # One worker is for persistent generator
 
-# Register simulation executable with executor
-sim_app = os.path.join(os.getcwd(), "../forces_app/forces.x")
+    # Persistent gen does not need resources
+    ensemble.libE_specs = LibeSpecs(
+        num_resource_sets=nsim_workers,
+        sim_dirs_make=True,
+        # resource_info = {"gpus_on_node": 4}  # for mocking GPUs
+        # stats_fmt = {"show_resource_sets": True}  # see resource sets in libE_stats.txt
+    )
 
-if not os.path.isfile(sim_app):
-    sys.exit("forces.x not found - please build first in ../forces_app dir")
+    ensemble.sim_specs = SimSpecs(
+        sim_f=run_forces,
+        inputs=["x"],
+        out=[("energy", float)],
+    )
 
-exctr.register_app(full_path=sim_app, app_name="forces")
+    ensemble.gen_specs = GenSpecs(
+        gen_f=gen_f,
+        inputs=[],  # No input when start persistent generator
+        persis_in=["sim_id"],  # Return sim_ids of evaluated points to generator
+        out=[("x", float, (1,)),
+            # ("resource_sets", int)  # Uncomment for var resources
+             ],
+        user={
+            "initial_batch_size": nsim_workers,
+            "lb": np.array([50000]),  # min particles
+            "ub": np.array([100000]),  # max particles
+            # "max_resource_sets": nsim_workers  # Uncomment for var resources
+        },
+    )
 
-# State the sim_f, inputs, outputs
-sim_specs = {
-    "sim_f": run_forces,  # sim_f, imported above
-    "in": ["x"],  # Name of input for sim_f
-    "out": [("energy", float)],  # Name, type of output from sim_f
-}
+    # Starts one persistent generator. Simulated values are returned in batch.
+    ensemble.alloc_specs = AllocSpecs(
+        alloc_f=alloc_f,
+        user={
+            "async_return": False,  # False causes batch returns
+        },
+    )
 
-# State the gen_f, inputs, outputs, additional parameters
-gen_specs = {
-    "gen_f": gen_f,  # Generator function
-    "in": [],  # Generator input
-    "persis_in": ["sim_id"],  # Just send something back to gen to get number of new points.
-    "out": [
-        ("x", float, (1,)),  # Name, type and size of data from gen_f
-        # ("resource_sets", int)  # Uncomment for var resources
-    ],
-    "user": {
-        "lb": np.array([50000]),  # fewest particles (changing will change checksum)
-        "ub": np.array([100000]),  # max particles (changing will change checksum)
-        "initial_batch_size": nsim_workers,
-        # "max_resource_sets": nsim_workers  # Uncomment for var resources
-    },
-}
+    # Instruct libEnsemble to exit after this many simulations
+    ensemble.exit_criteria = ExitCriteria(sim_max=8)
 
-alloc_specs = {
-    "alloc_f": alloc_f,
-    "user": {
-        "give_all_with_same_priority": False,
-        "async_return": False,  # False causes batch returns
-    },
-}
+    # Seed random streams for each worker, particularly for gen_f
+    ensemble.add_random_streams()
 
-# Create and work inside separate per-simulation directories
-libE_specs["sim_dirs_make"] = True
+    # Run ensemble
+    ensemble.run()
 
-# Uncomment to see resource sets in libE_stats.txt - useful with var resources
-# libE_specs["stats_fmt"] = {"show_resource_sets": True}
-
-# Instruct libEnsemble to exit after this many simulations
-exit_criteria = {"sim_max": 8}  # changing will change checksum
-
-# Seed random streams for each worker, particularly for gen_f
-persis_info = add_unique_random_streams({}, nworkers + 1)
-
-# Launch libEnsemble
-H, persis_info, flag = libE(
-    sim_specs, gen_specs, exit_criteria, persis_info=persis_info, alloc_specs=alloc_specs, libE_specs=libE_specs
-)
-
-# This is for configuration of this test (inc. lb/ub and sim_max values)
-if is_manager:
-    if exit_criteria["sim_max"] == 8:
-        chksum = np.sum(H["energy"])
-        assert np.isclose(chksum, 96288744.35136001), f"energy check sum is {chksum}"
-        print("Checksum passed")
-    else:
-        print("Run complete. A checksum has not been provided for the given sim_max")
+    if ensemble.is_manager:
+        # Note, this will change if change sim_max, nworkers, lb/ub etc...
+        if ensemble.exit_criteria.sim_max == 8:
+            chksum = np.sum(ensemble.H["energy"])
+            assert np.isclose(chksum, 96288744.35136001), f"energy check sum is {chksum}"
+            print("Checksum passed")
+        else:
+            print("Run complete. A checksum has not been provided for the given sim_max")
