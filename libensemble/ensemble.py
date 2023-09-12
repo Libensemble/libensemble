06623120f62cd32b41784ec001c1746d67af2c24
John-Luke Navarro <33131245+jlnav@users.noreply.github.com>
2023-09-12 10:45:42 -0500

    does removing this whitespace line fix the parameter section? (#1082)
    * does removing this whitespace line fix the parameter section?
    
    * fix class path to LibeSpecs in parameter
    
    * fix class path to Executor in parameter

--- libensemble/ensemble.py
+++ libensemble/ensemble.py
@@ -216,7 +216,7 @@ class Ensemble:
 
         Tell libEnsemble when to stop a run
 
-    libE_specs: :obj:`dict` or :class:`LibeSpecs<libensemble.specs.libeSpecs>`, optional
+    libE_specs: :obj:`dict` or :class:`LibeSpecs<libensemble.specs.LibeSpecs>`, optional
 
         Specifications for libEnsemble
 
@@ -224,13 +224,12 @@ class Ensemble:
 
         Specifications for the allocation function
 
-
     persis_info: :obj:`dict`, optional
 
         Persistent information to be passed between user function instances
         :doc:`(example)<data_structures/persis_info>`
 
-    executor: :class:`Executor<libensemble.executors.executor.executor>`, optional
+    executor: :class:`Executor<libensemble.executors.executor.Executor>`, optional
 
         libEnsemble Executor instance for use within simulation or generator functions
 
