7b50cb1eecf3a5e7ff22a4ced89e817cf375c239
jlnav <jnavarro@anl.gov>
2023-09-13 14:07:11 -0500

    undo sphinx bump

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ sys.path.append(os.path.abspath("../libensemble/resources"))
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = "6.2.1"
+needs_sphinx = "5.3"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
