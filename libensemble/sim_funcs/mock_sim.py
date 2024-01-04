7d5d35ce6f11c41f6164dc1eb3dc9ec60c8e9d72
Jeffrey Larson <jmlarson@anl.gov>
2024-01-04 12:47:40 -0600

    Update mock_sim.py

--- libensemble/sim_funcs/mock_sim.py
+++ libensemble/sim_funcs/mock_sim.py
@@ -4,8 +4,8 @@ import numpy as np
 def mock_sim(H, persis_info, sim_specs, libE_info):
     """Places sim_specs["out"] from a numpy file into the outputs. Allow user
     to reproduce an existing run while, for example, capturing additional
-    information from a gen. Requires user has set
-    sim_specs["user"]["history_file"] to a history file from the previous run.
+    information from a gen. Requires user to have set
+    sim_specs["user"]["history_file"] to point to a history file from the previous run.
     """
 
     hfile = sim_specs["user"]["history_file"]
