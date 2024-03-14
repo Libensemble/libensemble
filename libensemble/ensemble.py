dbdf88f1e8992b648b323f95f1e2f4ce939d7e40
jlnav <jnavarro@anl.gov>
2024-03-14 15:10:58 -0500

    platform_specs sometimes seems to be at risk of disappearing when we convert LibeSpecs to dict, so lets save it and reinsert

--- libensemble/ensemble.py
+++ libensemble/ensemble.py
@@ -326,8 +326,14 @@ class Ensemble:
             return
 
         # Cast new libE_specs temporarily to dict
-        if not isinstance(new_specs, dict):
-            new_specs = specs_dump(new_specs, by_alias=True, exclude_none=True, exclude_defaults=True)
+        if not isinstance(new_specs, dict):  # exclude_defaults should only be enabled with Pydantic v2
+            platform_specs_set = False
+            if new_specs.platform_specs != {}:  # bugginess across Pydantic versions for recursively casting to dict
+                platform_specs_set = True
+                platform_specs = new_specs.platform_specs
+            new_specs = specs_dump(new_specs, exclude_none=True, exclude_defaults=True)
+            if platform_specs_set:
+                new_specs["platform_specs"] = specs_dump(platform_specs, exclude_none=True)
 
         # Unset "comms" if we already have a libE_specs that contains that field, that came from parse_args
         if new_specs.get("comms") and hasattr(self._libE_specs, "comms") and self.parsed:
