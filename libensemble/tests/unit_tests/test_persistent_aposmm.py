1a9e676f7ce28dd7cc09c5e60dd2be1b342b021c
jlnav <jnavarro@anl.gov>
2024-04-02 14:49:54 -0500

    reenable other aposmm unit tests

--- libensemble/tests/unit_tests/test_persistent_aposmm.py
+++ libensemble/tests/unit_tests/test_persistent_aposmm.py
@@ -260,8 +260,8 @@ def test_asktell_with_persistent_aposmm():
 
 
 if __name__ == "__main__":
-    # test_persis_aposmm_localopt_test()
-    # test_update_history_optimal()
-    # test_standalone_persistent_aposmm()
-    # test_standalone_persistent_aposmm_combined_func()
+    test_persis_aposmm_localopt_test()
+    test_update_history_optimal()
+    test_standalone_persistent_aposmm()
+    test_standalone_persistent_aposmm_combined_func()
     test_asktell_with_persistent_aposmm()
