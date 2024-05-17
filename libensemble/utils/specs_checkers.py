a0eed6aeb63d37b8b946d31d2d7bed399a4af92f
jlnav <jnavarro@anl.gov>
2024-05-17 12:33:18 -0500

    only raise DeprecationWarning for AllocSpecs.out if that attribute's list is actually populated

--- libensemble/utils/specs_checkers.py
+++ libensemble/utils/specs_checkers.py
@@ -34,7 +34,10 @@ def _check_output_fields(values):
     if scg(values, "gen_specs"):
         out_names += [e[0] for e in scg(values, "gen_specs").outputs]
     if scg(values, "alloc_specs"):
-        warnings.warn('AllocSpecs.out / alloc_specs["out"] is deprecated, to be removed in v1.4.0.', DeprecationWarning)
+        if len(scg(values, "alloc_specs")):
+            warnings.warn(
+                'AllocSpecs.out / alloc_specs["out"] is deprecated, to be removed in v1.4.0.', DeprecationWarning
+            )
         out_names += [e[0] for e in scg(values, "alloc_specs").outputs]
 
     for name in scg(values, "sim_specs").inputs:
