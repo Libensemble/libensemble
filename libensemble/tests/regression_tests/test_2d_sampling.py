ca31eeed99640eb320b854abf7350606e0bb53fc
shudson <shudson@anl.gov>
2024-07-19 12:12:07 -0500

    Test whole array

--- libensemble/tests/regression_tests/test_2d_sampling.py
+++ libensemble/tests/regression_tests/test_2d_sampling.py
@@ -25,26 +25,25 @@ from libensemble.specs import ExitCriteria, GenSpecs, LibeSpecs, SimSpecs
 # Main block is necessary only when using local comms with spawn start method (default on macOS and Windows).
 if __name__ == "__main__":
     sampling = Ensemble(parse_args=True)
-    sampling.libE_specs = LibeSpecs(save_every_k_gens=300)
     sampling.sim_specs = SimSpecs(sim_f=sim_f)
     sampling.gen_specs = GenSpecs(
         gen_f=gen_f,
         outputs=[("x", float, 2)],
         user={
-            "gen_batch_size": 500,
+            "gen_batch_size": 100,
             "lb": np.array([-3]),
             "ub": np.array([3]),
         },
     )
 
-    sampling.exit_criteria = ExitCriteria(sim_max=500)
+    sampling.exit_criteria = ExitCriteria(sim_max=200)
     sampling.add_random_streams()
 
     sampling.run()
     if sampling.is_manager:
-        assert len(sampling.H) >= 500
-        x = sampling.H['x'][:10]
-        f = sampling.H['f'][:10]
+        assert len(sampling.H) >= 200
+        x = sampling.H['x']
+        f = sampling.H['f']
         assert np.all(np.isclose(f, np.sqrt(np.sum(x**2, axis=1))))
         print("\nlibEnsemble with random sampling has generated enough points")
     sampling.save_output(__file__)
