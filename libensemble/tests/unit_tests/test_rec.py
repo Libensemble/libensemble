4213502e1159d29872c8ceb18be029cdb68da4b1
shudson <shudson@anl.gov>
2024-07-22 11:35:34 -0500

    Print out diagnostics

--- libensemble/tests/unit_tests/test_rec.py
+++ libensemble/tests/unit_tests/test_rec.py
@@ -1,7 +1,12 @@
 import numpy as np
+import scipy
 from scipy.spatial.distance import cdist
 
 def test_cdist():
+
+    print(f"NumPy version: {np.__version__}")
+    print(f"SciPy version: {scipy.__version__}")
+
     a = [[0.0, 0.0]]
     b = [[0.0, 0.0],
         [0.0, 0.0],
