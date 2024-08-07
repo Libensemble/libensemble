4489d42f24098918df2b46d6f5325ad2ffd618d5
jlnav <jnavarro@anl.gov>
2024-08-07 15:21:30 -0500

    dont import gpcam classes into gen_classes level

--- libensemble/gen_classes/__init__.py
+++ libensemble/gen_classes/__init__.py
@@ -1,4 +1,3 @@
 from .aposmm import APOSMM  # noqa: F401
-from .gpCAM import GP_CAM, GP_CAM_Covar  # noqa: F401
 from .sampling import UniformSample, UniformSampleDicts  # noqa: F401
 from .surmise import Surmise  # noqa: F401
