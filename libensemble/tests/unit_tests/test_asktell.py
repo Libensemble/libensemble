a969f500f58f52609ba954829477cef8041702d9
jlnav <jnavarro@anl.gov>
2024-08-16 15:16:36 -0500

    remove debug statement

--- libensemble/tests/unit_tests/test_asktell.py
+++ libensemble/tests/unit_tests/test_asktell.py
@@ -23,10 +23,6 @@ def test_asktell_sampling():
     gen = UniformSample(lb=np.array([-3, -2]), ub=np.array([3, 2]))
     assert len(gen.ask(10)) == 10
 
-    import ipdb
-
-    ipdb.set_trace()
-
     out = gen.ask_numpy(3)  # should get numpy arrays, non-flattened
     out = gen.ask(3)  # needs to get dicts, 2d+ arrays need to be flattened
     assert all([len(x) == 2 for x in out])  # np_to_list_dicts is now tested
