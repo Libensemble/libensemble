8d9a71de0c4e93731ba883d9c309835d23f2a152
jlnav <jnavarro@anl.gov>
2024-09-09 15:06:42 -0500

    fix edge-case where validation didn't run again after doing parse_args

--- libensemble/ensemble.py
+++ libensemble/ensemble.py
@@ -320,7 +320,9 @@ class Ensemble:
         if not self._libE_specs:
             self._libE_specs = LibeSpecs(**libE_specs_parsed)
         else:
-            self._libE_specs.__dict__.update(**libE_specs_parsed)
+            base = self._libE_specs.__dict__
+            base.update(libE_specs_parsed)
+            self._libE_specs = LibeSpecs(**base)  # do an instantation again so we get validation
 
         return self.nworkers, self.is_manager, self._libE_specs
 
