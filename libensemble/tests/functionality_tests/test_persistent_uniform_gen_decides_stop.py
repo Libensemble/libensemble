9b899b4a625b7e812949917f95856f643bc273db
shudson <shudson@anl.gov>
2024-10-07 15:53:40 -0500

    Remove wrong delimiter

--- libensemble/tests/functionality_tests/test_persistent_uniform_gen_decides_stop.py
+++ libensemble/tests/functionality_tests/test_persistent_uniform_gen_decides_stop.py
@@ -13,7 +13,7 @@ The number of concurrent evaluations of the objective function with 2 gens will 
 
 # Do not change these lines - they are parsed by run-tests.sh
 # TESTSUITE_COMMS: mpi local
-# TESTSUITE_NPROCS: 5, 7
+# TESTSUITE_NPROCS: 5 7
 # TESTSUITE_OS_SKIP: WIN
 
 import sys
