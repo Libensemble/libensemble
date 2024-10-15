c9ca12b54630195cfa3af893a7c99b685e055d4c
jlnav <jnavarro@anl.gov>
2024-10-15 16:41:29 -0500

    initial commit, creating workflow + sim_f that compiles some input files to t3d, then launches that t3d via effis with executor

---
+++ libensemble/sim_funcs/effis_t3d.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import os
+import re
+
+import effis.composition
+import numpy as np
+import t3d.trinity_lib
+
+
+def effis_t3d(H_in, _, sim_specs, libE_info):
+
+    # COMPILATION STEP
+
+    t3dir = os.path.dirname(t3d.trinity_lib.__file__)
+    setupdir = os.path.join(os.path.dirname(t3dir), "tests", "regression")
+    datadir = os.path.join(os.path.dirname(t3dir), "tests", "data")
+    configname = "test-w7x-relu"
+
+    x0 = H_in["x"][0]
+    x1 = H_in["x"][1]
+
+    exctr = libE_info["executor"]
+
+    runner = effis.composition.Workflow.DetectRunnerInfo()
+
+    MyWorkflow = effis.composition.Workflow(
+        Runner=runner,
+        Directory="output",
+    )
+    MyWorkflow.GroupMax["t3d"] = 2
+
+    Simulation = MyWorkflow.Application(cmd="t3d", Name="T3D-1", Ranks=1, Group="t3d")
+    Simulation.CommandLineArguments += [
+        "{0}.in".format(configname),
+        "--log",
+        "{0}.out".format(configname),
+    ]
+
+    Simulation.Input += effis.composition.Input(os.path.join(setupdir, "{0}.in".format(configname)))
+    Simulation.Input += effis.composition.Input(os.path.join(datadir, "wout_w7x.nc"), link=True)
+
+    plot = MyWorkflow.Application(
+        cmd="t3d-plot",
+        Name="plot-1",
+        Runner=None,
+    )
+    plot.CommandLineArguments += [
+        os.path.relpath(
+            os.path.join(Simulation.Directory, "{0}.bp".format(configname)),
+            start=plot.Directory,
+        ),
+        "--grid",
+        "--savefig",
+        "-p",
+        "density",
+        "temperature",
+        "pressure",
+        "heat_flux",
+        "particle_flux",
+        "flux",
+    ]
+    plot.DependsOn += Simulation
+
+    MyWorkflow.Create()
+
+    for Simulation in enumerate(MyWorkflow.Applications):
+
+        if Simulation.Name.find("plot") != -1:
+            continue
+
+        # Rewrite a few things in the config file
+        with open(os.path.join(Simulation.Directory, "{0}.in".format(configname)), "r") as infile:
+            config = infile.read()
+        config = re.compile(r"geo_file\s*=\s*.*", re.MULTILINE).sub('geo_file = "wout_w7x.nc"', config)
+
+        core = 0.35 + x0 * 0.01
+        edge = 0.29 + x1 * 0.01
+        config = re.compile(r"density = {core = 0.35, edge = 0.29, alpha=1, evolve = false}", re.MULTILINE,).sub(
+            "density = {{core = {0}, edge = {1}, alpha=1, evolve = false}}".format(core, edge),
+            config,
+        )
+
+        with open(os.path.join(Simulation.Directory, "{0}.in".format(configname)), "w") as outfile:
+            outfile.write(config)
+
+    # END COMPILATION STEP
+
+    # START SIM STEP
+    app_args = "output"
+    task = exctr.submit(app_name="effis", num_procs=1, app_args=app_args)
+    calc_status = exctr.polling_loop(task, timeout=5, delay=0.3)
+
+    # TODO: read output file, load into H_out
+
+    # END SIM STEP
+
+    H_out = np.zeros(1, dtype=sim_specs["out"])
+
+    return H_out, _, calc_status
---
+++ libensemble/tests/regression_tests/test_effis_t3d.py
@@ -0,0 +1,43 @@
+# Do not change these lines - they are parsed by run-tests.sh
+# TESTSUITE_COMMS: local
+# TESTSUITE_NPROCS: 8
+
+import shutil
+
+import numpy as np
+
+from libensemble import Ensemble
+
+# Import libEnsemble items for this test
+from libensemble.executors import Executor
+from libensemble.gen_funcs.sampling import latin_hypercube_sample
+from libensemble.sim_funcs.effis_t3d import effis_t3d
+from libensemble.specs import ExitCriteria, GenSpecs, LibeSpecs, SimSpecs
+from libensemble.tools import add_unique_random_streams
+
+# Main block is necessary only when using local comms with spawn start method (default on macOS and Windows).
+if __name__ == "__main__":
+    sampling = Ensemble(parse_args=True)
+    sampling.sim_specs = SimSpecs(sim_f=effis_t3d, inputs=["x"], outputs=[("Wtot_MJ", float)])
+    sampling.libE_specs = LibeSpecs(sim_dirs_make=True)
+    sampling.gen_specs = GenSpecs(
+        gen_f=latin_hypercube_sample,
+        outputs=[("x", float, (2,))],
+        user={
+            "gen_batch_size": 10,
+            "lb": np.array([0, 10]),
+            "ub": np.array([0, 10]),
+        },
+    )
+
+    exctr = Executor()
+    exctr.register_app(shutil.which("effis-submit"), app_name="effis")
+
+    sampling.persis_info = add_unique_random_streams({}, sampling.nworkers + 1)
+    sampling.exit_criteria = ExitCriteria(sim_max=10)
+
+    sampling.run()
+    if sampling.is_manager:
+        assert len(sampling.H) >= 10
+        print("\nlibEnsemble with random sampling has generated enough points")
+    sampling.save_output(__file__)
