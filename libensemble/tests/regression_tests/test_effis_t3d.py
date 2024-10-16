19a27f81e6cc2efb104d2141a813ae65434132ae
Jeffrey Larson <jmlarson@anl.gov>
2024-10-16 16:05:28 -0500

    Using a pregenerated sample for now

--- libensemble/tests/regression_tests/test_effis_t3d.py
+++ libensemble/tests/regression_tests/test_effis_t3d.py
@@ -10,31 +10,34 @@ from libensemble import Ensemble
 
 # Import libEnsemble items for this test
 from libensemble.executors import Executor
-from libensemble.gen_funcs.sampling import latin_hypercube_sample
+from libensemble.alloc_funcs.give_pregenerated_work import give_pregenerated_sim_work as alloc_f
 from libensemble.sim_funcs.effis_t3d import effis_t3d
-from libensemble.specs import ExitCriteria, GenSpecs, LibeSpecs, SimSpecs
-from libensemble.tools import add_unique_random_streams
+from libensemble.specs import ExitCriteria, AllocSpecs, LibeSpecs, SimSpecs
 
 # Main block is necessary only when using local comms with spawn start method (default on macOS and Windows).
 if __name__ == "__main__":
+    core = np.linspace(0.34, 0.36, 11)
+    edge = np.linspace(0.28, 0.3, 11)
+
+    # Create the meshgrid
+    X, Y = np.meshgrid(core, edge)
+
+    pairs = np.array(list(zip(X.ravel(), Y.ravel())))
+    n_samp = len(pairs)
+
+    H0 = np.zeros(n_samp, dtype=[("x", float, 8), ("sim_id", int), ("sim_started", bool)])
+    H0["sim_id"] = range(n_samp)
+    H0["sim_started"] = False
+
     sampling = Ensemble(parse_args=True)
     sampling.sim_specs = SimSpecs(sim_f=effis_t3d, inputs=["x"], outputs=[("Wtot_MJ", float)])
     sampling.libE_specs = LibeSpecs(sim_dirs_make=True)
-    sampling.gen_specs = GenSpecs(
-        gen_f=latin_hypercube_sample,
-        outputs=[("x", float, (2,))],
-        user={
-            "gen_batch_size": 10,
-            "lb": np.array([0, 0]),
-            "ub": np.array([10, 10]),
-        },
-    )
+    sampling.H0 = H0
+    sampling.alloc_specs = AllocSpecs(alloc_f=alloc_f)
+    sampling.exit_criteria = ExitCriteria(sim_max=n_samp)
 
     exctr = Executor()
     exctr.register_app(shutil.which("effis-submit"), app_name="effis")
-
-    sampling.persis_info = add_unique_random_streams({}, sampling.nworkers + 1)
-    sampling.exit_criteria = ExitCriteria(sim_max=10)
 
     sampling.run()
     if sampling.is_manager:
