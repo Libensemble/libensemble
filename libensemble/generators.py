033c32d4325a55138c95427d18d67d09fcb37ff6
shudson <shudson@anl.gov>
2024-11-13 16:47:11 -0600

    Add conditional code for executor forwarding

--- libensemble/generators.py
+++ libensemble/generators.py
@@ -150,10 +150,11 @@ class LibensembleGenThreadInterfacer(LibensembleGenerator):
         # self.inbox = thread_queue.Queue()  # sending betweween HERE and gen
         # self.outbox = thread_queue.Queue()
 
-        # SH this contains the thread lock -  removing.... wrong comm to pass on anyway.
-        # SH what if not using executor - should be an IF here
-        del Executor.executor.comm
-        self.libE_info["executor"] = Executor.executor
+        if Executor.executor is not None:
+            # SH this contains the thread lock -  removing.... wrong comm to pass on anyway.
+            if hasattr(Executor.executor, "comm"):
+                del Executor.executor.comm
+            self.libE_info["executor"] = Executor.executor
 
         # SH - fix comment (thread and process & name object appropriately - task? qcomm?)
         # self.thread = QCommThread(  # TRY A PROCESS
