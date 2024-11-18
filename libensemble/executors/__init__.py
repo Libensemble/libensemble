06004a10b5ecede1c2cb8ccb52f16a5b421efb64
jlnav <jnavarro@anl.gov>
2024-11-18 14:44:31 -0600

    DeprecationWarning for BalsamExecutor

--- libensemble/executors/__init__.py
+++ libensemble/executors/__init__.py
@@ -1,4 +1,5 @@
 import importlib.util
+import warnings
 
 from libensemble.executors.executor import Executor
 from libensemble.executors.mpi_executor import MPIExecutor
@@ -8,5 +9,11 @@ if importlib.util.find_spec("balsam"):
         from libensemble.executors.balsam_executor import BalsamExecutor  # noqa: F401
 
         __all__ = ["Executor", "MPIExecutor", "BalsamExecutor"]
+        warnings.warn(
+            "BalsamExecutor is deprecated, to be removed in v2.0. "
+            + "For cross-site ensembles, use Globus Compute. See the docs for more information.",
+            FutureWarning,
+        )
+
     except (ModuleNotFoundError, ImportError, AttributeError):
         __all__ = ["Executor", "MPIExecutor"]
