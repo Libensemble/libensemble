24c4afa8978bd86c8e28914d44772747cdab8ee7
jlnav <jnavarro@anl.gov>
2025-03-21 15:48:19 -0500

    add type hints to tools.py, fix a docstring, use npt.NDArray where appropriate

--- libensemble/tools/live_data/live_data.py
+++ libensemble/tools/live_data/live_data.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+import numpy.typing as npt
+
 
 class LiveData(ABC):
     """A base class for capturing and processing data during an ensemble"""
@@ -9,7 +11,7 @@ class LiveData(ABC):
         pass
 
     @abstractmethod
-    def live_update(self, hist: object) -> None:
+    def live_update(self, hist: npt.NDArray) -> None:
         """Process history data after simulation results have been added to history
 
         Parameters
@@ -19,7 +21,7 @@ class LiveData(ABC):
             A libEnsemble history type object.
         """
 
-    def finalize(self, hist: object) -> None:
+    def finalize(self, hist: npt.NDArray) -> None:
         """
         Finzalize live data processing
 
