1d6f83b748b864ffa7b22f58371ceb0357a8785d
jlnav <jnavarro@anl.gov>
2025-07-02 13:40:00 -0500

    cleanup sampling.py

--- libensemble/gen_classes/sampling.py
+++ libensemble/gen_classes/sampling.py
@@ -3,46 +3,11 @@
 import numpy as np
 from generator_standard.vocs import VOCS
 
-from libensemble.generators import Generator  # , LibensembleGenerator
-
-# from libensemble.utils.misc import list_dicts_to_np
+from libensemble.generators import Generator
 
 __all__ = [
     "UniformSample",
-    # "UniformSampleDicts",
 ]
-
-
-# class SampleBase(LibensembleGenerator):
-#     """Base class for sampling generators"""
-
-#     def _get_user_params(self, VOCS):
-#         """Extract user params"""
-#         self.ub = user_specs["ub"]
-#         self.lb = user_specs["lb"]
-#         self.n = len(self.lb)  # dimension
-#         assert isinstance(self.n, int), "Dimension must be an integer"
-#         assert isinstance(self.lb, np.ndarray), "lb must be a numpy array"
-#         assert isinstance(self.ub, np.ndarray), "ub must be a numpy array"
-
-
-# class UniformSample(SampleBase):
-#     """
-#     This generator returns ``gen_specs["initial_batch_size"]`` uniformly
-#     sampled points the first time it is called. Afterwards, it returns the
-#     number of points given. This can be used in either a batch or asynchronous
-#     mode by adjusting the allocation function.
-#     """
-
-#     def __init__(self, VOCS: VOCS, *args, **kwargs):
-#         super().__init__(VOCS, **kwargs)
-#         self._get_user_params(VOCS)
-
-#     def suggest_numpy(self, n_trials):
-#         return list_dicts_to_np(UniformSampleDicts(VOCS).suggest(n_trials))
-
-#     def ingest_numpy(self, calc_in):
-#         pass  # random sample so nothing to tell
 
 
 class UniformSample(Generator):
