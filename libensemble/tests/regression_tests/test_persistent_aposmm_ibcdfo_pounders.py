a571b77eb0e837ce8a5f9bce7de7d38b383f5b8d
jlnav <jnavarro@anl.gov>
2025-08-13 15:27:02 -0500

    check worker zero for run_order for aposmm-ibcdfo-pounders

--- libensemble/tests/regression_tests/test_persistent_aposmm_ibcdfo_pounders.py
+++ libensemble/tests/regression_tests/test_persistent_aposmm_ibcdfo_pounders.py
@@ -135,7 +135,7 @@ if __name__ == "__main__":
         H, persis_info, flag = libE(sim_specs, gen_specs, exit_criteria, persis_info, alloc_specs, libE_specs)
 
         if is_manager:
-            assert persis_info[1].get("run_order"), "Run_order should have been given back"
+            assert persis_info[0].get("run_order"), "Run_order should have been given back"
             assert flag == 0
 
             save_libE_output(H, persis_info, __file__, nworkers)
