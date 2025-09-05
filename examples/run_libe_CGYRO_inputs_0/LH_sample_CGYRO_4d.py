2decea708c48ed2b246d174e4864d6b5185d0692
Jeffrey Larson <jmlarson@anl.gov>
2025-09-05 08:47:29 -0700

    LH_sample code where KY doesn't seem to have much effect

---
+++ examples/run_libe_CGYRO_inputs_0/LH_sample_CGYRO_4d.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+import os
+import sys, getopt
+import subprocess
+
+import jinja2
+import numpy as np
+from CGYRO_sim import run_CGYRO_over_KY  # Sim func from current dir
+
+from libensemble import Ensemble
+from libensemble.executors import MPIExecutor
+from libensemble.gen_funcs.sampling import latin_hypercube_sample as gen_f
+from libensemble.specs import AllocSpecs, ExitCriteria, GenSpecs, LibeSpecs, SimSpecs
+
+
+def main(argv):
+
+    try:
+        opts, args = getopt.getopt(argv, "n:", ["nproc=", "nomp=", "numa=", "mpinuma="])
+
+        print(opts)
+        print(args)
+
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("--nproc"):
+            nproc = arg
+        elif opt in ("--nomp"):
+            nomp = arg
+        elif opt in ("--numa"):
+            numa = arg
+        elif opt in ("--mpinuma"):
+            mpinuma = arg
+
+    cgyro_input_file = "input.cgyro"
+
+    exctr = MPIExecutor()
+
+    # sim_app = "/global/common/software/atom/gacode-perlmutter-gpu/cgyro/src/cgyro"
+    sim_app = "/global/cfs/cdirs/m4493/ebelli/gacode/cgyro/src/cgyro"
+    # wrapper = "/global/cfs/cdirs/m4493/ebelli/gacode-perlmutter-gpu/platform/exec/wrap.PERLMUTTER_GPU"
+    wrapper = "/global/cfs/cdirs/m4493/ebelli/gacode/platform/exec/wrap.PERLMUTTER_GPU"
+
+    if not os.path.isfile(sim_app):
+        sys.exit("cgyro not found")
+
+    exctr.register_app(full_path=sim_app, app_name="cgyro", precedent=wrapper)
+
+    # Parse number of workers, comms type, etc. from arguments
+    ensemble = Ensemble(parse_args=True, executor=exctr)
+    nworkers = ensemble.nworkers
+
+    platform_specs = {"gpu_setting_type": "option_gpus_per_node"}
+
+    # Persistent gen does not need resources
+    ensemble.libE_specs = LibeSpecs(
+        gen_on_manager=True,
+        sim_dirs_make=True,
+        sim_input_dir="/global/homes/j/jmlarson/research/libensemble/examples/run_libe_CGYRO_inputs_0/kappa_correction_with_KY",
+        platform_specs=platform_specs,
+        reuse_output_dir=True,
+        save_every_k_sims=1,
+        stats_fmt = {"show_resource_sets": True, "task_datetime": True},
+    )
+
+    ensemble.sim_specs = SimSpecs(
+        sim_f=run_CGYRO_over_KY,
+        inputs=["x"],
+        outputs=[("f", float), ("fvec", float, 11), ("convstatement", "U100")],
+        user={
+            "input_filename": cgyro_input_file,
+            "input_names": ["KAPPA","DELTA","ZETA","KY"],
+            "plot_heat_flux": False,
+            "nproc": nproc,
+            "nomp": nomp,
+            "numa": numa,
+            "mpinuma": mpinuma,
+        },
+    )
+
+    ensemble.gen_specs = GenSpecs(
+        gen_f=gen_f,
+        inputs=[],  # No input when start persistent generator
+        persis_in=["sim_id"],  # Return sim_ids of evaluated points to generator
+        outputs=[("x", float, (3,))],
+        user={
+            # "initial_batch_size": nworkers,
+            "gen_batch_size": 10,
+            "lb": np.array([0.5, -0.75, -0.05]),  # lower bound for input
+            "ub": np.array([4.0, 0.0, 0.05]),  # upper bound for input
+        },
+    )
+
+    # Instruct libEnsemble to exit after this many simulations
+    ensemble.exit_criteria = ExitCriteria(sim_max=45)
+
+    # Seed random streams for each worker, particularly for gen_f
+    ensemble.add_random_streams()
+
+    # Run ensemble
+    ensemble.run()
+
+    if ensemble.is_manager:
+        ensemble.save_output(__file__)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
