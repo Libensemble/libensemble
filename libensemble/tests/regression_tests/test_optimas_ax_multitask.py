c4110ab095489b2c083a31eff0641a981e14a23f
shudson <shudson@anl.gov>
2026-02-09 17:48:35 -0600

    Make test_optimas_ax_multitask.py local only

--- libensemble/tests/regression_tests/test_optimas_ax_multitask.py
+++ libensemble/tests/regression_tests/test_optimas_ax_multitask.py
@@ -15,7 +15,7 @@ the objective function will be 4 as the generator is on the manager.
 """
 
 # Do not change these lines - they are parsed by run-tests.sh
-# TESTSUITE_COMMS: mpi local
+# TESTSUITE_COMMS: local
 # TESTSUITE_NPROCS: 4
 # TESTSUITE_EXTRA: true
 
