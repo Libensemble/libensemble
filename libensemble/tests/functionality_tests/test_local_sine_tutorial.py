2070a93c0877cc2b7f6286d847dff9a0fdb63006
jlnav <jnavarro@anl.gov>
2026-03-03 07:49:08 -0600

    fix tutorial code

--- libensemble/tests/functionality_tests/test_local_sine_tutorial.py
+++ libensemble/tests/functionality_tests/test_local_sine_tutorial.py
@@ -4,7 +4,8 @@ from sine_gen_std import RandomSample
 from sine_sim import sim_find_sine
 
 from libensemble import Ensemble
-from libensemble.specs import ExitCriteria, GenSpecs, LibeSpecs, SimSpecs
+from libensemble.alloc_funcs.start_only_persistent import only_persistent_gens
+from libensemble.specs import AllocSpecs, ExitCriteria, GenSpecs, LibeSpecs, SimSpecs
 
 if __name__ == "__main__":  # Python-quirk required on macOS and windows
     libE_specs = LibeSpecs(nworkers=4, comms="local")
@@ -19,6 +20,9 @@ if __name__ == "__main__":  # Python-quirk required on macOS and windows
         batch_size=4,
     )
 
+    # Specify that libEnsemble should pass work back-and-forth between the generator object
+    alloc_specs = AllocSpecs(alloc_f=only_persistent_gens)
+
     sim_specs = SimSpecs(
         sim_f=sim_find_sine,  # Our simulator function
         inputs=["x"],  # InputArray field names. "x" from gen_f output
@@ -27,7 +31,7 @@ if __name__ == "__main__":  # Python-quirk required on macOS and windows
 
     exit_criteria = ExitCriteria(sim_max=80)  # Stop libEnsemble after 80 simulations
 
-    ensemble = Ensemble(sim_specs, gen_specs, exit_criteria, libE_specs)
+    ensemble = Ensemble(sim_specs, gen_specs, exit_criteria, libE_specs, alloc_specs)
     ensemble.add_random_streams()  # setup the random streams unique to each worker
     ensemble.run()  # start the ensemble. Blocks until completion.
 
@@ -42,7 +46,7 @@ if __name__ == "__main__":  # Python-quirk required on macOS and windows
 
     for i in range(1, libE_specs.nworkers + 1):
         worker_xy = np.extract(history["sim_worker"] == i, history)
-        x = [entry.tolist()[0] for entry in worker_xy["x"]]
+        x = [entry.tolist() for entry in worker_xy["x"]]
         y = [entry for entry in worker_xy["y"]]
         plt.scatter(x, y, label="Worker {}".format(i), c=colors[i - 1])
 
