2f4b8814286fd4cb8f7bd0aafd91d12a5a0c5f84
Jeffrey Larson <jmlarson@anl.gov>
2026-04-08 12:13:05 -0500

    Updating IBCDFO handling of minq

--- install/install_ibcdfo.sh
+++ install/install_ibcdfo.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-git clone --recurse-submodules -b main https://github.com/POptUS/IBCDFO.git
-pushd IBCDFO/minq/py/minq5/
+git glone https://github.com/POptUS/MINQ
+pushd MINQ/py/minq5/
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
 echo "PYTHONPATH=$PYTHONPATH" >> $GITHUB_ENV
 popd
+git clone -b main https://github.com/POptUS/IBCDFO.git
 pushd IBCDFO/ibcdfo_pypkg/
 pip install -e .
 popd
