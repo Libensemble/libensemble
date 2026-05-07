d222be59c78d2a119df3e78d5fc1b3a16b37c157
jlnav <jnavarro@anl.gov>
2026-05-07 12:21:14 -0500

    add IBCDFO to pyproject.toml, keep minq where it is

--- install/install_ibcdfo.sh
+++ install/install_ibcdfo.sh
@@ -5,7 +5,3 @@ pushd MINQ/py/minq5/
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
 echo "PYTHONPATH=$PYTHONPATH" >> $GITHUB_ENV
 popd
-git clone -b main https://github.com/POptUS/IBCDFO.git
-pushd IBCDFO/ibcdfo_pypkg/
-pip install -e .
-popd
