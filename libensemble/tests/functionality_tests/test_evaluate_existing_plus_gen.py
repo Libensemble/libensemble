83020debbf65cf2563b1f3ebc99b4df8ca3073e4
jlnav <jnavarro@anl.gov>
2026-06-11 15:24:05 -0500

    fix array-equal comparisons

--- libensemble/tests/functionality_tests/test_evaluate_existing_plus_gen.py
+++ libensemble/tests/functionality_tests/test_evaluate_existing_plus_gen.py
@@ -55,13 +55,14 @@ if __name__ == "__main__":
         },
     }
     sampling.gen_specs = GenSpecs(**gen_specs)
-    sampling.H0 = create_H0(gen_specs, 50)
+    H0 = create_H0(gen_specs, 50)
+    sampling.H0 = H0
     sampling.alloc_specs = AllocSpecs(alloc_f=give_sim_work_first)
     sampling.run(sim_max=100)
 
     if sampling.is_manager:
-        assert len(sampling.H) == 2 * len(sampling.H0)
-        assert np.array_equal(sampling.H0["x"][:50], sampling.H["x"][:50])
+        assert len(sampling.H) == 2 * len(H0)
+        assert np.array_equal(H0["x"][:50], sampling.H["x"][:50])
         assert np.all(sampling.H["sim_ended"])
         assert np.all(sampling.H["gen_worker"] == 0)
         print("\nlibEnsemble correctly appended to the initial sample via an additional gen.")
