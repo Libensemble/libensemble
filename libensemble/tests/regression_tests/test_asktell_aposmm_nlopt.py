7732d13277718d6764301cd52820aae1405a9743
jlnav <jnavarro@anl.gov>
2026-07-24 11:16:43 -0500

    Revert "Merge pull request #1763 from Libensemble/split-regression-commits"
    This reverts commit 6c4d35e7c0ac7ab7bf22236583ecef741efd11a7, reversing
    changes made to 4105473c34929eb7130664c27673e6aebfe4d421.

--- libensemble/tests/regression_tests/test_asktell_aposmm_nlopt.py
+++ libensemble/tests/regression_tests/test_asktell_aposmm_nlopt.py
@@ -2,12 +2,13 @@
 Runs libEnsemble with APOSMM with the NLopt local optimizer.
 
 Execute via one of the following commands (e.g. 3 workers):
-   mpiexec -np 4 python test_aposmm_nlopt.py
-   python test_aposmm_nlopt.py --nworkers 3 --comms local
-   python test_aposmm_nlopt.py --nworkers 3 --comms tcp
+   mpiexec -np 4 python test_persistent_aposmm_nlopt.py
+   python test_persistent_aposmm_nlopt.py --nworkers 3 --comms local
+   python test_persistent_aposmm_nlopt.py --nworkers 3 --comms tcp
 
 When running with the above commands, the number of concurrent evaluations of
-the objective function will be 3, as the generator runs on the manager.
+the objective function will be 2, as one of the three workers will be the
+persistent generator.
 """
 
 # Do not change these lines - they are parsed by run-tests.sh
@@ -19,6 +20,8 @@ from math import gamma, pi, sqrt
 import numpy as np
 
 import libensemble.gen_funcs
+
+# Import libEnsemble items for this test
 
 libensemble.gen_funcs.rc.aposmm_optimizers = "nlopt"
 from time import time
@@ -35,12 +38,13 @@ def six_hump_camel_func(x):
     """
     Definition of the six-hump camel
     """
-    x1 = x["x0"]
-    x2 = x["x1"]
+    x1 = x["core"]
+    x2 = x["edge"]
     term1 = (4 - 2.1 * x1**2 + (x1**4) / 3) * x1**2
     term2 = x1 * x2
     term3 = (-4 + 4 * x2**2) * x2**2
-    return {"f": term1 + term2 + term3}
+
+    return {"energy": term1 + term2 + term3}
 
 
 # Main block is necessary only when using local comms with spawn start method (default on macOS and Windows).
@@ -54,39 +58,39 @@ if __name__ == "__main__":
 
     vocs = VOCS(
         variables={
-            "x0": [-3, 3],
-            "x1": [-2, 2],
-            "x0_on_cube": [0, 1],
-            "x1_on_cube": [0, 1],
+            "core": [-3, 3],
+            "edge": [-2, 2],
+            "core_on_cube": [0, 1],
+            "edge_on_cube": [0, 1],
         },
-        objectives={"f": "MINIMIZE"},
+        objectives={"energy": "MINIMIZE"},
     )
 
     aposmm = APOSMM(
         vocs,
         max_active_runs=6,
         variables_mapping={
-            "x": ["x0", "x1"],
-            "x_on_cube": ["x0_on_cube", "x1_on_cube"],
-            "f": ["f"],
+            "x": ["core", "edge"],
+            "x_on_cube": ["core_on_cube", "edge_on_cube"],
+            "f": ["energy"],
         },
-        initial_sample_size=100,
+        initial_sample_size=200,
         sample_points=np.round(minima, 1),
         localopt_method="LN_BOBYQA",
         rk_const=0.5 * ((gamma(1 + (n / 2)) * 5) ** (1 / n)) / sqrt(pi),
         xtol_abs=1e-6,
         ftol_abs=1e-6,
-        dist_to_bound_multiple=0.5,
     )
 
     workflow.gen_specs = GenSpecs(
         generator=aposmm,
         vocs=vocs,
-        initial_batch_size=100,
+        batch_size=5,
+        initial_batch_size=10,
     )
 
     workflow.sim_specs = SimSpecs(simulator=six_hump_camel_func, vocs=vocs)
-    workflow.exit_criteria = ExitCriteria(sim_max=3000)
+    workflow.exit_criteria = ExitCriteria(sim_max=3000, wallclock_max=600)
 
     # Perform the run
     H, _, _ = workflow.run()
